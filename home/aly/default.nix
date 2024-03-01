0ec552b7f185c401b4a3cd8a02033d83d4554ffb
Aly Raffauf <achacega@gmail.com>
2024-02-29 21:19:54 -0500

    removed neofetch from aly

--- home/aly/default.nix
+++ home/aly/default.nix
@@ -10,7 +10,6 @@
         # here is some command line tools I use frequently
         # feel free to add your own or remove some of them
 
-        neofetch
         nnn # terminal file manager
 
         # utils
