bd6c6fac4ae5b1a49620f7f80c38c442312959f8
Aly Raffauf <achacega@gmail.com>
2024-02-29 21:46:49 -0500

    set rustboro to ondemand cpu governor

--- hosts/rustboro/default.nix
+++ hosts/rustboro/default.nix
@@ -61,6 +61,8 @@
   #  wget
   ];
 
+  powerManagement.cpuFreqGovernor = "ondemand"
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
