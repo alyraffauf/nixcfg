35a2362e75dd1aa82cdc2ab4fe57bffd0e03a7af
Aly Raffauf <achacega@gmail.com>
2024-03-01 11:08:05 -0500

    added easyeffects to rustboro host

--- hosts/rustboro/default.nix
+++ hosts/rustboro/default.nix
@@ -50,8 +50,9 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  # ];
+  environment.systemPackages = with pkgs; [
+    easyeffects
+  ];
 
   powerManagement.cpuFreqGovernor = "ondemand";
 
