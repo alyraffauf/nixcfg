460c18792b53a42a55277ba26848a16a7b3c4118
Aly Raffauf <achacega@gmail.com>
2024-03-01 10:25:13 -0500

    removed gaming directory

--- programs/steam/default.nix
+++ programs/steam/default.nix
@@ -5,4 +5,9 @@
     "com.valvesoftware.Steam"
   ];
   hardware.steam-hardware.enable = true;
+  # programs.steam = {
+  #   enable = true;
+  #   remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  #   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  # };
 }
