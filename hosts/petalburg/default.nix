6ee3c7fc2f8a3bfc4d0e2832c522f1dd3114ac3a
Aly Raffauf <alyraffauf@gmail.com>
2024-03-02 14:23:36 -0500

    enabled auto-rotate in tablet mode for petalburg

--- hosts/petalburg/default.nix
+++ hosts/petalburg/default.nix
@@ -17,6 +17,8 @@
 
   networking.hostName = "petalburg"; # Define your hostname.
 
+  hardware.sensor.iio.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
