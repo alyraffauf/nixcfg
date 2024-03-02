a4626858ca608e5bf36fe2e3a5f505665b6abbc3
Aly Raffauf <achacega@gmail.com>
2024-03-01 23:50:07 -0500

    removed commented global config from flatpak/default.nix

--- flatpak/default.nix
+++ flatpak/default.nix
@@ -8,18 +8,6 @@
     "org.mozilla.firefox"
   ];
   services.flatpak.overrides = {
-    global = {
-      # Force Wayland by default
-      # Context.sockets = ["wayland" "!x11" "!fallback-x11"];
-
-      # Environment = {
-      #   # Fix un-themed cursor in some Wayland apps
-      #   XCURSOR_PATH = "/run/host/user-share/icons:/run/host/share/icons";
-
-      #   # # Force correct theme for some GTK apps
-      #   # GTK_THEME = "Adwaita:dark";
-      # };
-    };
 
     "com.visualstudio.code".Context = {
       filesystems = [
