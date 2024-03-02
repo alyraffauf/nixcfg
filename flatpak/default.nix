f0a0b84a4e0f7bd013f4e76e0fdd57bc92c14fa0
Aly Raffauf <achacega@gmail.com>
2024-03-01 19:42:48 -0500

    migrated fonts.fonts -> fonts.packages

--- flatpak/default.nix
+++ flatpak/default.nix
@@ -46,7 +46,7 @@
     };
     aggregatedFonts = pkgs.buildEnv {
       name = "system-fonts";
-      paths = config.fonts.fonts;
+      paths = config.fonts.packages;
       pathsToLink = [ "/share/fonts" ];
     };
   in {
