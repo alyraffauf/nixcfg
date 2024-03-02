89bbe3d167443aaa618a20f001ae7f45d61fd90b
Aly Raffauf <achacega@gmail.com>
2024-03-01 23:53:48 -0500

    moved sh settings to network .nix

--- network/default.nix
+++ network/default.nix
@@ -3,21 +3,8 @@
 {
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
   # Enable tailscale
   services.tailscale.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # Enable avahi.
   services.avahi.enable = true;
@@ -34,7 +21,12 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  networking.firewall.allowedTCPPorts = [ 22 ];
 }
