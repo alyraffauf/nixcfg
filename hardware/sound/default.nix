35dc2c4657ea32e5cb7506c9c6251260adf681f6
Aly Raffauf <achacega@gmail.com>
2024-03-01 23:48:12 -0500

    removed unused lines from hardware/sound/default.nix

--- hardware/sound/default.nix
+++ hardware/sound/default.nix
@@ -16,9 +16,5 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 }
