5c36e390e66210b36bf8f8eeec1d07f45296be18
Aly Raffauf <achacega@gmail.com>
2024-03-03 12:04:50 -0500

    brought over dconf settings for dustin

--- home/dustin/default.nix
+++ home/dustin/default.nix
@@ -19,20 +19,24 @@
 
     dconf = {
         enable = true;
-        settings."org/gnome/desktop/interface".clock-format = "12h";
-        settings."org/gnome/mutter".edge-tiling = true;
-        settings."org/gnome/mutter".dynamic-workspaces = true;
-        settings."org/gnome/desktop/interface".enable-hot-corners = true;
         settings."org/gnome/desktop/datetime".automatic-timezone = true;
-        settings."org/gtk/settings/file-chooser".sort-directories-first = true;
-        settings."org/gtk/gtk4/settings/file-chooser".sort-directories-first = true;
-        settings."org/gnome/shell/extensions/blur-my-shell/panel".customize = true;
-        settings."org/gnome/shell/extensions/blur-my-shell/panel".override-background-dynamically = "true";
-        settings."org/gnome/shell/extensions/blur-my-shell/panel".unblur-in-overview = true;
-        settings."org/gnome/shell/extensions/blur-my-shell/overview".style-components = 3;
-        settings."org/gnome/system/location".enabled = true;
-        settings."org/gnome/desktop/wm/preferences".auto-raise = true;
+        settings."org/gnome/desktop/interface".clock-format = "12h";
+        settings."org/gnome/desktop/interface".enable-hot-corners = true;
+        settings."org/gnome/desktop/peripherals/touchpad".tap-to-click = true;
         settings."org/gnome/desktop/search-providers".enabled = "['org.gnome.Calendar.desktop', 'org.gnome.Weather.desktop', 'org.gnome.Contacts.desktop', 'org.gnome.Calculator.desktop', 'org.gnome.Characters.desktop', 'org.gnome.clocks.desktop']";
+        settings."org/gnome/desktop/wm/preferences".auto-raise = true;
+        settings."org/gnome/mutter".dynamic-workspaces = true;
+        settings."org/gnome/mutter".edge-tiling = true;
+        settings."org/gnome/shell/extensions/blur-my-shell/overview".style-components = 3;
+        settings."org/gnome/shell/extensions/blur-my-shell/panel".blur = false;
+        settings."org/gnome/shell/extensions/blur-my-shell/panel".customize = true;
+        settings."org/gnome/shell/extensions/blur-my-shell/panel".style-panel = 0;
+        settings."org/gnome/shell/extensions/blur-my-shell/panel".override-background = false;
+        settings."org/gnome/shell/extensions/blur-my-shell/panel".override-background-dynamically = false;
+        settings."org/gnome/shell/extensions/blur-my-shell/panel".unblur-in-overview = true;
+        settings."org/gnome/system/location".enabled = true;
+        settings."org/gtk/gtk4/settings/file-chooser".sort-directories-first = true;
+        settings."org/gtk/settings/file-chooser".sort-directories-first = true;
         settings."org/gnome/shell".enabled-extensions = [
             "appindicatorsupport@rgcjonas.gmail.com"
             "blur-my-shell@aunetx"
@@ -41,6 +45,7 @@
             "noannoyance-fork@vrba.dev"
             "tailscale-status@maxgallup.github.com"
             "tiling-assistant@leleat-on-github"
+            "drive-menu@gnome-shell-extensions.gcampax.github.com"
           ];
         };
     # This value determines the home Manager release that your
