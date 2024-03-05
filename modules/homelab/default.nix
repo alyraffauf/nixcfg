4de8c1c12a2c5eed82503f002515ba3cfb5ec189
Aly Raffauf <alyraffauf@gmail.com>
2024-03-05 15:19:01 -0500

    added oci  plex, audiobookshelf, and transmission oci containers to homelab config

--- modules/homelab/default.nix
+++ modules/homelab/default.nix
@@ -1,4 +1,37 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
+    # Open TCP ports for audiobookshelf, plex-server, and transmission-server.
+    networking.firewall.allowedTCPPorts = [ 13378 32400 9091 ];
+    virtualisation = {
+        podman = {
+            enable = true;
 
+            # Create a `docker` alias for podman, to use it as a drop-in replacement
+            dockerCompat = true;
+
+            # Required for containers under podman-compose to be able to talk to each other.
+            defaultNetwork.settings.dns_enabled = true;
+        };
+        oci-containers = {
+            backend = "podman";
+        };
+
+        oci-containers.containers = {
+            audiobookshelf = {
+                ports = ["0.0.0.0:13378:80"];
+                image = "ghcr.io/advplyr/audiobookshelf:latest";
+            };
+            plex-server = {
+                ports = ["0.0.0.0:32400:32400"];
+                image = "plexinc/pms-docker:public";
+            };
+            transmission-server = {
+                ports = ["0.0.0.0:9091:9091"];
+                image = "linuxserver/transmission:latest";
+                # volumes = [
+                #     "/home/aly/transmission:/config"
+                # ];
+            };
+        };
+    };
 }
