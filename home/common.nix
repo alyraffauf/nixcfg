d53881137db18fb2c26633a132034be241885c38
Aly Raffauf <alyraffauf@gmail.com>
2024-03-06 18:19:36 -0500

    cleaned up dconf config and added virt-manager auto connect to qemu

--- home/common.nix
+++ home/common.nix
@@ -20,27 +20,27 @@
         defaultEditor = true;
     };
 
-    dconf = {
-        enable = true;
-        settings."org/gnome/desktop/datetime".automatic-timezone = true;
-        settings."org/gnome/desktop/interface".clock-format = "12h";
-        settings."org/gnome/desktop/interface".enable-hot-corners = true;
-        settings."org/gnome/desktop/peripherals/touchpad".tap-to-click = true;
-        settings."org/gnome/desktop/search-providers".enabled = "['org.gnome.Calendar.desktop', 'org.gnome.Weather.desktop', 'org.gnome.Contacts.desktop', 'org.gnome.Calculator.desktop', 'org.gnome.Characters.desktop', 'org.gnome.clocks.desktop']";
-        settings."org/gnome/desktop/wm/preferences".auto-raise = true;
-        settings."org/gnome/mutter".dynamic-workspaces = true;
-        settings."org/gnome/mutter".edge-tiling = true;
-        settings."org/gnome/shell/extensions/blur-my-shell/overview".style-components = 3;
-        settings."org/gnome/shell/extensions/blur-my-shell/panel".blur = false;
-        settings."org/gnome/shell/extensions/blur-my-shell/panel".customize = true;
-        settings."org/gnome/shell/extensions/blur-my-shell/panel".style-panel = 0;
-        settings."org/gnome/shell/extensions/blur-my-shell/panel".override-background = false;
-        settings."org/gnome/shell/extensions/blur-my-shell/panel".override-background-dynamically = false;
-        settings."org/gnome/shell/extensions/blur-my-shell/panel".unblur-in-overview = true;
-        settings."org/gnome/system/location".enabled = true;
-        settings."org/gtk/gtk4/settings/file-chooser".sort-directories-first = true;
-        settings."org/gtk/settings/file-chooser".sort-directories-first = true;
-        settings."org/gnome/shell".enabled-extensions = [
+    dconf.enable = true;
+    dconf.settings = {
+        "org/gnome/desktop/datetime".automatic-timezone = true;
+        "org/gnome/desktop/interface".clock-format = "12h";
+        "org/gnome/desktop/interface".enable-hot-corners = true;
+        "org/gnome/desktop/peripherals/touchpad".tap-to-click = true;
+        "org/gnome/desktop/search-providers".enabled = "['org.gnome.Calendar.desktop', 'org.gnome.Weather.desktop', 'org.gnome.Contacts.desktop', 'org.gnome.Calculator.desktop', 'org.gnome.Characters.desktop', 'org.gnome.clocks.desktop']";
+        "org/gnome/desktop/wm/preferences".auto-raise = true;
+        "org/gnome/mutter".dynamic-workspaces = true;
+        "org/gnome/mutter".edge-tiling = true;
+        "org/gnome/shell/extensions/blur-my-shell/overview".style-components = 3;
+        "org/gnome/shell/extensions/blur-my-shell/panel".blur = false;
+        "org/gnome/shell/extensions/blur-my-shell/panel".customize = true;
+        "org/gnome/shell/extensions/blur-my-shell/panel".style-panel = 0;
+        "org/gnome/shell/extensions/blur-my-shell/panel".override-background = false;
+        "org/gnome/shell/extensions/blur-my-shell/panel".override-background-dynamically = false;
+        "org/gnome/shell/extensions/blur-my-shell/panel".unblur-in-overview = true;
+        "org/gnome/system/location".enabled = true;
+        "org/gtk/gtk4/settings/file-chooser".sort-directories-first = true;
+        "org/gtk/settings/file-chooser".sort-directories-first = true;
+        "org/gnome/shell".enabled-extensions = [
             "appindicatorsupport@rgcjonas.gmail.com"
             "blur-my-shell@aunetx"
             "gsconnect@andyholmes.github.io"
@@ -49,8 +49,12 @@
             "tailscale-status@maxgallup.github.com"
             "tiling-assistant@leleat-on-github"
             "drive-menu@gnome-shell-extensions.gcampax.github.com"
-          ];
+        ];
+        "org/virt-manager/virt-manager/connections" = {
+            autoconnect = ["qemu:///system"];  
+            uris = ["qemu:///system"];
         };
+    };
     # This value determines the home Manager release that your
     # configuration is compatible with. This helps avoid breakage
     # when a new home Manager release introduces backwards
