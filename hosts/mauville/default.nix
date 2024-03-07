de2bcdc56269ac4187b72cca76c820009e158f78
Aly Raffauf <alyraffauf@gmail.com>
2024-03-07 02:02:00 -0500

    enabled autologin on mauville

--- hosts/mauville/default.nix
+++ hosts/mauville/default.nix
@@ -27,6 +27,9 @@
     options = lib.mkForce "--delete-older-than 14d";
   };
 
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "aly";
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
