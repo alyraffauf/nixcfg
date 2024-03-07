90e1fcf2861a80280d8d2c3bf0306cb4bcee3f4f
Aly Raffauf <alyraffauf@gmail.com>
2024-03-07 18:54:43 -0500

    enabled thermald on petalburg

--- hosts/petalburg/default.nix
+++ hosts/petalburg/default.nix
@@ -56,6 +56,7 @@ in {
   hardware.sensor.iio.enable = true;
 
   powerManagement.powertop.enable = true;
+  services.thermald.enable = true;
 
   environment.systemPackages = [ cs-adjuster pp-adjuster ];
 
