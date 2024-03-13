91feaecb971b74e9996d42d0c59b5f84b18cb59f
Aly Raffauf <alyraffauf@gmail.com>
2024-03-13 15:36:44 -0400

    switched sddm to use wayland

--- desktop/kde.nix
+++ desktop/kde.nix
@@ -7,7 +7,7 @@
     ];
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm.wayland.enable = true;
   services.xserver.desktopManager.plasma6.enable = true;
 
   environment.systemPackages = with pkgs; [
