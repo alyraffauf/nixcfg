cb3590483e54eca9d71001ee5e6571b02ee18a3a
Aly Raffauf <alyraffauf@gmail.com>
2024-03-12 22:37:58 -0400

    moved steam to nixpkgs source

--- modules/steam/default.nix
+++ modules/steam/default.nix
@@ -1,13 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  services.flatpak.packages = [
-    "com.valvesoftware.Steam"
-  ];
-  hardware.steam-hardware.enable = true;
-  # programs.steam = {
-  #   enable = true;
-  #   remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-  #   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  # };
+    hardware.steam-hardware.enable = true;
+    programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    };
 }
