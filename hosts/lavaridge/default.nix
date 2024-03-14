c28621217b6e6c24bf16eb8ae65d8f6cf8a8ecce
Aly Raffauf <alyraffauf@gmail.com>
2024-03-13 23:15:42 -0400

    added fwupd downgrade to fix fingerprint reader on lavaridge

--- hosts/lavaridge/default.nix
+++ hosts/lavaridge/default.nix
@@ -20,7 +20,17 @@
   networking.hostName = "lavaridge"; # Define your hostname.
 
   powerManagement.powertop.enable = true;
-  services.fwupd.enable = true;
+
+  {
+    services.fwupd.enable = true;
+    # we need fwupd 1.9.7 to downgrade the fingerprint sensor firmware
+    services.fwupd.package = (import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/bb2009ca185d97813e75736c2b8d1d8bb81bde05.tar.gz";
+      sha256 = "sha256:003qcrsq5g5lggfrpq31gcvj82lb065xvr7bpfa8ddsw8x4dnysk";
+    }) {
+      inherit (pkgs) system;
+    }).fwupd;
+  }
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
