1d0364500878b7bd4da45b9899748423198eebe3
Aly Raffauf <alyraffauf@gmail.com>
2024-03-15 00:02:03 -0400

    lavaridge: force chromium and electron apps to use wayland

--- hosts/lavaridge/default.nix
+++ hosts/lavaridge/default.nix
@@ -32,6 +32,9 @@
   #   inherit (pkgs) system;
   # }).fwupd;
 
+  nixpkgs.config.chromium.commandLineArgs = "--ozone-platform=wayland";
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
