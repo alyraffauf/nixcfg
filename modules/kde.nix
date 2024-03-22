35deb5e409db4de0a17a1e7968c5772e13eb3b5d
Aly Raffauf <alyraffauf@gmail.com>
2024-03-22 14:09:25 -0400

    kde: fixed sddm settings

--- modules/kde.nix
+++ modules/kde.nix
@@ -11,8 +11,8 @@
     desktopManager.plasma6.enable = true;
     xserver = {
       displayManager.sddm = {
+        enable = true;
         wayland.enable = true;
-        enableHidpi = true;
       };
     };
   };
