153495afbd45088428c31f3e8fcf33e93e7dc9a1
Aly Raffauf <alyraffauf@gmail.com>
2024-03-22 18:26:42 -0400

    updated emacs config

--- home/shell.nix
+++ home/shell.nix
@@ -11,6 +11,15 @@
 
     programs.emacs = {
         enable = true;
+        extraPackages = ( epkgs: (with epkgs; [
+            better-defaults
+            org
+            org-bullets
+            org-journal
+            org-roam     
+            nix-mode 
+            ])
+        );
         package = pkgs.emacs-nox;
         extraConfig = ''
             (setq-default indent-tabs-mode nil)
@@ -48,10 +57,22 @@
         enable = true;
         mouse = true;
         newSession = true;
+        plugins = with pkgs; [
+            tmuxPlugins.weather
+            tmuxPlugins.better-mouse-mode
+            tmuxPlugins.cpu
+            tmuxPlugins.battery
+        ];
         terminal = "tmux-256color";
         extraConfig = ''
             set-option -g status-bg blue
             set-option -g pane-active-border-style fg=blue
+            set-option -g @tmux-weather-format "%x+%t"
+            set-option -g @tmux-weather-units "u"
+            set -g status-right '#{cpu_percentage} CPU | #{battery_percentage} BAT | %I:%M %p | #{weather}'
+            run-shell ${pkgs.tmuxPlugins.weather}/share/tmux-plugins/weather/tmux-weather.tmux
+            run-shell ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/cpu/cpu.tmux
+            run-shell ${pkgs.tmuxPlugins.battery}/share/tmux-plugins/battery/battery.tmux
         '';
     };
 }
