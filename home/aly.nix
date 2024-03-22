8c13f018b9d05e306218a5e506697ab14b341944
Aly Raffauf <alyraffauf@gmail.com>
2024-03-22 13:41:10 -0400

    home-manager: added new alacritty config

--- home/aly.nix
+++ home/aly.nix
@@ -30,17 +30,23 @@
     programs.alacritty = {
         enable = true;
         settings = {
+            colors = {
+                primary = {
+                    foreground = "#fafafa";
+                    background = "#000000";
+                };
+                draw_bold_text_with_bright_colors = true;
+            };
             font = {
                 normal = { family = "DroidSansM Nerd Font Mono"; style = "Regular"; };
                 size = 11;
             };
             selection.save_to_clipboard = true;
             window = {
-                # blur = true;
-                decorations = "None";
+                blur = true;
+                # decorations = "None";
                 dynamic_padding = true;
                 opacity = 0.8;
-                startup_mode = "Maximized";
             };
         };
     };
