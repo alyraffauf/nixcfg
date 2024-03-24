375a0927d7147a6fb2a86875a16a0e2c6c9e42a9
Aly Raffauf <alyraffauf@gmail.com>
2024-03-24 14:04:42 -0400

    home-manager: updated waybar font and colors

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -1,86 +1,92 @@
 { config, pkgs, ... }:
 
 {
-    # Packages that should be installed to the user profile.
-    home.packages = with pkgs; [
-      overskride
-      pavucontrol
-    ];
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [ overskride pavucontrol ];
 
-    xdg.configFile."waybar/style.css".source = ./waybar.css;
+  xdg.configFile."waybar/style.css".source = ./waybar.css;
 
-    programs.waybar.enable = true;
-    programs.waybar.settings = {
-        mainBar = {
-            layer = "top";
-            position = "top";
-            height = 36;
-            output = [
-                "eDP-1"
-                "HDMI-A-1"
-            ];
-            modules-left = [ "hyprland/workspaces" "hyprland/submap" ];
-            modules-center = [ "hyprland/window" ];
-            modules-right = [ "tray" "bluetooth" "network" "pulseaudio" "battery" "power-profiles-daemon" "clock"];
+  programs.waybar.enable = true;
+  programs.waybar.settings = {
+    mainBar = {
+      layer = "top";
+      position = "top";
+      height = 36;
+      output = [ "eDP-1" "DP-1" "HDMI-A-1" ];
+      modules-left = [ "hyprland/workspaces" "hyprland/submap" ];
+      modules-center = [ "hyprland/window" ];
+      modules-right = [
+        "tray"
+        "bluetooth"
+        "network"
+        "pulseaudio"
+        "battery"
+        "power-profiles-daemon"
+        "clock"
+      ];
 
-            "hyprland/workspaces" = {
-                "all-outputs" = true;
-            };
-            "hyprland/window" = {
-                "max-length" = 150;
-            };
-            "clock" = {
-                "tooltip-format" = "{:%Y-%m-%d | %H:%M}";
-                "interval" = 60;
-                "format" = "{:%I:%M%p}";
-            };
-            "battery" = {
-                "states" = {
-                    "critical" = 20;
-                };
-                "format" = "{icon}";
-                "format-icons" = ["󰁺" "󰁼" "󰁿" "󰂁" "󰁹"];
-            };
-            "bluetooth" = {
-                "format" = "󰂯　{status}";
-                "format-disabled" = ""; # an empty format will hide the module
-                "format-connected" = "󰂯　{num_connections} connected";
-                "tooltip-format" = "{controller_alias}\t{controller_address}";
-                "tooltip-format-connected" = "{controller_alias}\t{controller_address}\n\n{device_enumerate}";
-                "tooltip-format-enumerate-connected" = "{device_alias}\t{device_address}";
-                "on-click" = "overskride";
-            };
-            "pulseaudio" = {
-                "format" = "　{volume}%";
-                 "format-bluetooth" = "{volume}% {icon}󰂯";
-                 "format-muted" = "";
-                 "format-icons"=  {
-                     "headphones" = "󰋋";
-                     "handsfree" = "󰋎";
-                     "headset" = "󰋎";
-                 };
-                "on-click" = "pavucontrol";
-            };
-            "network" = {
-                "format-wifi" = "󰣾　{signalStrength}%";
-                "format-disconnected" = "⚠";
-                "tooltip-format" = "{ifname} via {gwaddr} 󰊗";
-                "tooltip-format-wifi" = "{essid} ({signalStrength}%) 󰣾";
-                "tooltip-format-ethernet" = "{ifname} ";
-                "tooltip-format-disconnected" = "Disconnected";
-                "on-click" = "alacritty -e nmtui";
-            };
-            "power-profiles-daemon" = {
-                "format" = "{icon}";
-                "tooltip-format" = "Power profile: {profile}\nDriver: {driver}";
-                "tooltip" = true;
-                "format-icons" = {
-                    "default" = "󱐌";
-                    "performance" = "󱐌";
-                    "balanced" = "󰗑";
-                    "power-saver" = "󰌪";
-                };
-            };
+      "hyprland/workspaces" = { "all-outputs" = true; };
+      "hyprland/window" = { "max-length" = 100; };
+      "clock" = {
+        "tooltip-format" = "{:%Y-%m-%d | %H:%M}";
+        "interval" = 60;
+        "format" = "{:%I:%M%p}";
+      };
+      "battery" = {
+        "states" = {
+          "critical" = 20;
+          "normal" = 90;
+          "full" = 100;
         };
+        "format" = "{icon}";
+        "format-icons" = [ "󰁺" "󰁼" "󰁿" "󰂁" "󰁹" ];
+      };
+      "bluetooth" = {
+        "format" = "󰂯　{status}";
+        "format-disabled" = ""; # an empty format will hide the module
+        "format-connected" = "󰂯　{num_connections} connected";
+        "tooltip-format" = "{controller_alias}	{controller_address}";
+        "tooltip-format-connected" = ''
+          {controller_alias}	{controller_address}
+
+          {device_enumerate}'';
+        "tooltip-format-enumerate-connected" =
+          "{device_alias}	{device_address}";
+        "on-click" = "overskride";
+      };
+      "pulseaudio" = {
+        "format" = "　{volume}%";
+        "format-bluetooth" = "{volume}% {icon}󰂯";
+        "format-muted" = "";
+        "format-icons" = {
+          "headphones" = "󰋋";
+          "handsfree" = "󰋎";
+          "headset" = "󰋎";
+        };
+        "on-click" = "pavucontrol";
+      };
+      "network" = {
+        "format-wifi" = "󰣾　{signalStrength}%";
+        "format-disconnected" = "⚠";
+        "tooltip-format" = "{ifname} via {gwaddr} 󰊗";
+        "tooltip-format-wifi" = "{essid} ({signalStrength}%) 󰣾";
+        "tooltip-format-ethernet" = "{ifname} ";
+        "tooltip-format-disconnected" = "Disconnected";
+        "on-click" = "alacritty -e nmtui";
+      };
+      "power-profiles-daemon" = {
+        "format" = "{icon}";
+        "tooltip-format" = ''
+          Power profile: {profile}
+          Driver: {driver}'';
+        "tooltip" = true;
+        "format-icons" = {
+          "default" = "󱐌";
+          "performance" = "󱐌";
+          "balanced" = "󰗑";
+          "power-saver" = "󰌪";
+        };
+      };
     };
+  };
 }
