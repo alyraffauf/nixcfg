d4ddbfb4e3d476af2b9d07b8f364821968c2956f
Aly Raffauf <alyraffauf@gmail.com>
2024-03-24 11:41:16 -0400

    hyprland: set cursor and gtk themes to Vanilla-DMZ-AA and breeze

--- home/hypr/default.nix
+++ home/hypr/default.nix
@@ -1,34 +1,48 @@
 { config, pkgs, ... }:
 
 {
-    # Packages that should be installed to the user profile.
-    home.packages = with pkgs; [
-      brightnessctl
-      hyprcursor
-      hypridle
-      hyprlock
-      hyprpaper
-      hyprshade
-      hyprshot
-      playerctl
-      udiskie
-    ];
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    brightnessctl
+    hyprcursor
+    hypridle
+    hyprlock
+    hyprpaper
+    hyprshade
+    hyprshot
+    playerctl
+    udiskie
+  ];
 
-    services.mako = {
-      enable = true;
-      font = "DroidSansM Nerd Font Mono 11";
-      backgroundColor = "#00000080";
-      textColor = "#FFFFFF";
-      borderRadius = 10;
-      defaultTimeout = 10000;
-      padding = "15";
+  wayland.windowManager.hyprland = {
+    enable = true;
+    extraConfig = builtins.readFile ./hyprland.conf;
+  };
+
+  xdg.configFile."hypr/hypridle.conf".source = ./hypridle.conf;
+  xdg.configFile."hypr/hyprlock.conf".source = ./hyprlock.conf;
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    # x11.enable = true;
+    package = pkgs.vanilla-dmz;
+    name = "Vanilla-DMZ-AA";
+    size = 24;
+  };
+
+  gtk = {
+    enable = true;
+    theme = {
+      package = pkgs.kdePackages.breeze-gtk;
+      name = "breeze-gtk";
     };
-
-    wayland.windowManager.hyprland = {
-      enable = true;
-      extraConfig = builtins.readFile ./hyprland.conf;
+    iconTheme = {
+      package = pkgs.kdePackages.breeze-icons;
+      name = "Breeze";
     };
-
-    xdg.configFile."hypr/hypridle.conf".source = ./hypridle.conf;
-    xdg.configFile."hypr/hyprlock.conf".source = ./hyprlock.conf;
+    font = {
+      name = "Sans";
+      size = 11;
+    };
+  };
 }
