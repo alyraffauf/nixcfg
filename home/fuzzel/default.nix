f4ccd95637769d34a184e7a816e22a95ddbf4c0c
Aly Raffauf <alyraffauf@gmail.com>
2024-03-24 15:59:13 -0400

    fuzzel: set border size

--- home/fuzzel/default.nix
+++ home/fuzzel/default.nix
@@ -1,20 +1,23 @@
 { config, pkgs, ... }:
 
 {
-    programs.fuzzel = {
-      enable = true;
-      settings = {
-        main = {
-          font = "Noto Nerd Font Sans Mono";
-          icon-theme = "breeze";
-          layer = "overlay";
-          terminal = "${pkgs.alacritty}/bin/alacritty -e";
-        };
-        colors.background = "#00000099";
-        colors.selection = "#000000FF";
-        colors.selection-match = "#FF0000FF";
-        colors.selection-text = "#008000FF";
-        colors.text = "#FFFFFFFF";
+  programs.fuzzel = {
+    enable = true;
+    settings = {
+      main = {
+        font = "Noto Nerd Font Sans Mono";
+        icon-theme = "breeze";
+        layer = "overlay";
+        terminal = "${pkgs.alacritty}/bin/alacritty -e";
+      };
+      border = { width = 2; };
+      colors = {
+        background = "#00000099";
+        selection = "#000000FF";
+        selection-match = "#FF0000FF";
+        selection-text = "#008000FF";
+        text = "#FFFFFFFF";
       };
     };
+  };
 }
