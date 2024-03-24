311b0acae47beac299eb2d011da5c87b50242c74
Aly Raffauf <alyraffauf@gmail.com>
2024-03-24 19:17:29 -0400

    hyprland: added lightdm slick greeter

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,27 +1,27 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include X settings.
-      ./desktop.nix
-    ];
+  imports = [ # Include X settings.
+    ./desktop.nix
+  ];
 
-    programs.hyprland.enable = true;
-    services.power-profiles-daemon.enable = true;
-    services.upower.enable = true;
+  services.xserver.displayManager.lightdm = {
+    enable = true;
+    greeters.slick.enable = true;
+  };
+  programs.hyprland.enable = true;
+  services.power-profiles-daemon.enable = true;
+  services.upower.enable = true;
 
-    services.gnome.gnome-keyring.enable = true;
+  services.gnome.gnome-keyring.enable = true;
 
-    programs.xfconf.enable = true;
-    programs.thunar = {
-      enable = true;
-      plugins = with pkgs.xfce; [
-        thunar-archive-plugin
-        thunar-volman
-      ];
-    };
-    services.gvfs.enable = true; # Mount, trash, and other functionalities
-    services.tumbler.enable = true; # Thumbnail support for images
+  programs.xfconf.enable = true;
+  programs.thunar = {
+    enable = true;
+    plugins = with pkgs.xfce; [ thunar-archive-plugin thunar-volman ];
+  };
+  services.gvfs.enable = true; # Mount, trash, and other functionalities
+  services.tumbler.enable = true; # Thumbnail support for images
 
-    environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
