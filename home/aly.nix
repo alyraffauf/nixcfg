6827e53ba6dd0a98b9ed523caf5f343ad7b3cdc2
Aly Raffauf <alyraffauf@gmail.com>
2024-03-23 23:49:49 -0400

    home-manager: moved hyprland config to hyprland.nix

--- home/aly.nix
+++ home/aly.nix
@@ -24,37 +24,6 @@
 
     services.syncthing.enable = true;
 
-    wayland.windowManager.hyprland = {
-      enable = true;
-      extraConfig = builtins.readFile ./dotfiles/hyprland.conf;
-    };
-
-    xdg.configFile."hypr/hypridle.conf".source = ./dotfiles/hypridle.conf;
-    programs.waybar.enable = true;
-    programs.waybar.settings = {
-        mainBar = {
-            layer = "top";
-            position = "top";
-            height = 36;
-            output = [
-                "eDP-1"
-                "HDMI-A-1"
-            ];
-            modules-left = [ "hyprland/workspaces" "hyprland/mode" ];
-            modules-center = [ "hyprland/window" ];
-            modules-right = [ "tray" "battery" "clock"];
-
-            "hyprland/workspaces" = {
-                all-outputs = true;
-            };
-            "clock" = {
-                "interval" = 60;
-                "format" = "{:%I:%M}";
-            };
-        };
-    };
-
-
     programs.alacritty = {
         enable = true;
         settings = {
