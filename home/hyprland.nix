06713a2e7c9ba1cf3e9d557fe9dd2370cb1af85d
Aly Raffauf <alyraffauf@gmail.com>
2024-03-24 05:00:08 -0400

    mako: added transparency

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -21,7 +21,7 @@
     services.mako = {
       enable = true;
       font = "DroidSansM Nerd Font Mono 11";
-      backgroundColor = "#000000";
+      backgroundColor = "#00000080";
       textColor = "#FFFFFF";
       borderRadius = 10;
       defaultTimeout = 10000;
