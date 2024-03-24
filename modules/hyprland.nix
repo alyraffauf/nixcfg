448dd78c91cc1f15e2b11895587df15ab8ec924f
Aly Raffauf <alyraffauf@gmail.com>
2024-03-24 02:19:15 -0400

    hyprland: enabled gnome keyring

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -10,5 +10,7 @@
     services.power-profiles-daemon.enable = true;
     services.upower.enable = true;
 
+    services.gnome.gnome-keyring.enable = true;
+
     environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
