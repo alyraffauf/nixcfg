344b7f063679024c436897a4d57bb37044b7abe5
Aly Raffauf <alyraffauf@gmail.com>
2024-03-26 00:40:20 -0400

    hyprland: moved new wallpapers to hypr/wallpapers

--- home/hypr/default.nix
+++ home/hypr/default.nix
@@ -25,8 +25,9 @@
   xdg.configFile."hypr/hyprlock.conf".source = ./hyprlock.conf;
 
   xdg.configFile."hypr/hyprpaper.conf".source = ./hyprpaper.conf;
-  xdg.configFile."hypr/tokyoGreen.jpg".source = ./tokyoGreen.jpg;
-  xdg.configFile."hypr/greenCity.jpg".source = ./greenCity.jpg;
+  xdg.configFile."hypr/wallpapers/tokyoGreen.jpg".source = ./wallpapers/tokyoGreen.jpg;
+  xdg.configFile."hypr/wallpapers/greenCity.jpg".source = ./wallpapers/greenCity.jpg;
+  xdg.configFile."hypr/wallpapers/jr-korpa-9XngoIpxcEo-unsplash.jpg".source = ./wallpapers/jr-korpa-9XngoIpxcEo-unsplash.jpg;
 
   xdg.configFile."hypr/shaders/blue-light-filter.glsl".source =
     ./blue-light-filter.glsl;
