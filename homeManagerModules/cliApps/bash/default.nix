3dbe0eb154084944b43250549cdd30bedb855e6d
Aly Raffauf <alyraffauf@gmail.com>
2024-03-29 08:49:48 -0400

    bash: set options

--- homeManagerModules/cliApps/bash/default.nix
+++ homeManagerModules/cliApps/bash/default.nix
@@ -6,6 +6,18 @@
     programs.bash = {
       enable = true;
       enableCompletion = true;
+      enableVteIntegration = true;
+      shellOptions = [
+        "autocd"
+        "cdspell"
+        "checkjobs"
+        "checkwinsize"
+        "dirspell"
+        "dotglob"
+        "extglob"
+        "globstar"
+        "histappend"
+      ];
       initExtra = ''
         export PS1="[\[$(tput setaf 27)\]\u\[$(tput setaf 135)\]@\[$(tput setaf 45)\]\h:\[$(tput setaf 33)\]\w] \[$(tput sgr0)\]$ "
       '';
