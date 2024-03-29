6df4738bedbccad5ad627b7eef9c4ef3c8a763a5
Aly Raffauf <alyraffauf@gmail.com>
2024-03-28 21:34:08 -0400

    home: added chromium

---
+++ homeManagerModules/guiApps/chromium/default.nix
@@ -0,0 +1,22 @@
+{ pkgs, lib, config, ... }: {
+
+  options = {
+    guiApps.chromium.enable = lib.mkEnableOption "Enables Chromium.";
+  };
+
+  config = lib.mkIf config.guiApps.chromium.enable {
+    programs.chromium = {
+      enable = true;
+      extensions = [
+        { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden
+        { id = "bfogiafebfohielmmehodmfbbebbbpei"; } # Keeper
+        { id = "aomidfkchockcldhbkggjokdkkebmdll"; } # Redirect Path
+        { id = "occjjkgifpmdgodlplnacmkejpdionan"; } # Autoscroll
+        { id = "ioalpmibngobedobkmbhgmadaphocjdn"; } # OneLogin
+        { id = "jldhpllghnbhlbpcmnajkpdmadaolakh"; } # Todoist
+        { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # uBlock Origin
+        { id = "lcbjdhceifofjlpecfpeimnnphbcjgnc"; } # xBrowserSync
+      ];
+    };
+  };
+}
--- homeManagerModules/guiApps/default.nix
+++ homeManagerModules/guiApps/default.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports = [ ./alacritty ./firefox ./fuzzel ./librewolf ./mako ./waybar ];
+  imports = [ ./alacritty ./chromium ./firefox ./fuzzel ./librewolf ./mako ./waybar ];
 
   guiApps.firefox.enable = lib.mkDefault true;
   guiApps.alacritty.enable = lib.mkDefault true;
