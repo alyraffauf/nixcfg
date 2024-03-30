c0e6d57378de1d43e9ad510617f27df6f2f22bc0
Aly Raffauf <alyraffauf@gmail.com>
2024-03-29 21:16:32 -0400

    lavaridge: added hashed password

--- hosts/lavaridge/default.nix
+++ hosts/lavaridge/default.nix
@@ -19,10 +19,16 @@
 
   networking.hostName = "lavaridge"; # Define your hostname.
 
-  services.fwupd.enable = true;
+  services = {
+    fwupd.enable = true;
+    fprintd.package = pkgs.fprintd.overrideAttrs {
+      mesonCheckFlags = [ "--no-suite" "fprintd:TestPamFprintd" ];
+    }; 
+  };
 
-  services.fprintd.package = pkgs.fprintd.overrideAttrs {
-    mesonCheckFlags = [ "--no-suite" "fprintd:TestPamFprintd" ];
+  systemConfig = {
+    plymouth.enable = true;
+    zramSwap.enable = true;
   };
 
   desktopConfig = {
@@ -37,8 +43,7 @@
 
   homeLab.virtualization.enable = true;
 
-  systemConfig.plymouth.enable = true;
-  systemConfig.zramSwap.enable = true;
+  users.users.aly.hashedPassword = "$y$j9T$O9NgTk6iRfh3mxiRDvfdm1$BjvsAKYEMB3C28652FF15cj/i.3TgQAObQvR0rN1E6C";
 
   system.stateVersion = "23.11"; # Did you read the comment?
 }
