a07c9ffe2ca377e2ad934ad9885cc1f67994e344
Aly Raffauf <alyraffauf@gmail.com>
2024-03-30 22:49:17 -0400

    home/zoom-us: fixed wrong package in zoom module

--- homeManagerModules/guiApps/zoom/default.nix
+++ homeManagerModules/guiApps/zoom/default.nix
@@ -3,6 +3,6 @@
   options = { guiApps.zoom.enable = lib.mkEnableOption "Enables Zoom."; };
 
   config = lib.mkIf config.guiApps.zoom.enable {
-    home.packages = with pkgs; [ webcord ];
+    home.packages = with pkgs; [ zoom-us ];
   };
 }
