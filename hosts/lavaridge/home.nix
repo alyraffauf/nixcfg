9a7cf30cc04cc1b6273bf78f2041f711d59ef3e8
Aly Raffauf <alyraffauf@gmail.com>
2024-03-31 13:21:46 -0400

    lavaridge: automatically apply framework13 easy effects preset

--- hosts/lavaridge/home.nix
+++ hosts/lavaridge/home.nix
@@ -8,5 +8,10 @@
 
     home.stateVersion = "23.11";
     programs.home-manager.enable = true;
+
+    userServices.easyeffects = {
+      enable = true;
+      preset = "framework13.json";
+    };
   };
 }
