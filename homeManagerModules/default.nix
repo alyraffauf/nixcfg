af336197c48008170f4057fcbfa3e60c64790906
Aly Raffauf <alyraffauf@gmail.com>
2024-04-02 23:00:13 -0400

    home-manager: added xdg userDirs

--- homeManagerModules/default.nix
+++ homeManagerModules/default.nix
@@ -10,4 +10,15 @@
       allowUnfree = true;
     };
   };
+  
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    documents = "${config.home.homeDirectory}/docs";
+    download = "${config.home.homeDirectory}/downloads";
+    music = "${config.home.homeDirectory}/music";
+    videos = "${config.home.homeDirectory}/vids";
+    pictures = "${config.home.homeDirectory}/pics";
+    extraConfig = { XDG_SRC_DIR = "${config.home.homeDirectory}/src"; };
+  };
 }
