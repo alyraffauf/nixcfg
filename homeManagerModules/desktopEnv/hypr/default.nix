4019f069aaf36aa19843571a1f3c513854a625f7
Aly Raffauf <alyraffauf@gmail.com>
2024-04-03 21:12:07 -0400

    hyprland: add celluloid

--- homeManagerModules/desktopEnv/hypr/default.nix
+++ homeManagerModules/desktopEnv/hypr/default.nix
@@ -28,6 +28,7 @@
     # Packages that should be installed to the user profile.
     home.packages = with pkgs; [
       brightnessctl
+      celluloid
       evince
       hyprcursor
       hyprland-protocols
