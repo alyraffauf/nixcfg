ae022a2801a19283a186aa9b18f5258094ec28fb
Aly Raffauf <alyraffauf@gmail.com>
2024-04-03 21:33:04 -0400

    home: expanded xdg userdirs

--- homeManagerModules/default.nix
+++ homeManagerModules/default.nix
@@ -14,12 +14,14 @@
   xdg.userDirs = {
     enable = true;
     createDirectories = true;
-    desktop = "${config.home.homeDirectory}/desktop";
+    desktop = "${config.home.homeDirectory}/dsktp";
     documents = "${config.home.homeDirectory}/docs";
-    download = "${config.home.homeDirectory}/downloads";
+    download = "${config.home.homeDirectory}/dwnlds";
     music = "${config.home.homeDirectory}/music";
     videos = "${config.home.homeDirectory}/vids";
     pictures = "${config.home.homeDirectory}/pics";
+    publicShare = "${config.home.homeDirectory}/pub";
+    templates = "${config.home.homeDirectory}/tmplts";
     extraConfig = { XDG_SRC_DIR = "${config.home.homeDirectory}/src"; };
   };
 }
