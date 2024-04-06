cbb61849d5e0b0c9b5057674847bec70c6a951e7
Aly Raffauf <alyraffauf@gmail.com>
2024-04-06 12:12:40 -0400

    petalburg: add hashed passwd

--- hosts/petalburg/home.nix
+++ hosts/petalburg/home.nix
@@ -1,6 +1,8 @@
 { inputs, config, pkgs, lib, ... }:
 
 {
+  users.users.aly.hashedPassword =
+    "$y$j9T$Ug0ZLHQQuRciFJDgOI6r00$eHc.KyQY0oU4k0LKRiZiGWJ19jkKNWHpOoyCJbtJif8";
   home-manager.users.aly = {
     imports = [ ../../homeManagerModules ];
     home.username = "aly";
