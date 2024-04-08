1757bb6961e866f2ccf30d371fa554fcd8b930a4
Aly Raffauf <alyraffauf@gmail.com>
2024-04-07 22:11:39 -0400

    lavaridge: added radv and amdvulkan drivers

--- hosts/lavaridge/hardware-configuration.nix
+++ hosts/lavaridge/hardware-configuration.nix
@@ -9,7 +9,7 @@
   boot.initrd.availableKernelModules =
     [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ "kvm-amd" "amd-gpu" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
@@ -23,6 +23,18 @@
   };
 
   swapDevices = [ ];
+
+  hardware.opengl = {
+    ## radv: an open-source Vulkan driver from freedesktop
+    driSupport = true;
+    driSupport32Bit = true;
+
+    ## amdvlk: an open-source Vulkan driver from AMD
+    extraPackages = [ pkgs.rocmPackages.clr.icd pkgs.amdvlk ];
+    extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
+  };
+
+  services.xserver.videoDrivers = [ "amdgpu" ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
