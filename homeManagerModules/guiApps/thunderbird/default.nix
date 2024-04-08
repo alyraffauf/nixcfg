36fc9b1c50c113b45cc886f1dac75da48d7f043b
Aly Raffauf <alyraffauf@gmail.com>
2024-04-07 23:54:15 -0400

    home: fixed broken thunderbird option

--- homeManagerModules/guiApps/thunderbird/default.nix
+++ homeManagerModules/guiApps/thunderbird/default.nix
@@ -7,6 +7,6 @@
   options = {guiApps.thunderbird.enable = lib.mkEnableOption "Enable Thunderbird.";};
 
   config = lib.mkIf config.guiApps.thunderbird.enable {
-    programs.thunderbird = {enable = true;};
+    home.packages = [pkgs.thunderbird];
   };
 }
