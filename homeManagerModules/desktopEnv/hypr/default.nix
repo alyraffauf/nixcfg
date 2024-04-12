b31fa15b5ca6133448345b50f39ba8bd6e81e0e6
Aly Raffauf <alyraffauf@gmail.com>
2024-04-12 08:00:50 -0400

    hyprland: added work monitor

--- homeManagerModules/desktopEnv/hypr/default.nix
+++ homeManagerModules/desktopEnv/hypr/default.nix
@@ -132,6 +132,7 @@
       monitor = desc:Samsung Display Corp. 0x4152,preferred,auto,2,transform,0 # petalburg
       monitor = desc:Guangxi Century Innovation Display Electronics Co. Ltd 27C1U-D 0000000000001,preferred,-2560x0,1.5 # workshop
       monitor = desc:LG Electronics LG IPS QHD 109NTWG4Y865,preferred,-2560x0,auto
+      monitor = desc:HP Inc. HP 24mh 3CM037248S,preferred,-1920x0,auto
       monitor = ,preferred,auto,auto
 
         # unscale XWayland apps
