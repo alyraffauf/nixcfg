4992fec1506d6c143c791e0baa7c8b0d82cb78f3
Aly Raffauf <alyraffauf@gmail.com>
2024-04-13 23:15:26 -0400

    sway: updated config

--- homeManagerModules/desktopEnv/sway/default.nix
+++ homeManagerModules/desktopEnv/sway/default.nix
@@ -16,14 +16,22 @@
     guiApps.wlogout.enable = lib.mkDefault true;
     guiApps.alacritty.enable = lib.mkDefault true;
     guiApps.firefox.enable = lib.mkDefault true;
+    guiApps.kanshi.enable = lib.mkDefault true;
 
     # Packages that should be installed to the user profile.
     home.packages = with pkgs; [
-      brightnessctl
+      # brightnessctl
+      # hyprnome
+      celluloid
       evince
+      gnome.eog
+      gnome.file-roller
+      kdePackages.polkit-kde-agent-1
+      networkmanagerapplet
       playerctl
+      swayosd
+      trayscale
       xfce.exo
-      xfce.ristretto
       xfce.thunar
       xfce.thunar-archive-plugin
       xfce.thunar-media-tags-plugin
@@ -32,9 +40,12 @@
       xfce.xfce4-settings
       xfce.xfce4-taskmanager
       xfce.xfconf
+      swayidle
     ];
 
-    services.cliphist.enable = true;
+    programs.swaylock.enable = lib.mkDefault true;
+
+    services.cliphist.enable = lib.mkDefault true;
 
     wayland.windowManager.sway.enable = true;
     wayland.windowManager.sway.config = {
@@ -43,6 +54,57 @@
       terminal = "${pkgs.alacritty}/bin/alacritty";
       menu = "${pkgs.fuzzel}/bin/fuzzel";
       modifier = "Mod4";
+      colors.focused = {
+        background = "#ca9ee6";
+        border = "#ca9ee6";
+        childBorder = "#ca9ee6";
+        indicator = "#ca9ee6";
+        text = "#ffffff";
+      };
+      colors.focusedInactive = {
+        background = "#99d1db";
+        border = "#99d1db";
+        childBorder = "#99d1db";
+        indicator = "#99d1db";
+        text = "#ffffff";
+      };
+      colors.unfocused = {
+        background = "#99d1db";
+        border = "#99d1db";
+        childBorder = "#99d1db";
+        indicator = "#99d1db";
+        text = "#ffffff";
+      };
+      gaps.inner = 5;
+      gaps.outer = 10;
+      window.titlebar = false;
+      fonts = {
+        names = ["Noto SansM Nerd Font"];
+        style = "Bold";
+        size = 12.0;
+      };
+      startup = [
+        # { command = "${pkgs.kanshi}"; }
+        {command = "nm-applet";}
+        {command = "swayosd-server";}
+        {command = "thunar --daemon";}
+        {command = "${pkgs.swayidle} -w timeout 300 '${pkgs.swaylock}' before-sleep '${pkgs.swaylock}'";}
+      ];
+      output = {
+        "BOE 0x095F Unknown" = {
+          scale = "1.5";
+        };
+      };
+      input = {
+        "type:touchpad" = {
+          click_method = "clickfinger";
+          dwt = "enabled";
+          natural_scroll = "enabled";
+          scroll_method = "two_finger";
+          tap = "enabled";
+          tap_button_map = "lrm";
+        };
+      };
     };
 
     xdg.configFile."xfce4/helpers.rc".text = ''
