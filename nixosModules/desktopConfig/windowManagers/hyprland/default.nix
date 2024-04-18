f46827b48a16d181fad2ee2f19bc79e42b3ee977
Aly Raffauf <aly@raffauflabs.com>
2024-04-17 20:17:17 -0400

    hyprland: enable kwallet and gnome keyring on login with greetd

--- nixosModules/desktopConfig/windowManagers/hyprland/default.nix
+++ nixosModules/desktopConfig/windowManagers/hyprland/default.nix
@@ -11,8 +11,6 @@
   };
 
   config = lib.mkIf config.desktopConfig.windowManagers.hyprland.enable {
-    desktopConfig.displayManagers.lightdm.enable = lib.mkDefault false;
-
     services.greetd = {
       enable = true;
       settings = rec {
@@ -22,11 +20,19 @@
       };
     };
 
+    security.pam.services.greetd.enableKwallet = true;
+    security.pam.services.greetd.enableGnomeKeyring = true;
+
     programs.hyprland.enable = true;
     programs.hyprland.package =
       inputs.hyprland.packages.${pkgs.system}.hyprland;
 
     programs.gnupg.agent.pinentryPackage = pkgs.pinentry-gnome3;
+
+    services.dbus.packages = [pkgs.gcr];
+    services.udev.packages = [pkgs.swayosd];
+
+    environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
     nix.settings = {
       substituters = ["https://hyprland.cachix.org"];
@@ -34,10 +40,5 @@
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
-
-    services.dbus.packages = [pkgs.gcr];
-    services.udev.packages = [pkgs.swayosd];
-
-    environment.sessionVariables.NIXOS_OZONE_WL = "1";
   };
 }
