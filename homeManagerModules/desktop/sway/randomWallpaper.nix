55f5b5075493cefebde4ff671b2897d456f8ce5e
Aly Raffauf <aly@raffauflabs.com>
2024-04-26 16:52:36 -0400

    sway: cleaned up random wallpaper script

--- homeManagerModules/desktop/sway/randomWallpaper.nix
+++ homeManagerModules/desktop/sway/randomWallpaper.nix
@@ -12,14 +12,13 @@
 
     if [ -d "$directory" ]; then
         while true; do
-
           for pid in ''${OLD_PIDS[@]}; do
             kill $pid
           done
+
           OLD_PIDS=()
 
           monitor=`${config.wayland.windowManager.sway.package}/bin/swaymsg -t get_outputs -p | grep Output | awk '{print $2}'`
-          
           for m in ''${monitor[@]}; do
             random_background=$(ls $directory/*.{png,jpg} | shuf -n 1)
             ${pkgs.swaybg}/bin/swaybg -o $m -i $random_background &
