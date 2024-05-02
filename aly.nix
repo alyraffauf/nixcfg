72d2fe226933e379814d08c625e7aeb84f6468cd
Aly Raffauf <aly@raffauflabs.com>
2024-05-02 05:49:39 -0400

    aly: add chromium to work workspace

--- aly.nix
+++ aly.nix
@@ -63,7 +63,7 @@
     "workspace 1: web" = [{app_id = "firefox";}];
     "workspace 2: code" = [{app_id = "codium-url-handler";}];
     "workspace 3: chat" = [{app_id = "org.gnome.Fractal";} {app_id = "WebCord";}];
-    "workspace 4: work" = [{app_id = "google-chrome";}];
+    "workspace 4: work" = [ {app_id = "google-chrome";} {app_id = "chromium-browser";}];
     "workspace 10: zoom" = [{class = "zoom";} {app_id = "Zoom";}];
   };
   wayland.windowManager.hyprland.extraConfig = ''
