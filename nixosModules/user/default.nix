5694a9ef6ce4787cab75e3bd46c8a24c2e5baa4e
Aly Raffauf <alyraffauf@gmail.com>
2024-05-08 21:54:29 -0400

    ssh: updated rustboro pub key

--- nixosModules/user/default.nix
+++ nixosModules/user/default.nix
@@ -19,7 +19,7 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBK+QkM3C98BxnJtcEOuxjT7bbUG8gsUafrzW9uKuxz aly@petalburg"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINHdpGTfjmnnau18CowChY4hPn/fzRkgJvXFs+yPy74I aly@mauville"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEREYTfh+b8XKcfTLpENqRzzWQLSkXCZIKGUFTlmJPy6 aly@lavaridge"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOz2Lvy5WB/fCJnbK4eY2MY4CTYkKqQzUwRkXWiipHSc aly@rustboro"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLAo1qjO7vDiaNzo17haSFERSqMPELHufCfCNhOb9lM aly@rustboro"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGcJBb7+ZxkDdk06A0csNsbgT9kARUN185M8k3Lq7E/d u0_a336@localhost" # termux on winona
   ];
 }
