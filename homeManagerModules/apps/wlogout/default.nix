7654e4069ccf998a9e49ab88cb5b9bc2f50cd983
Aly Raffauf <aly@raffauflabs.com>
2024-05-11 17:45:01 -0400

    wlogout: new theme

--- homeManagerModules/apps/wlogout/default.nix
+++ homeManagerModules/apps/wlogout/default.nix
@@ -13,22 +13,77 @@
         {
           label = "logout";
           action = ''${lib.getExe' pkgs.systemd "loginctl"} terminate-user ${config.home.username}'';
-          text = "Logout";
+          text = "logout (e)";
           keybind = "e";
         }
         {
           label = "shutdown";
           action = ''${lib.getExe' pkgs.systemd "systemctl"} poweroff'';
-          text = "Shutdown";
+          text = "shutdown (s)";
           keybind = "s";
         }
         {
           label = "reboot";
           action = ''${lib.getExe' pkgs.systemd "systemctl"} reboot'';
-          text = "Reboot";
+          text = "reboot (r)";
           keybind = "r";
         }
       ];
+      style = ''
+        * {
+          font-family: "${config.alyraffauf.desktop.theme.font.name}", sans-serif;
+        	background-image: none;
+        	box-shadow: none;
+          transition: 20ms;
+        }
+
+        window {
+        	background-color: rgba(12, 12, 12, 0.0);
+        }
+
+        button {
+        	text-decoration-color: #FAFAFA;
+          color: #FAFAFA;
+        	background-color: ${config.alyraffauf.desktop.theme.colors.background};
+          background-color: rgba(12, 12, 12, 0.0);
+        	background-repeat: no-repeat;
+        	background-size: 10%;
+          border-radius: 10;
+        }
+
+        button:active, button:hover {
+        	background-color: ${config.alyraffauf.desktop.theme.colors.primary};
+        	outline-style: none;
+        }
+
+        #lock, #logout, #suspend, #hibernate, #shutdown, #reboot {
+          padding: 0;
+        }
+
+        #lock {
+          background-image: image(url("${config.programs.wlogout.package}/share/wlogout/icons/lock.png"), url("/usr/local/share/wlogout/icons/lock.png"));
+        }
+
+        #logout {
+          background-image: image(url("${config.programs.wlogout.package}/share/wlogout/icons/logout.png"), url("/usr/local/share/wlogout/icons/logout.png"));
+        }
+
+        #suspend {
+          background-image: image(url("${config.programs.wlogout.package}/share/wlogout/icons/suspend.png"), url("/usr/local/share/wlogout/icons/suspend.png"));
+        }
+
+        #hibernate {
+          background-image: image(url("${config.programs.wlogout.package}/share/wlogout/icons/hibernate.png"), url("/usr/local/share/wlogout/icons/hibernate.png"));
+        }
+
+        #shutdown {
+          background-image: image(url("${config.programs.wlogout.package}/share/wlogout/icons/shutdown.png"), url("/usr/local/share/wlogout/icons/shutdown.png"));
+        }
+
+        #reboot {
+          background-image: image(url("${config.programs.wlogout.package}/share/wlogout/icons/reboot.png"), url("/usr/local/share/wlogout/icons/reboot.png"));
+        }
+      '';
     };
   };
 }
