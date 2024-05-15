294fd9fa9ccb6276259aca5c023354d0e6992ef7
Aly Raffauf <aly@raffauflabs.com>
2024-05-15 15:31:02 -0400

    mako: make colors and border more consistent with windowmanager themes

--- homeManagerModules/apps/mako/default.nix
+++ homeManagerModules/apps/mako/default.nix
@@ -4,14 +4,15 @@
   config,
   ...
 }: {
-  options = {alyraffauf.apps.mako.enable = lib.mkEnableOption "Enables mako.";};
+  options = {alyraffauf.apps.mako.enable = lib.mkEnableOption "Mako notification daemon.";};
 
   config = lib.mkIf config.alyraffauf.apps.mako.enable {
     services.mako = {
       enable = true;
       anchor = "top-center";
       backgroundColor = "${config.alyraffauf.desktop.theme.colors.background}CC";
-      borderColor = "${config.alyraffauf.desktop.theme.colors.primary}";
+      borderColor = "${config.alyraffauf.desktop.theme.colors.primary}EE";
+      borderSize = 2;
       borderRadius = 10;
       defaultTimeout = 10000;
       font = "${config.alyraffauf.desktop.theme.font.name} Regular ${toString config.alyraffauf.desktop.theme.font.size}";
