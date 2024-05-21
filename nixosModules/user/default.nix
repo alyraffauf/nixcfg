a828d154a400cf2449a02a5cf6833ee8b6592d2c
Aly Raffauf <aly@raffauflabs.com>
2024-05-21 19:29:40 -0400

    lavaridge: updated ssh key

--- nixosModules/user/default.nix
+++ nixosModules/user/default.nix
@@ -18,7 +18,7 @@
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBK+QkM3C98BxnJtcEOuxjT7bbUG8gsUafrzW9uKuxz aly@petalburg"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINHdpGTfjmnnau18CowChY4hPn/fzRkgJvXFs+yPy74I aly@mauville"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEREYTfh+b8XKcfTLpENqRzzWQLSkXCZIKGUFTlmJPy6 aly@lavaridge"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA5xWjZIdMQaQE7vyPP7VRAKNHbrFeh0QtF3bAXni66V aly@lavaridge"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLAo1qjO7vDiaNzo17haSFERSqMPELHufCfCNhOb9lM aly@rustboro"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGcJBb7+ZxkDdk06A0csNsbgT9kARUN185M8k3Lq7E/d u0_a336@localhost" # termux on winona
   ];
