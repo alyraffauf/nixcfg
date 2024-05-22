390a6f438118bb006946a3cd4fc22aa463bd6842
Aly Raffauf <alyraffauf@gmail.com>
2024-05-22 16:48:18 -0400

    mauville: add systemd service for backblaze backups

--- hosts/mauville/home.nix
+++ hosts/mauville/home.nix
@@ -14,7 +14,30 @@
         alyraffauf.desktop.sway.autoSuspend = false;
       }
     ];
-    users.aly = import ../../aly.nix;
+    users.aly = {
+      imports = [../../aly.nix];
+      systemd.user.services.backblaze-sync = {
+        Unit = {
+          Description = "Push Syncthing folers to Backblaze.";
+        };
+        Install = {
+          WantedBy = ["default.target"];
+        };
+        Service = {
+          ExecStart = "${pkgs.writeShellScript "backblaze-sync" ''
+          #!/run/current-system/sw/bin/bash
+
+          BACKBLAZE=${lib.getExe pkgs.backblaze-b2}
+
+          $BACKBLAZE sync --delete /mnt/Media/Music b2://aly-music
+          $BACKBLAZE sync --delete /mnt/Media/Audiobooks b2://aly-audiobooks
+          $BACKBLAZE sync --delete /mnt/Archive/Archive b2://aly-archive
+
+          $BACKBLAZE sync --delete /home/aly/sync b2://aly-sync
+          ''}";
+        };
+      };
+    };
     users.dustin = import ../../dustin.nix;
   };
 }
