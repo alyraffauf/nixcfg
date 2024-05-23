40020eef877557e6c50e9b7ee920932ade0ec52c
Aly Raffauf <aly@raffauflabs.com>
2024-05-22 20:27:08 -0400

    mauville: add timer for backblaze-sync

--- hosts/mauville/home.nix
+++ hosts/mauville/home.nix
@@ -16,24 +16,38 @@
     ];
     users.aly = {
       imports = [../../aly.nix];
-      systemd.user.services.backblaze-sync = {
-        Unit = {
-          Description = "Push Syncthing folders to Backblaze.";
-        };
-        Install = {
-          WantedBy = ["default.target"];
-        };
-        Service = {
-          ExecStart = "${pkgs.writeShellScript "backblaze-sync" ''
-            BACKBLAZE=${lib.getExe pkgs.backblaze-b2}
+      systemd.user = {
+        services.backblaze-sync = {
+          Unit = {
+            Description = "Backup to Backblaze.";
+            After = ["network.target"];
+          };
+          Install = {
+            WantedBy = ["default.target"];
+          };
+          Service = {
+            ExecStart = "${pkgs.writeShellScript "backblaze-sync" ''
+              BACKBLAZE=${lib.getExe pkgs.backblaze-b2}
 
-            $BACKBLAZE sync --delete /mnt/Media/Music b2://aly-music
-            $BACKBLAZE sync --delete /mnt/Media/Audiobooks b2://aly-audiobooks
-            $BACKBLAZE sync --delete /mnt/Archive/Archive b2://aly-archive
+              $BACKBLAZE sync --delete /mnt/Media/Music b2://aly-music
+              $BACKBLAZE sync --delete /mnt/Media/Audiobooks b2://aly-audiobooks
+              $BACKBLAZE sync --delete /mnt/Archive/Archive b2://aly-archive
 
-            $BACKBLAZE sync --delete /home/aly/sync b2://aly-sync
-            $BACKBLAZE sync --delete /home/aly/pics/camera b2://aly-camera
-          ''}";
+              $BACKBLAZE sync --delete /home/aly/sync b2://aly-sync
+              $BACKBLAZE sync --delete /home/aly/pics/camera b2://aly-camera
+            ''}";
+          };
+        };
+        timers.backblaze-sync = {
+          Unit = {
+            Description = "Daily backups to Backblaze.";
+          };
+          Install = {
+            WantedBy = ["timers.target"];
+          };
+          Timer = {
+            OnCalendar = "*-*-* 03:00:00";
+          };
         };
       };
     };
