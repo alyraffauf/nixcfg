ec2ef98ab504ee729e3ac404af18720a9b07715f
Aly Raffauf <aly@raffauflabs.com>
2024-05-26 14:58:38 -0400

    lavaridge: hyprland by default

--- hosts/lavaridge/default.nix
+++ hosts/lavaridge/default.nix
@@ -45,7 +45,7 @@
       enable = true;
       greetd = {
         enable = true;
-        session = config.programs.sway.package + "/bin/sway";
+        session = lib.getExe config.programs.hyprland.package;
         autologin = {
           enable = true;
           user = "aly";
