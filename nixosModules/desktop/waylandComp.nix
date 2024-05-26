1a86afa3af7d734705c906be0f7ee9df38183d60
Aly Raffauf <aly@raffauflabs.com>
2024-05-26 14:09:10 -0400

    swaylock: enable fingerprint login with password fallback

--- nixosModules/desktop/waylandComp.nix
+++ nixosModules/desktop/waylandComp.nix
@@ -20,7 +20,25 @@
     };
 
     security.pam.services = {
-      swaylock = {};
+      swaylock = {
+        text = ''
+          # Account management.
+          account required pam_unix.so # unix (order 10900)
+
+          # Authentication management.
+          auth sufficient pam_unix.so likeauth try_first_pass likeauth nullok # unix (order 11500)
+          ${if config.services.fprintd.enable then "auth sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so # fprintd (order 11300)" else ""}
+          
+          auth required pam_deny.so # deny (order 12300)
+
+          # Password management.
+          password sufficient pam_unix.so nullok yescrypt # unix (order 10200)
+
+          # Session management.
+          session required pam_env.so conffile=/etc/pam/environment readenv=0 # env (order 10100)
+          session required pam_unix.so # unix (order 10200)
+        '';
+      };
     };
 
     programs = {
