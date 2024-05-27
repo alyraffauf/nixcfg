5f8fcbd29b79f2c09b0d39549588c183eebb47f4
Aly Raffauf <aly@raffauflabs.com>
2024-05-26 20:51:32 -0400

    hyprland: decrease gaps_out 10 -> 7

--- homeManagerModules/desktop/hyprland/default.nix
+++ homeManagerModules/desktop/hyprland/default.nix
@@ -195,7 +195,7 @@
 
       general {
           gaps_in = 5
-          gaps_out = 10
+          gaps_out = 7
           border_size = 2
           col.active_border = rgba(${lib.strings.removePrefix "#" config.alyraffauf.desktop.theme.colors.secondary}EE) rgba(${lib.strings.removePrefix "#" config.alyraffauf.desktop.theme.colors.primary}EE) 45deg
           col.inactive_border = rgba(${lib.strings.removePrefix "#" config.alyraffauf.desktop.theme.colors.inactive}AA)
