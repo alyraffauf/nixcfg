e03e94d36d80c4bcbcabadfcfc3f1ca3ce1f6c7b
Aly Raffauf <alyraffauf@gmail.com>
2024-06-05 14:49:58 -0400

    mauville: use light theme

--- hosts/mauville/home.nix
+++ hosts/mauville/home.nix
@@ -11,9 +11,62 @@
   home-manager = {
     sharedModules = [
       {
+        programs.vscode.userSettings = {
+          "editor.fontSize" = "16";
+        };
         xdg.userDirs.music = "/mnt/Media/Music";
         alyraffauf.desktop.hyprland.autoSuspend = false;
         alyraffauf.desktop.sway.autoSuspend = false;
+        alyraffauf.desktop.theme = lib.mkForce {
+          enable = true;
+          gtk = {
+            name = "Catppuccin-Latte-Compact-Mauve-Light";
+            package = pkgs.catppuccin-gtk.override {
+              accents = ["mauve"];
+              size = "compact";
+              variant = "latte";
+              tweaks = ["normal"];
+            };
+          };
+          qt = {
+            name = "Catppuccin-Latte-Mauve";
+            package = pkgs.catppuccin-kvantum.override {
+              accent = "Mauve";
+              variant = "Latte";
+            };
+          };
+          iconTheme = {
+            name = "Papirus-Light";
+            package = pkgs.catppuccin-papirus-folders.override {
+              flavor = "latte";
+              accent = "mauve";
+            };
+          };
+          cursorTheme = {
+            name = "Catppuccin-Latte-Dark-Cursors";
+            size = 32;
+            package = pkgs.catppuccin-cursors.latteDark;
+          };
+          font = {
+            name = "NotoSans Nerd Font";
+            size = 14;
+            package = pkgs.nerdfonts.override {fonts = ["Noto"];};
+          };
+          terminalFont = {
+            name = "NotoSansM Nerd Font";
+            size = 14;
+            package = pkgs.nerdfonts.override {fonts = ["Noto"];};
+          };
+          colors = {
+            preferDark = false;
+            text = "#4c4f69";
+            background = "#eff1f5";
+            primary = "#8839ef";
+            secondary = "#04a5e5";
+            inactive = "#626880";
+            shadow = "#1A1A1A";
+          };
+        };
       }
     ];
     users.aly = {
