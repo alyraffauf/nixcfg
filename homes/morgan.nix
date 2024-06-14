d819e646a0511d792decc8b7b3a6a8cdbc171362
Aly Raffauf <aly@raffauflabs.com>
2024-06-14 13:54:53 -0400

    home: add morgan user config

---
+++ homes/morgan.nix
@@ -0,0 +1,93 @@
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  self,
+  ...
+}: {
+  home = {
+    username = "morgan";
+    homeDirectory = "/home/morgan";
+    stateVersion = "24.05";
+    packages = with pkgs; [
+      fractal
+      gnome.file-roller
+      libreoffice-fresh
+      vlc
+      webcord
+      xfce.xfce4-taskmanager
+    ];
+  };
+
+  programs.home-manager.enable = true;
+
+  alyraffauf = {
+    apps = {
+      alacritty.enable = true;
+      bash.enable = true;
+      chromium.enable = true;
+      eza.enable = true;
+      firefox.enable = true;
+      fzf.enable = true;
+      vsCodium.enable = true;
+    };
+    defaultApps.enable = true;
+    desktop = {
+      hyprland = {
+        enable = true;
+        randomWallpaper = true;
+      };
+    };
+    theme = {
+      enable = true;
+      gtk = {
+        name = "Catppuccin-Frappe-Compact-Mauve-Dark";
+        package = pkgs.catppuccin-gtk.override {
+          accents = ["mauve"];
+          size = "compact";
+          variant = "frappe";
+          tweaks = ["normal"];
+        };
+      };
+      qt = {
+        name = "Catppuccin-Frappe-Mauve";
+        package = pkgs.catppuccin-kvantum.override {
+          accent = "Mauve";
+          variant = "Frappe";
+        };
+      };
+      iconTheme = {
+        name = "Papirus-Dark";
+        package = pkgs.catppuccin-papirus-folders.override {
+          flavor = "frappe";
+          accent = "mauve";
+        };
+      };
+      cursorTheme = {
+        name = "Catppuccin-Frappe-Dark-Cursors";
+        size = 24;
+        package = pkgs.catppuccin-cursors.frappeDark;
+      };
+      font = {
+        name = "NotoSans Nerd Font";
+        size = 11;
+        package = pkgs.nerdfonts.override {fonts = ["Noto"];};
+      };
+      terminalFont = {
+        name = "NotoSansM Nerd Font";
+        size = 11;
+        package = pkgs.nerdfonts.override {fonts = ["Noto"];};
+      };
+      colors = {
+        text = "#FAFAFA";
+        background = "#232634";
+        primary = "#CA9EE6";
+        secondary = "#99D1DB";
+        inactive = "#626880";
+        shadow = "#1A1A1A";
+      };
+      wallpaper = "${config.xdg.dataHome}/backgrounds/jr-korpa-9XngoIpxcEo-unsplash.jpg";
+    };
+  };
+}
