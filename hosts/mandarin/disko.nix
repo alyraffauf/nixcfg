0197650237721c5e16b6a8458863b33c7a75e780
Aly Raffauf <aly@raffauflabs.com>
2024-06-14 19:31:27 -0400

    mandarin: updated install disk

--- hosts/mandarin/disko.nix
+++ hosts/mandarin/disko.nix
@@ -3,7 +3,7 @@
     disk = {
       vdb = {
         type = "disk";
-        device = "/dev/nvme0n1";
+        device = "/dev/nvme1n1";
         content = {
           type = "gpt";
           partitions = {
