1c04af0471c156b98ce43f7efee12f006f380392
Aly Raffauf <alyraffauf@gmail.com>
2024-06-14 22:47:46 -0400

    mauville: use catppuccin-machiato desktop themes

--- hosts/mauville/home.nix
+++ hosts/mauville/home.nix
@@ -8,15 +8,61 @@
   home-manager = {
     sharedModules = [
       {
+        programs.vscode.userSettings = {
+          "workbench.colorTheme" = lib.mkForce "Catppuccin Macchiato";
+          "workbench.iconTheme" = lib.mkForce "catppuccin-macchiato";
+        };
+
         xdg.userDirs.music = "/mnt/Media/Music";
         alyraffauf = {
           desktop = {
             hyprland.autoSuspend = false;
             sway.autoSuspend = false;
           };
+          theme = lib.mkForce {
+            enable = true;
+            gtk = {
+              name = "Catppuccin-Macchiato-Compact-Mauve-Dark";
+              package = pkgs.catppuccin-gtk.override {
+                accents = ["mauve"];
+                size = "compact";
+                variant = "macchiato";
+                tweaks = ["normal"];
+              };
+            };
+            qt = {
+              name = "Catppuccin-Macchiato-Mauve";
+              package = pkgs.catppuccin-kvantum.override {
+                accent = "Mauve";
+                variant = "Macchiato";
+              };
+            };
+            iconTheme = {
+              name = "Papirus-Light";
+              package = pkgs.catppuccin-papirus-folders.override {
+                flavor = "macchiato";
+                accent = "mauve";
+              };
+            };
+            cursorTheme = {
+              name = "Catppuccin-Macchiato-Dark-Cursors";
+              size = 32;
+              package = pkgs.catppuccin-cursors.macchiatoDark;
+            };
+            colors = {
+              preferDark = true;
+              text = "#4c4f69";
+              background = "#eff1f5";
+              primary = "#8839ef";
+              secondary = "#04a5e5";
+              inactive = "#626880";
+              shadow = "#1A1A1A";
+            };
+          };
         };
       }
     ];
+
     users.aly = {
       imports = [../../homes/aly.nix];
       systemd.user = {
