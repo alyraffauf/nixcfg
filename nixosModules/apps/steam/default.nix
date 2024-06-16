7e42bde3db5cea17727cb9b3540e621dea63d8eb
Aly Raffauf <aly@raffauflabs.com>
2024-06-15 20:21:34 -0400

    steam: add gamescope support

--- nixosModules/apps/steam/default.nix
+++ nixosModules/apps/steam/default.nix
@@ -6,12 +6,16 @@
 }: {
   config = lib.mkIf config.alyraffauf.apps.steam.enable {
     hardware.steam-hardware.enable = true;
-    programs.steam = {
-      enable = true;
-      remotePlay.openFirewall =
-        true; # Open ports in the firewall for Steam Remote Play
-      dedicatedServer.openFirewall =
-        true; # Open ports in the firewall for Source Dedicated Server
+    programs = {
+      gamescope.enable = true;
+      steam = {
+        enable = true;
+        gamescopeSession.enable = true;
+        remotePlay.openFirewall =
+          true; # Open ports in the firewall for Steam Remote Play
+        dedicatedServer.openFirewall =
+          true; # Open ports in the firewall for Source Dedicated Server
+      };
     };
   };
 }
