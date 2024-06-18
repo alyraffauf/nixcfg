9f10655a6c1a03fa3ce8a9f3bfe28cda1b774370
Aly Raffauf <aly@raffauflabs.com>
2024-06-18 19:16:35 -0400

    rustboro: add samba share automounts

--- hosts/rustboro/default.nix
+++ hosts/rustboro/default.nix
@@ -21,6 +21,42 @@
 
   networking.hostName = "rustboro"; # Define your hostname.
 
+  fileSystems = {
+    "/mnt/Archive" = {
+      device = "//mauville/Archive";
+      fsType = "cifs";
+      options = [
+        "gid=100"
+        "guest"
+        "nofail"
+        "uid=${toString config.users.users.aly.uid}"
+        "x-systemd.after=network.target"
+        "x-systemd.after=tailscaled.service"
+        "x-systemd.automount"
+        "x-systemd.device-timeout=5s"
+        "x-systemd.idle-timeout=60"
+        "x-systemd.mount-timeout=5s"
+      ];
+    };
+
+    "/mnt/Media" = {
+      device = "//mauville/Media";
+      fsType = "cifs";
+      options = [
+        "gid=100"
+        "guest"
+        "nofail"
+        "uid=${toString config.users.users.aly.uid}"
+        "x-systemd.after=network.target"
+        "x-systemd.after=tailscaled.service"
+        "x-systemd.automount"
+        "x-systemd.device-timeout=5s"
+        "x-systemd.idle-timeout=60"
+        "x-systemd.mount-timeout=5s"
+      ];
+    };
+  };
+
   alyraffauf = {
     base = {
       enable = true;
--- nixosModules/users/aly/default.nix
+++ nixosModules/users/aly/default.nix
@@ -13,7 +13,7 @@
       isNormalUser = true;
       linger = true;
       uid = 1000;
-      
+
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGcJBb7+ZxkDdk06A0csNsbgT9kARUN185M8k3Lq7E/d u0_a336@localhost" # termux on winona
         (builtins.readFile ../../../secrets/publicKeys/aly_lavaridge.pub)
