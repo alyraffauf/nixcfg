b4bbe60e515e5c2e5f96eba9d570c67747af4c41
Aly Raffauf <aly@raffauflabs.com>
2024-06-22 20:19:52 -0400

    gnome: don't auto-enable night mode

--- homeManagerModules/desktop/gnome/default.nix
+++ homeManagerModules/desktop/gnome/default.nix
@@ -25,7 +25,7 @@
       };
 
       "org/gnome/settings-daemon/plugins/color" = {
-        night-light-enabled = true;
+        # night-light-enabled = true;
       };
 
       "org/gnome/shell".enabled-extensions = [
