2dc96a42af459ecf97487e5d3f630364491c96c0
Aly Raffauf <aly@raffauflabs.com>
2024-06-24 21:51:53 -0400

    firefox: optionally enable keepassxc nativemessaginghost

--- homeManagerModules/apps/firefox/default.nix
+++ homeManagerModules/apps/firefox/default.nix
@@ -7,7 +7,7 @@
   config = lib.mkIf config.alyraffauf.apps.firefox.enable {
     programs.firefox = {
       enable = true;
-      #      nativeMessagingHosts = lib.optionals (config.alyraffauf.apps.keepassxc.enable) [pkgs.keepassxc];
+      nativeMessagingHosts = lib.optionals (config.alyraffauf.apps.keepassxc.enable) [pkgs.keepassxc];
     };
   };
 }
