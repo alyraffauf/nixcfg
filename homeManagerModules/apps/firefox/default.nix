5a727fcfaf73fb575c63aae07dafeb91c37d005d
Aly Raffauf <aly@raffauflabs.com>
2024-06-24 22:30:09 -0400

    firefox: enable gnome browser-connector native messaging host if alyraffauf.desktop.gnome.enable

--- homeManagerModules/apps/firefox/default.nix
+++ homeManagerModules/apps/firefox/default.nix
@@ -7,7 +7,9 @@
   config = lib.mkIf config.alyraffauf.apps.firefox.enable {
     programs.firefox = {
       enable = true;
-      nativeMessagingHosts = lib.optionals (config.alyraffauf.apps.keepassxc.enable) [pkgs.keepassxc];
+      nativeMessagingHosts =
+        lib.optionals (config.alyraffauf.apps.keepassxc.enable) [pkgs.keepassxc]
+        ++ lib.optionals (config.alyraffauf.desktop.gnome.enable) [pkgs.gnome-browser-connector];
     };
   };
 }
