43b1c2e840cb9221865d2011cc374de1fe7553e9
Aly Raffauf <aly@raffauflabs.com>
2024-06-29 15:32:08 -0400

    aly: cleanup authorized ssh keys

--- nixosModules/users/aly/default.nix
+++ nixosModules/users/aly/default.nix
@@ -25,14 +25,19 @@
       linger = true;
       uid = 1000;
 
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG0HAmaMTAvrFrinB+b83c8hq6PyjmxRHg1IxR2GH6RN u0_a344@localhost" # termux on wallace
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGcJBb7+ZxkDdk06A0csNsbgT9kARUN185M8k3Lq7E/d u0_a336@localhost" # termux on winona
-        (builtins.readFile ../../../secrets/publicKeys/aly_lavaridge.pub)
-        (builtins.readFile ../../../secrets/publicKeys/aly_mauville.pub)
-        (builtins.readFile ../../../secrets/publicKeys/aly_petalburg.pub)
-        (builtins.readFile ../../../secrets/publicKeys/aly_rustboro.pub)
-      ];
+      openssh.authorizedKeys = {
+        keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG0HAmaMTAvrFrinB+b83c8hq6PyjmxRHg1IxR2GH6RN u0_a344@localhost" # termux on wallace
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGcJBb7+ZxkDdk06A0csNsbgT9kARUN185M8k3Lq7E/d u0_a336@localhost" # termux on winona
+        ];
+
+        keyFiles = [
+          ../../../secrets/publicKeys/aly_lavaridge.pub
+          ../../../secrets/publicKeys/aly_mauville.pub
+          ../../../secrets/publicKeys/aly_petalburg.pub
+          ../../../secrets/publicKeys/aly_rustboro.pub
+        ];
+      };
     };
   };
 }
