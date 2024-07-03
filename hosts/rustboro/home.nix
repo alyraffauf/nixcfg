a5190b0bed0efa8d7223e0dae647bb38ce0a9dfa
Aly Raffauf <aly@raffauflabs.com>
2024-07-03 09:17:29 -0400

    rustboro/home: migrate cursor theme overrides

--- hosts/rustboro/home.nix
+++ hosts/rustboro/home.nix
@@ -11,13 +11,16 @@
         programs.vscode.userSettings = {
           "editor.fontSize" = lib.mkForce "16";
         };
+
+        home.pointerCursor.size = lib.mkForce 24;
+
         ar.home = {
           services.easyeffects = {
             enable = true;
             preset = "LoudnessEqualizer";
           };
+
           theme = {
-            cursorTheme.size = lib.mkForce 24;
             font.size = lib.mkForce 14;
             terminalFont.size = lib.mkForce 14;
           };
