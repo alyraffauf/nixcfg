1cfb1877cbdd5b6653154068a642b3b425b9c79f
Aly Raffauf <aly@raffauflabs.com>
2024-07-07 14:48:46 -0400

    nixos/gnome: add triple buffering patch

--- nixosModules/desktop/gnome/default.nix
+++ nixosModules/desktop/gnome/default.nix
@@ -37,6 +37,23 @@ in {
       ]
       ++ [gnomeCsAdjuster];
 
+    nixpkgs.overlays = [
+      # GNOME 46: triple-buffering-v4-46
+      (final: prev: {
+        gnome = prev.gnome.overrideScope (gnomeFinal: gnomePrev: {
+          mutter = gnomePrev.mutter.overrideAttrs (old: {
+            src = pkgs.fetchFromGitLab {
+              domain = "gitlab.gnome.org";
+              owner = "vanvugt";
+              repo = "mutter";
+              rev = "triple-buffering-v4-46";
+              hash = "sha256-fkPjB/5DPBX06t7yj0Rb3UEuu5b9mu3aS+jhH18+lpI=";
+            };
+          });
+        });
+      })
+    ];
+
     # Enable keyring support for KDE apps in GNOME.
     security.pam.services.gdm = {
       enableGnomeKeyring = true;
