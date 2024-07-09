bf223007013b0662d46e3ae149f9122c256f32da
Aly Raffauf <aly@raffauflabs.com>
2024-07-09 18:22:05 -0400

    fetch wallpaper correctly

--- homes/morgan/default.nix
+++ homes/morgan/default.nix
@@ -40,7 +40,7 @@ self: {
 
     theme = {
       enable = true;
-      wallpaper = "${self.inputs.wallpapers.packages.${pkgs.system}.default}/share//backgrounds/jr-korpa-9XngoIpxcEo-unsplash.jpg";
+      wallpaper = "${config.xdg.dataHome}/backgrounds/jr-korpa-9XngoIpxcEo-unsplash.jpg";
     };
   };
 }
