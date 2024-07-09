17c234ac676d2884d67573bc5407b1d3d8f8bbdf
Aly Raffauf <aly@raffauflabs.com>
2024-07-09 18:40:48 -0400

    home/nemo: add dconf settings

--- homeManagerModules/apps/nemo/default.nix
+++ homeManagerModules/apps/nemo/default.nix
@@ -12,11 +12,33 @@
     dconf = {
       enable = true;
       settings = {
-        "org/nemo/preferences".show-image-thumbnails = "always";
-        "org/nemo/preferences/menu-config".background-menu-open-as-root =
-          !(config.ar.home.desktop.hyprland.enable || config.ar.home.desktop.sway.enable);
-        "org/nemo/preferences/menu-config".selection-menu-open-as-root =
-          !(config.ar.home.desktop.hyprland.enable || config.ar.home.desktop.sway.enable);
+        "org/nemo/preferences" = {
+          show-image-thumbnails = "always";
+          thumbnail-limit = lib.hm.gvariant.mkUint64 (100 * 1024 * 1024);
+          tooltips-in-icon-view = true;
+          tooltips-show-access-date = true;
+          tooltips-show-birth-date = true;
+          tooltips-show-file-type = true;
+          tooltips-show-mod-date = true;
+        };
+
+        "org/nemo/preferences/menu-config" = {
+          background-menu-open-as-root =
+            !(config.ar.home.desktop.hyprland.enable || config.ar.home.desktop.sway.enable);
+          selection-menu-open-as-root =
+            !(config.ar.home.desktop.hyprland.enable || config.ar.home.desktop.sway.enable);
+        };
+
+        "org/nemo/plugins".disabled-actions =
+          lib.optionals
+          (config.ar.home.desktop.hyprland.enable || config.ar.home.desktop.sway.enable) [
+            "90_new-launcher.nemo_action"
+            "add-desklets.nemo_action"
+            "change-background.nemo_action"
+            "mount-archive.nemo_action"
+            "set-as-background.nemo_action"
+            "set-resolution.nemo_action"
+          ];
       };
     };
   };
