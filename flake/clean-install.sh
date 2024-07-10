2a7632f19ccd014873e232ddcb84bc6d7f0aec17
Aly Raffauf <aly@raffauflabs.com>
2024-07-09 23:34:43 -0400

    flake/clean-install: reformat

--- flake/clean-install.sh
+++ flake/clean-install.sh
@@ -1,7 +1,7 @@
 # Check if an argument is provided
 if [ $# -eq 0 ]; then
-        echo "Error: Please provide a valid hostname as an argument."
-        exit 1
+    echo "Error: Please provide a valid hostname as an argument."
+    exit 1
 fi
 
 HOST=$1
@@ -11,12 +11,12 @@ echo "Warning: Running this script will wipe the currently installed system."
 read -p "Do you want to continue? (y/n): " answer
 
 if [ "$answer" != "y" ]; then
-        echo "Aborted."
-        exit 0
+    echo "Aborted."
+    exit 0
 fi
 
 sudo nix --experimental-features "nix-command flakes" run \
-        github:nix-community/disko -- --mode disko --flake $FLAKE
+    github:nix-community/disko -- --mode disko --flake $FLAKE
 
 # Install NixOS with the updated flake input and root settings
 sudo nixos-install --no-root-password --root /mnt --flake $FLAKE
