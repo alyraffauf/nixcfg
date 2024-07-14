4406bff10274d63f83d00e4925e4a7b69e1036d9
Aly Raffauf <aly@raffauflabs.com>
2024-07-13 23:35:22 -0400

    home/zed: improved defaults

--- homeManagerModules/apps/zed/default.nix
+++ homeManagerModules/apps/zed/default.nix
@@ -13,8 +13,8 @@
     xdg.configFile."zed/settings.json".text = let
       defaults = {
         auto_install_extensions = {
-          adwaita_pastel = true;
-          bahser = true;
+          adwaita-pastel = true;
+          basher = true;
           docker-compose = true;
           dockerfile = true;
           git-firefly = true;
@@ -25,14 +25,14 @@
           log = true;
           make = true;
           nix = true;
-          python = true;
           ruby = true;
         };
 
         auto_update = false;
-        autosave = "on_focus_change";
+        autosave.after_delay.milliseconds = 1000;
         buffer_font_family = "NotoSansM Nerd Font";
         buffer_font_size = 14;
+        current_line_highlight = "line";
 
         indent_guides = {
           enabled = true;
@@ -42,10 +42,26 @@
         };
 
         languages = {
-          Nix.formatter.external = {
-            command = lib.getExe pkgs.alejandra;
-            arguments = [];
+          Nix = {
+            formatter.external = {
+              command = lib.getExe pkgs.alejandra;
+              arguments = [];
+            };
+
+            tab_size = 2;
           };
+        };
+
+        preferred_line_length = 80;
+
+        tab_bar = {
+          show = false;
+          show_nav_history_buttons = false;
+        };
+
+        tabs = {
+          close_position = "right";
+          git_status = true;
         };
 
         telemetry = {
@@ -53,10 +69,13 @@
           metrics = true;
         };
 
-        theme =
-          if config.ar.home.theme.darkMode
-          then "Adwaita Pastel Dark"
-          else "Adwaita Pastel Light";
+        terminal.font_size = 12;
+
+        theme = {
+          dark = "Adwaita Pastel Dark";
+          light = "Adwaita Pastel Light";
+          mode = "system";
+        };
 
         ui_font_size = 16;
         vim_mode = false;
