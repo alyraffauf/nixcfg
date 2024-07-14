5ba420030a420634cc000ef4de43b6a50e9c0e16
Aly Raffauf <aly@raffauflabs.com>
2024-07-13 21:34:41 -0400

    home/keepassxc: refactored settings

--- homeManagerModules/apps/keepassxc/default.nix
+++ homeManagerModules/apps/keepassxc/default.nix
@@ -7,44 +7,45 @@
   config = lib.mkIf config.ar.home.apps.keepassxc.enable {
     home.packages = [pkgs.keepassxc];
 
-    ar.home.apps.keepassxc.settings = lib.mkDefault {
-      Browser = {
-        AlwaysAllowAccess = true;
-        Enabled = true;
-        SearchInAllDatabases = true;
-        UpdateBinaryPath = false;
+    xdg.configFile."keepassxc/keepassxc.ini".text = let
+      defaults = {
+        Browser = {
+          AlwaysAllowAccess = true;
+          Enabled = true;
+          SearchInAllDatabases = true;
+          UpdateBinaryPath = false;
+        };
+
+        General = {
+          ConfigVersion = 2;
+          HideWindowOnCopy = true;
+          MinimizeAfterUnlock = false;
+          MinimizeOnOpenUrl = true;
+        };
+
+        GUI = {
+          ApplicationTheme = "classic";
+          ColorPasswords = false;
+          CompactMode = true;
+          MinimizeOnClose = true;
+          MinimizeOnStartup = true;
+          MinimizeToTray = true;
+          ShowTrayIcon = true;
+          TrayIconAppearance = "colorful";
+        };
+
+        Security = {
+          ClearClipboardTimeout = 15;
+          EnableCopyOnDoubleClick = true;
+          IconDownloadFallback = true;
+          LockDatabaseScreenLock = false;
+        };
+
+        SSHAgent.Enabled = true;
       };
 
-      General = {
-        ConfigVersion = 2;
-        HideWindowOnCopy = true;
-        MinimizeAfterUnlock = false;
-        MinimizeOnOpenUrl = true;
-      };
-
-      GUI = {
-        ApplicationTheme = "classic";
-        ColorPasswords = false;
-        CompactMode = true;
-        MinimizeOnClose = true;
-        MinimizeOnStartup = true;
-        MinimizeToTray = true;
-        ShowTrayIcon = true;
-        TrayIconAppearance = "colorful";
-      };
-
-      Security = {
-        ClearClipboardTimeout = 15;
-        EnableCopyOnDoubleClick = true;
-        IconDownloadFallback = true;
-        LockDatabaseScreenLock = false;
-      };
-
-      SSHAgent.Enabled = true;
-    };
-
-    xdg.configFile."keepassxc/keepassxc.ini".text =
-      lib.generators.toINI {}
-      config.ar.home.apps.keepassxc.settings;
+      settings = defaults // config.ar.home.apps.keepassxc.settings;
+    in
+      lib.generators.toINI {} settings;
   };
 }
