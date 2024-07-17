9c45f15ed60f105bab3ba020c81456b49a3950d4
Aly Raffauf <aly@raffauflabs.com>
2024-07-17 11:35:19 -0400

    users: login from phone

--- userModules/default.nix
+++ userModules/default.nix
@@ -13,11 +13,18 @@ self: {
   users = {
     mutableUsers = false;
 
-    users.root.openssh.authorizedKeys.keyFiles = [
-      ../secrets/publicKeys/aly_lavaridge.pub
-      ../secrets/publicKeys/aly_mauville.pub
-      ../secrets/publicKeys/aly_petalburg.pub
-      ../secrets/publicKeys/aly_rustboro.pub
-    ];
+    users.root.openssh.authorizedKeys = {
+      keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG0HAmaMTAvrFrinB+b83c8hq6PyjmxRHg1IxR2GH6RN u0_a344@localhost" # termux on wallace
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGcJBb7+ZxkDdk06A0csNsbgT9kARUN185M8k3Lq7E/d u0_a336@localhost" # termux on winona
+      ];
+
+      keyFiles = [
+        ../secrets/publicKeys/aly_lavaridge.pub
+        ../secrets/publicKeys/aly_mauville.pub
+        ../secrets/publicKeys/aly_petalburg.pub
+        ../secrets/publicKeys/aly_rustboro.pub
+      ];
+    };
   };
 }
