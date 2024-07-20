14b568f36f02f7fc6e311fc33c9c882e33eeeff9
Aly Raffauf <aly@raffauflabs.com>
2024-07-19 22:25:47 -0400

    Revert "hosts: enable tailscale ssh"
    This reverts commit e7bb6fc6764fe9c6eec8533b3e69720051df7b68.

--- hosts/common/network.nix
+++ hosts/common/network.nix
@@ -10,8 +10,7 @@
 
   services.tailscale = {
     enable = true;
-    authKeyFile = config.age.secrets.tailscaleAuthKey.path;
-    extraUpFlags = ["--ssh"];
     openFirewall = true;
+    authKeyFile = config.age.secrets.tailscaleAuthKey.path;
   };
 }
