6bfa581c54e7001147e29d28d2826200149f1a96
Aly Raffauf <aly@raffauflabs.com>
2024-07-25 12:15:14 -0400

    fetchgit script: simplify

--- update_fetchgit.sh
+++ update_fetchgit.sh
@@ -1,4 +1,2 @@
-#1/bin/bash
-nix run nixpkgs#update-nix-fetchgit -- homeManagerModules/desktop/default.nix
-nix run nixpkgs#update-nix-fetchgit -- homes/aly/firefox/default.nix
-nix run nixpkgs#update-nix-fetchgit -- homes/aly/mail/default.nix
+#!/bin/bash
+nix run nixpkgs#update-nix-fetchgit -- homeManagerModules/desktop/default.nix homes/aly/firefox/default.nix homes/aly/mail/default.nix
