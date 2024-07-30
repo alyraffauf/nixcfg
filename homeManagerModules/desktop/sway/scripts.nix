d037323c6032327b6fd1bc2a57d452b15a24e469
Aly Raffauf <aly@raffauflabs.com>
2024-07-30 15:56:44 -0400

    sway: don't sleep before suspend

--- homeManagerModules/desktop/sway/scripts.nix
+++ homeManagerModules/desktop/sway/scripts.nix
@@ -12,7 +12,7 @@ in {
       ["timeout 120 '${lib.getExe pkgs.brightnessctl} -s set 10' resume '${lib.getExe pkgs.brightnessctl} -r'"]
       ++ (
         if cfg.desktop.sway.autoSuspend
-        then ["timeout 600 'sleep 2 && ${lib.getExe' pkgs.systemd "systemctl"} suspend'"]
+        then ["timeout 600 '${lib.getExe' pkgs.systemd "systemctl"} suspend'"]
         else [
           "timeout 600 '${lib.getExe pkgs.swaylock}'"
           "timeout 630 '${swaymsg} \"output * dpms off\"' resume '${swaymsg} \"output * dpms on\"'"
