e82d9087f42522efdba3a1070db4a5ac80601e36
Aly Raffauf <aly@raffauflabs.com>
2024-08-08 16:50:50 -0400

    fastfetch: nixify config

--- homeManagerModules/apps/fastfetch/default.nix
+++ homeManagerModules/apps/fastfetch/default.nix
@@ -6,6 +6,6 @@
 }: {
   config = lib.mkIf config.ar.home.apps.fastfetch.enable {
     home.packages = [pkgs.fastfetch];
-    xdg.configFile."fastfetch/config.jsonc".source = ./config.jsonc;
+    xdg.configFile."fastfetch/config.jsonc".text = lib.generators.toJSON {} (import ./config.nix);
   };
 }
