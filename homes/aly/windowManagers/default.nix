66cb5f7f33186d050b38e902eebfafaabcf22c8d
Aly Raffauf <aly@raffauflabs.com>
2024-08-11 15:01:50 -0400

    aly/sway: bring to parity with hyprland workspaces (#87)

--- homes/aly/windowManagers/default.nix
+++ homes/aly/windowManagers/default.nix
@@ -11,21 +11,35 @@
   wayland.windowManager = {
     sway.config = {
       assigns = {
-        "workspace 1: web" = [{app_id = "firefox";} {app_id = "brave-browser";}];
-        "workspace 2: code" = [{app_id = "codium-url-handler";} {app_id = "dev.zed.Zed";}];
-        "workspace 3: note" = [{app_id = "obsidian";}];
-        "workspace 4: chat" = [{app_id = "org.gnome.Fractal";} {app_id = "WebCord";}];
-        "workspace 5: work" = [{app_id = "google-chrome";} {app_id = "chromium-browser";} {app_id = "firework";}];
+        "workspace 1:web" = [{app_id = "firefox";} {app_id = "brave-browser";}];
+        "workspace 2:note" = [{app_id = "obsidian";}];
+        "workspace 3:code" = [{app_id = "codium-url-handler";} {app_id = "dev.zed.Zed";}];
+        "workspace 4:mail" = [{app_id = "thunderbird";}];
+        "workspace 5:work" = [{app_id = "firework";}];
       };
 
       floating.criteria = [{app_id = "Bitwarden";} {app_id = "org.keepassxc.KeePassXC";}];
 
+      gaps = {
+        smartBorders = "on";
+        smartGaps = true;
+      };
+
       input."type:keyboard".xkb_options = "caps:ctrl_modifier";
 
       keybindings = {
-        "${config.wayland.windowManager.sway.config.modifier}+P" = "exec ${lib.getExe pkgs.rofi-rbw-wayland}";
         "${config.wayland.windowManager.sway.config.modifier}+N" = "exec ${lib.getExe' pkgs.obsidian "obsidian"}";
+        "${config.wayland.windowManager.sway.config.modifier}+P" = "exec ${lib.getExe pkgs.rofi-rbw-wayland}";
       };
+
+      startup = [
+        {command = "sleep 2 && ${lib.getExe' config.wayland.windowManager.sway.package "swaymsg"} workspace 1:web";}
+        {command = lib.getExe config.ar.home.defaultApps.editor;}
+        {command = lib.getExe config.ar.home.defaultApps.webBrowser;}
+        {command = lib.getExe pkgs.fractal;}
+        {command = lib.getExe pkgs.thunderbird;}
+        {command = lib.getExe' pkgs.obsidian "obsidian";}
+      ];
 
       output = {
         "Guangxi Century Innovation Display Electronics Co., Ltd 27C1U-D 0000000000001" = {
@@ -47,6 +61,10 @@
         {
           command = "resize set 80ppt 80ppt; move position center; sticky toggle;";
           criteria = {app_id = "org.keepassxc.KeePassXC";};
+        }
+        {
+          command = "move to scratchpad";
+          criteria = {app_id = "org.gnome.Fractal";};
         }
       ];
     };
@@ -76,11 +94,6 @@
         "workspace 5,class:(firework)"
         "workspace special:magic,class:(WebCord)"
         "workspace special:magic,class:(org.gnome.Fractal)"
-        # "workspace 1,class:(firefox)"
-        # "workspace 2,class:(obsidian)"
-        # "workspace 3,class:(codium-url-handler)"
-        # "workspace 3,class:(dev.zed.Zed)"
-        # "workspace 4,class:(thunderbird)"
       ];
 
       workspace = [
