62f2d5b1d067436e6b43b4460c3e74802ae4da2d
Aly Raffauf <aly@raffauflabs.com>
2024-08-12 01:05:07 -0400

    desktop: swaylock -> gtklock (#89)
    * nixos/desktop: add pam config for gtklock
    
    * apps: add gtklock config
    
    * nixos: fix duplicate pam settings
    
    * desktop: switch to gtklock
    
    * swayidle: switch to gtklock
    
    * nixfmt
    
    * desktop: simplify pam config with inherit

--- homeManagerModules/options.nix
+++ homeManagerModules/options.nix
@@ -36,6 +36,7 @@ in {
       fastfetch.enable = lib.mkEnableOption "Fastfetch.";
       firefox.enable = lib.mkEnableOption "Firefox web browser.";
       fuzzel.enable = lib.mkEnableOption "Fuzzel app launcher.";
+      gtklock.enable = lib.mkEnableOption "GTK lockscreen based on gtkgreet.";
       helix.enable = lib.mkEnableOption "Helix text editor.";
 
       keepassxc = {
--- homeManagerModules/services/swayidle/default.nix
+++ homeManagerModules/services/swayidle/default.nix
@@ -19,11 +19,11 @@ in {
         }
         {
           event = "before-sleep";
-          command = "${lib.getExe pkgs.swaylock} && ${lib.getExe' pkgs.coreutils "sleep"} 2";
+          command = "${lib.getExe pkgs.gtklock} && ${lib.getExe' pkgs.coreutils "sleep"} 2";
         }
         {
           event = "lock";
-          command = "${lib.getExe pkgs.swaylock}";
+          command = "${lib.getExe pkgs.gtklock}";
         }
       ];
 
@@ -42,7 +42,7 @@ in {
         ++ lib.optional (!cfg.desktop.autoSuspend)
         {
           timeout = 600;
-          command = "${lib.getExe pkgs.swaylock}";
+          command = "${lib.getExe pkgs.gtklock}";
         }
         ++ lib.optional (!cfg.desktop.autoSuspend && cfg.desktop.hyprland.enable)
         {
