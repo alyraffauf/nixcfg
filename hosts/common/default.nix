b188915ddc9e8e15b09999b79b532c56360030b9
Aly Raffauf <aly@raffauflabs.com>
2024-08-14 22:59:17 -0400

    add initial common stylix theme

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -14,5 +14,36 @@
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine-moon.yaml";
     image = ../../_img/hyprland.png;
+    imageScalingMode = "fill";
+    polarity = "dark";
+
+    cursor = {
+      name = "Bibata-Modern-Classic";
+      package = pkgs.bibata-cursors;
+      size = 24;
+    };
+
+    fonts = {
+      monospace = {
+        name = "UbuntuSansMono Nerd Font";
+        package = pkgs.nerdfonts.override {fonts = ["UbuntuSans"];};
+      };
+
+      sansSerif = {
+        name = "UbuntuSans Nerd Font";
+        package = pkgs.nerdfonts.override {fonts = ["UbuntuSans"];};
+      };
+
+      serif = {
+        name = "Vegur";
+        package = pkgs.vegur;
+      };
+    };
+
+    opacity = {
+      applications = 0.8;
+      desktop = 0.8;
+      terminal = 0.8;
+    };
   };
 }
