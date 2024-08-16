18ae4dfd8577b9cf5c378c72fc50eb8959606e68
Aly Raffauf <aly@raffauflabs.com>
2024-08-15 20:51:07 -0400

    update_fetchgit: remove unused fetchgits

--- update_fetchgit.sh
+++ update_fetchgit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nix run nixpkgs#update-nix-fetchgit -- homeManagerModules/desktop/default.nix homes/aly/firefox/default.nix homes/aly/mail/default.nix
+nix run nixpkgs#update-nix-fetchgit -- homeManagerModules/desktop/default.nix 
