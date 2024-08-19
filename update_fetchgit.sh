be4b2717cf844140821236ca0ecdf0addbd4f6bc
Aly Raffauf <aly@raffauflabs.com>
2024-08-18 22:45:00 -0400

    flake: update fetchgit script

--- update_fetchgit.sh
+++ update_fetchgit.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-nix run nixpkgs#update-nix-fetchgit -- homeManagerModules/desktop/default.nix 
 nix run nixpkgs#update-nix-fetchgit -- homeManagerModules/desktop/default.nix hosts/fallarbor/stylix.nix hosts/lavaridge/stylix.nix hosts/mauville/stylix.nix hosts/petalburg/stylix.nix hosts/rustboro/stylix.nix
