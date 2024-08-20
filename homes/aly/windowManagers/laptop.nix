0b489bbe1b3a0d4951355e887dd53cf07011afea
Aly Raffauf <aly@raffauflabs.com>
2024-08-20 14:34:12 -0400

    aly: restore hyprland workspace assognments for laptops

--- homes/aly/windowManagers/laptop.nix
+++ homes/aly/windowManagers/laptop.nix
@@ -25,6 +25,11 @@
           "sleep 2 && hyprctl dispatch workspace 2 && sleep 2 && hyprctl dispatch workspace 3 && sleep 2 && hyprctl dispatch workspace 4 && sleep 2 && hyprctl dispatch workspace 1;"
         ];
 
+        windowrulev2 = [
+          "workspace 1,class:(brave-browser)"
+          "workspace 5,class:(firework)"
+        ];
+
         workspace = [
           "1,defaultName:web,on-created-empty:${lib.getExe config.ar.home.defaultApps.webBrowser}"
           "2,defaultName:note,on-created-empty:${lib.getExe' pkgs.obsidian "obsidian"}"
