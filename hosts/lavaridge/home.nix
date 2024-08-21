366179e3d0197ced2b06a59d99b613d2c9bb8a43
Aly Raffauf <aly@raffauflabs.com>
2024-08-21 12:16:03 -0400

    lavaridge: disable gammastep

--- hosts/lavaridge/home.nix
+++ hosts/lavaridge/home.nix
@@ -1,4 +1,4 @@
-{
+{lib, ...}: {
   home-manager.sharedModules = [
     {
       wayland.windowManager.sway.config.output = {"BOE 0x095F Unknown".scale = "1.5";};
@@ -6,9 +6,13 @@
       ar.home = {
         desktop.hyprland.laptopMonitors = ["desc:BOE 0x095F,preferred,auto,1.566667"];
 
-        services.easyeffects = {
-          enable = true;
-          preset = "LoudnessEqualizer";
+        services = {
+          easyeffects = {
+            enable = true;
+            preset = "LoudnessEqualizer";
+          };
+
+          gammastep.enable = lib.mkForce false;
         };
       };
     }
