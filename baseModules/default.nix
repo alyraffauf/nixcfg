9e8630e3e3710d0344eb9bffc2df4231ca87b4a5
Aly Raffauf <aly@raffauflabs.com>
2024-08-22 07:25:42 -0400

    move nix settings base -> common/nix

--- baseModules/default.nix
+++ baseModules/default.nix
@@ -14,29 +14,6 @@ self: {lib, ...}: {
 
   networking.networkmanager.enable = true;
 
-  nix = {
-    gc = {
-      automatic = true;
-      dates = "daily";
-      options = "--delete-older-than 3d";
-      persistent = true;
-      randomizedDelaySec = "60min";
-    };
-
-    # Run GC when there is less than 100MiB left.
-    extraOptions = ''
-      min-free = ${toString (100 * 1024 * 1024)}
-      max-free = ${toString (1024 * 1024 * 1024)}
-    '';
-
-    optimise.automatic = true;
-
-    settings = {
-      auto-optimise-store = false;
-      experimental-features = ["nix-command" "flakes"];
-    };
-  };
-
   security = {
     polkit.enable = true;
     rtkit.enable = true;
