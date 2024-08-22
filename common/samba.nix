4769a30b3f41cfc4bf2e8a40ce7154575213a4dc
Aly Raffauf <aly@raffauflabs.com>
2024-08-22 07:23:01 -0400

    rename commonModules -> common

---
+++ common/samba.nix
@@ -0,0 +1,36 @@
+{config, ...}: {
+  fileSystems = let
+    fsType = "cifs";
+    options = [
+      "gid=100"
+      "guest"
+      "nofail"
+      "uid=${toString config.users.users.aly.uid}"
+      "x-systemd.after=network.target"
+      "x-systemd.after=tailscaled.service"
+      "x-systemd.automount"
+      "x-systemd.device-timeout=5s"
+      "x-systemd.idle-timeout=60"
+      "x-systemd.mount-timeout=5s"
+    ];
+  in {
+    "/mnt/Archive" = {
+      inherit options fsType;
+      device = "//mauville/Archive";
+    };
+
+    "/mnt/Media" = {
+      inherit options fsType;
+      device = "//mauville/Media";
+    };
+  };
+
+  home-manager.sharedModules = [
+    {
+      gtk.gtk3.bookmarks = [
+        "file:///mnt/Media"
+        "file:///mnt/Archive"
+      ];
+    }
+  ];
+}
---
+++ common/wifi.nix
@@ -0,0 +1,51 @@
+{config, ...}: let
+  mkOpenWiFi = ssid: {
+    connection.id = "${ssid}";
+    connection.type = "wifi";
+    ipv4.method = "auto";
+    ipv6.addr-gen-mode = "default";
+    ipv6.method = "auto";
+    wifi.mode = "infrastructure";
+    wifi.ssid = "${ssid}";
+  };
+
+  mkWPA2WiFi = ssid: psk: (
+    (mkOpenWiFi ssid)
+    // {
+      wifi-security.auth-alg = "open";
+      wifi-security.key-mgmt = "wpa-psk";
+      wifi-security.psk = "${psk}";
+    }
+  );
+
+  mkEAPWiFi = ssid: identity: pass: auth: (
+    (mkOpenWiFi ssid)
+    // {
+      "802-1x".eap = "peap;";
+      "802-1x".identity = "${identity}";
+      "802-1x".password = "${pass}";
+      "802-1x".phase2-auth = "${auth}";
+      wifi-security.auth-alg = "open";
+      wifi-security.key-mgmt = "wpa-eap";
+    }
+  );
+in {
+  age.secrets.wifi.file = ../secrets/wifi.age;
+
+  networking.networkmanager = {
+    enable = true;
+
+    ensureProfiles = {
+      environmentFiles = [config.age.secrets.wifi.path];
+      profiles = {
+        "Dustin's A54" = mkWPA2WiFi "Dustin's A54" "$DustinsA54PSK";
+        "javapatron" = mkOpenWiFi "javapatron";
+        "Muchacho Guest" = mkOpenWiFi "Muchacho Guest";
+        "Stargate-Discovery" = mkWPA2WiFi "Stargate-Discovery" "$StargateDiscoveryPSK";
+        "Taproom Public WiFi" = mkOpenWiFi "Taproom Public WiFi";
+        "wallace" = mkWPA2WiFi "wallace" "$wallacePSK";
+        "WeWorkWiFi" = mkEAPWiFi "WeWorkWiFi" "$WeWorkWiFiIdentity" "$WeWorkWiFiPassword" "mschapv2";
+      };
+    };
+  };
+}
