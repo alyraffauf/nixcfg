63906f27827174e99e0b30f543f07227d314ddd8
Aly Raffauf <aly@raffauflabs.com>
2024-08-25 00:10:01 -0400

    theme: remove unnecessary comments in gtk css

--- homeManagerModules/theme.nix
+++ homeManagerModules/theme.nix
@@ -36,7 +36,6 @@ in {
     };
 
     stylix.targets.gtk.extraCss = ''
-      // Control rounded corners
       window.background { border-radius: ${toString cfg.borders.radius}; }
 
       tooltip {
