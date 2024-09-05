cd1b43bf3ad387a9bf69ad2116ccdc17235667d1
Aly Raffauf <aly@raffauflabs.com>
2024-09-04 20:39:24 -0400

    kitty: add window padding

--- homeManagerModules/apps/kitty/default.nix
+++ homeManagerModules/apps/kitty/default.nix
@@ -14,6 +14,7 @@ in {
         confirm_os_window_close = "0";
         notify_on_cmd_finish = "unfocused 10.0 command ${lib.getExe pkgs.libnotify} -i ${pkgs.kitty}/share/icons/hicolor/256x256/apps/kitty.png \"Job Finished\"";
         tab_bar_style = "powerline";
+        window_padding_width = 5;
       };
     };
   };
