4288d83a02bb299c61758b79aa03ddc36b350309
Aly Raffauf <aly@raffauflabs.com>
2024-09-08 15:59:35 -0400

    lavaridge: set GDK_SCALE=2

--- hosts/lavaridge/default.nix
+++ hosts/lavaridge/default.nix
@@ -36,7 +36,7 @@
     };
   };
 
-  environment.variables.GDK_SCALE = "1.5";
+  environment.variables.GDK_SCALE = "2";
   networking.hostName = "lavaridge";
   programs.firefox.policies.Preferences."media.ffmpeg.vaapi.enabled" = lib.mkForce false;
   system.stateVersion = "24.05";
