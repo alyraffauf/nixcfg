2cce4e6f036d7f87213ed8c99ca3afcf518293d4
Aly Raffauf <aly@raffauflabs.com>
2024-09-13 16:58:15 -0400

    rustboro: use flake master

--- hosts/rustboro/default.nix
+++ hosts/rustboro/default.nix
@@ -36,11 +36,7 @@
     };
   };
 
-  environment.variables = {
-    FLAKE = "https://flakehub.com/f/alyraffauf/nixcfg/*.tar.gz";
-    GDK_SCALE = "1.25";
-  };
-
+  environment.variables.GDK_SCALE = "1.25";
   networking.hostName = "rustboro";
   system.stateVersion = "24.05";
   zramSwap.memoryPercent = 100;
