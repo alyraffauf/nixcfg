9be3979d7ebfa54d36038f0208582186d5946942
Aly Raffauf <aly@raffauflabs.com>
2024-10-13 15:27:28 -0400

    pacifidlog: use lanzaboote

--- hosts/pacifidlog/default.nix
+++ hosts/pacifidlog/default.nix
@@ -24,14 +24,14 @@
   boot = {
     initrd.systemd.enable = true;
 
-    # lanzaboote = {
-    #   enable = true;
-    #   pkiBundle = "/etc/secureboot";
-    # };
+    lanzaboote = {
+      enable = true;
+      pkiBundle = "/etc/secureboot";
+    };
 
     loader = {
       efi.canTouchEfiVariables = true;
-      systemd-boot.enable = lib.mkForce true;
+      systemd-boot.enable = lib.mkForce false;
     };
   };
 
