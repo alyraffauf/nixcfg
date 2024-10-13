a49c2dcb3200507702aae27252622ab6aa4148e2
Aly Raffauf <aly@raffauflabs.com>
2024-10-13 15:44:23 -0400

    pacifidlog: transform touch device in hyprland

--- hosts/pacifidlog/home.nix
+++ hosts/pacifidlog/home.nix
@@ -5,13 +5,16 @@
         wayland.windowManager = {
           hyprland.settings = {
             input = {
-              touchdevice.output = "eDP-1";
+              touchdevice = {
+                transform = 1;
+                output = "eDP-1";
+              };
             };
           };
         };
 
         ar.home = {
-          desktop.hyprland.laptopMonitors = ["eDP-1, 1600x2560@144, 0x0, 2, transform, 1"];
+          desktop.hyprland.laptopMonitors = ["desc:Lenovo Group Limited Go Display 0x00888888, 1600x2560@144, 0x0, 2, transform, 1"];
 
           # services = {
           #   easyeffects = {
