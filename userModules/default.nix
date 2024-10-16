a2ff41914869d0b71c4fb9eafb443edb32d1ccea
Aly Raffauf <aly@raffauflabs.com>
2024-10-15 20:16:41 -0400

    users: drop petalburg

--- userModules/default.nix
+++ userModules/default.nix
@@ -15,7 +15,6 @@ self: {
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGcJBb7+ZxkDdk06A0csNsbgT9kARUN185M8k3Lq7E/d u0_a336@localhost" # termux on winona
       (builtins.readFile ../secrets/publicKeys/aly_lavaridge.pub)
       (builtins.readFile ../secrets/publicKeys/aly_mauville.pub)
-      (builtins.readFile ../secrets/publicKeys/aly_petalburg.pub)
       (builtins.readFile ../secrets/publicKeys/aly_rustboro.pub)
     ];
   };
