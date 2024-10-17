cac8a85f0c683be7ffb2ec19aea9ed8d169f69d4
Aly Raffauf <aly@raffauflabs.com>
2024-10-16 22:18:27 -0400

    users/aly: drop mossdeep ssh key

--- userModules/aly/default.nix
+++ userModules/aly/default.nix
@@ -21,7 +21,6 @@
         keyFiles = [
           ../../secrets/publicKeys/aly_lavaridge.pub
           ../../secrets/publicKeys/aly_mauville.pub
-          ../../secrets/publicKeys/aly_mossdeep.pub
           ../../secrets/publicKeys/aly_petalburg.pub
           ../../secrets/publicKeys/aly_rustboro.pub
         ];
