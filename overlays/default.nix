976bcbdb38e00a1b7619073760c30407d91d596f
Aly Raffauf <aly@raffauflabs.com>
2024-10-19 15:20:18 -0400

    pkgs: add rofi-bluetooth

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,4 +2,5 @@
 {self}: final: prev: {
   adjustor = self.packages.${prev.system}.adjustor;
   hhd-ui = self.packages.${prev.system}.hhd-ui;
+  rofi-bluetooth = self.packages.${prev.system}.rofi-bluetooth;
 }
