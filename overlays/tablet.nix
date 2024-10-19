acf3380875997b320a5750084c1f12eebcfd2c47
Aly Raffauf <aly@raffauflabs.com>
2024-10-19 13:33:23 -0400

    overlays: init with tablet overlays

---
+++ overlays/tablet.nix
@@ -0,0 +1,19 @@
+final: prev: {
+  brave = prev.brave.override {commandLineArgs = "--gtk-version=4 --enable-wayland-ime";};
+
+  obsidian = prev.obsidian.overrideAttrs (old: {
+    installPhase =
+      builtins.replaceStrings ["--ozone-platform=wayland"]
+      ["--ozone-platform=wayland --enable-wayland-ime"]
+      old.installPhase;
+  });
+
+  vscodium = prev.vscodium.override {commandLineArgs = "--enable-wayland-ime";};
+
+  webcord = prev.webcord.overrideAttrs (old: {
+    installPhase =
+      builtins.replaceStrings ["--ozone-platform-hint=auto"]
+      ["--ozone-platform-hint=auto --enable-wayland-ime"]
+      old.installPhase;
+  });
+}
