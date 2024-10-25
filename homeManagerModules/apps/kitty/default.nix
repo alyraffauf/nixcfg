c4b8a2d168feb2b128a7086fa2e0dc6f87ffa6d7
Aly Raffauf <aly@raffauflabs.com>
2024-10-24 22:30:33 -0400

    kitty: add background blur

--- homeManagerModules/apps/kitty/default.nix
+++ homeManagerModules/apps/kitty/default.nix
@@ -11,7 +11,8 @@ in {
       enable = true;
 
       settings = {
-        confirm_os_window_close = "0";
+        background_blur = 1;
+        confirm_os_window_close = 0;
         notify_on_cmd_finish = "unfocused 10.0 command ${lib.getExe pkgs.libnotify} -i ${pkgs.kitty}/share/icons/hicolor/256x256/apps/kitty.png \"Job Finished\"";
         tab_bar_style = "powerline";
         window_padding_width = 5;
