c70d7e116d7c45c195d1d6ce132835d548fce098
Aly Raffauf <aly@raffauflabs.com>
2024-10-26 17:04:33 -0400

    kde: remove apps from default installation

--- nixosModules/desktop/kde/default.nix
+++ nixosModules/desktop/kde/default.nix
@@ -1,9 +1,21 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   config = lib.mkIf config.ar.desktop.kde.enable {
+    environment.plasma6.excludePackages = lib.attrsets.attrValues {
+      inherit
+        (pkgs.kdePackages)
+        elisa
+        gwenview
+        krdp
+        okular
+        oxygen
+        ;
+    };
+
     services.desktopManager.plasma6.enable = true;
   };
 }
