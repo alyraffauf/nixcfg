94c599752451b9ae33e958e58e4aa9e17413ca7f
Aly Raffauf <aly@raffauflabs.com>
2024-10-28 13:46:04 -0400

    gnome: remove unnecessary workaround for fprintd authentication

--- nixosModules/desktop/gnome/default.nix
+++ nixosModules/desktop/gnome/default.nix
@@ -23,7 +23,7 @@
     # ];
 
     security.pam.services = {
-      login.fprintAuth = false;
+      # login.fprintAuth = false;
 
       gdm = {
         enableGnomeKeyring = true;
@@ -31,25 +31,25 @@
         kwallet.enable = true;
       };
 
-      gdm-fingerprint = lib.mkIf (config.services.fprintd.enable) {
-        text = ''
-          auth       required                    pam_shells.so
-          auth       requisite                   pam_nologin.so
-          auth       requisite                   pam_faillock.so      preauth
-          auth       required                    ${pkgs.fprintd}/lib/security/pam_fprintd.so
-          auth       optional                    pam_permit.so
-          auth       required                    pam_env.so
-          auth       [success=ok default=1]      ${pkgs.gnome.gdm}/lib/security/pam_gdm.so
-          auth       optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so
+      # gdm-fingerprint = lib.mkIf (config.services.fprintd.enable) {
+      #   text = ''
+      #     auth       required                    pam_shells.so
+      #     auth       requisite                   pam_nologin.so
+      #     auth       requisite                   pam_faillock.so      preauth
+      #     auth       required                    ${pkgs.fprintd}/lib/security/pam_fprintd.so
+      #     auth       optional                    pam_permit.so
+      #     auth       required                    pam_env.so
+      #     auth       [success=ok default=1]      ${pkgs.gnome.gdm}/lib/security/pam_gdm.so
+      #     auth       optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so
 
-          account    include                     login
+      #     account    include                     login
 
-          password   required                    pam_deny.so
+      #     password   required                    pam_deny.so
 
-          session    include                     login
-          session    optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
-        '';
-      };
+      #     session    include                     login
+      #     session    optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
+      #   '';
+      # };
     };
 
     # Enable GNOME and GDM.
