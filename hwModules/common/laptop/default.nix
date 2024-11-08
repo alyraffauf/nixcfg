d7a03838527f83587f00841b1fe0aff88438ff67
Aly Raffauf <aly@raffauflabs.com>
2024-11-07 20:18:06 -0500

    hw/laptop:: only trigger ppd rules with ACAD

--- hwModules/common/laptop/default.nix
+++ hwModules/common/laptop/default.nix
@@ -18,8 +18,8 @@
     power-profiles-daemon.enable = lib.mkDefault true;
 
     udev.extraRules = ''
-      SUBSYSTEM=="power_supply", ATTR{online}=="1", ACTION=="change", RUN+="${lib.getExe pkgs.power-profiles-daemon} set balanced"
-      SUBSYSTEM=="power_supply", ATTR{online}=="0", ACTION=="change", RUN+="${lib.getExe pkgs.power-profiles-daemon} set power-saver"
+      SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_NAME}=="ACAD", ATTR{online}=="1", ACTION=="change", RUN+="${lib.getExe pkgs.power-profiles-daemon} set balanced"
+      SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_NAME}=="ACAD", ATTR{online}=="0", ACTION=="change", RUN+="${lib.getExe pkgs.power-profiles-daemon} set power-saver"
     '';
 
     upower.enable = lib.mkDefault true;
