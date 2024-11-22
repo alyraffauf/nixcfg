9d4625e9e891131d969540a470faba74d9051d4f
Aly Raffauf <aly@raffauflabs.com>
2024-11-22 15:16:48 -0500

    desktop: add pam rules for hyprlock

--- nixosModules/desktop/waylandComp.nix
+++ nixosModules/desktop/waylandComp.nix
@@ -16,6 +16,29 @@
     };
 
     security.pam.services = {
+      hyprlock = {
+        text = ''
+          # Account management.
+          account required pam_unix.so # unix (order 10900)
+
+          # Authentication management.
+          auth sufficient pam_unix.so likeauth try_first_pass likeauth nullok # unix (order 11500)
+          ${
+            lib.strings.optionalString config.services.fprintd.enable
+            "auth sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so # fprintd (order 11300)"
+          }
+
+          auth required pam_deny.so # deny (order 12300)
+
+          # Password management.
+          password sufficient pam_unix.so nullok yescrypt # unix (order 10200)
+
+          # Session management.
+          session required pam_env.so conffile=/etc/pam/environment readenv=0 # env (order 10100)
+          session required pam_unix.so # unix (order 10200)
+        '';
+      };
+
       swaylock = {
         text = ''
           # Account management.
