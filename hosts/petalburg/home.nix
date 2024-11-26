d4bd77acf933371a259713c3c591871061e0d504
Aly Raffauf <aly@raffauflabs.com>
2024-11-25 20:30:05 -0500

    petalburg: enable easyeffects

--- hosts/petalburg/home.nix
+++ hosts/petalburg/home.nix
@@ -9,7 +9,14 @@
           };
         };
 
-        ar.home.desktop.hyprland.laptopMonitors = ["desc:China Star Optoelectronics Technology Co. Ltd MNG007QA1-1,1920x1200@165, 0x0, 1.25, vrr, 1"];
+        ar.home = {
+          desktop.hyprland.laptopMonitors = ["desc:China Star Optoelectronics Technology Co. Ltd MNG007QA1-1,1920x1200@165, 0x0, 1.25, vrr, 1"];
+
+          services.easyeffects = {
+            enable = true;
+            preset = "LoudnessEqualizer";
+          };
+        };
       }
     ];
 
