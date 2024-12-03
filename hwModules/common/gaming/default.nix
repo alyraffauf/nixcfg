0dd273d214092c368367426a6d14ea9025be2c2f
Aly Raffauf <aly@raffauflabs.com>
2024-12-03 08:57:56 -0500

    hw/gaming: fixup overrides

--- hwModules/common/gaming/default.nix
+++ hwModules/common/gaming/default.nix
@@ -1,28 +1,28 @@
 {lib, ...}: {
   boot.kernel.sysctl = {
     # Improved file monitoring
-    "fs.inotify.max_user_instances" = 8192;
-    "fs.inotify.max_user_watches" = 524288;
+    "fs.inotify.max_user_instances" = lib.mkOverride 100 8192; # Re-use the default from Bazzite even though the default NixOS value is higher.
+    # "fs.inotify.max_user_watches" = lib.mkOverride 100 524288;
 
     # Disable watchdogs for maximum performance at the cost of resiliency
-    "kernel.nmi_watchdog" = 0;
-    "kernel.soft_watchdog" = 0;
-    "kernel.split_lock_mitigate" = 0;
-    "kernel.watchdog" = 0;
+    "kernel.nmi_watchdog" = lib.mkOverride 100 0;
+    "kernel.soft_watchdog" = lib.mkDefault 0;
+    # "kernel.split_lock_mitigate" = lib.mkOverride 100 0;
+    "kernel.watchdog" = lib.mkDefault 0;
 
     # Network optimizations
-    "net.core.default_qdisc" = "fq";
-    "net.ipv4.tcp_congestion_control" = "bbr";
+    "net.core.default_qdisc" = lib.mkDefault "fq";
+    "net.ipv4.tcp_congestion_control" = lib.mkDefault "bbr";
     "net.ipv4.tcp_mtu_probing" = lib.mkForce 1;
 
     # Memory management
-    "vm.dirty_background_bytes" = 134217728;
-    "vm.dirty_bytes" = 268435456;
-    "vm.max_map_count" = lib.mkForce 2147483642;
-    "vm.page-cluster" = 0;
-    "vm.swappiness" = 180;
-    "vm.watermark_boost_factor" = 0;
-    "vm.watermark_scale_factor" = 125;
+    "vm.dirty_background_bytes" = lib.mkDefault 134217728;
+    "vm.dirty_bytes" = lib.mkDefault 268435456;
+    # "vm.max_map_count" = lib.mkOverride 100 2147483642;
+    "vm.page-cluster" = lib.mkDefault 0;
+    "vm.swappiness" = lib.mkDefault 180;
+    "vm.watermark_boost_factor" = lib.mkDefault 0;
+    "vm.watermark_scale_factor" = lib.mkDefault 125;
   };
 
   services.udev.extraRules = ''
