d8799acec07dc387b83186f2ee952fd48324fcf6
Aly Raffauf <aly@raffauflabs.com>
2024-12-04 08:18:39 -0500

    theme: migrate to stylix iconTheme setting

--- homeManagerModules/theme.nix
+++ homeManagerModules/theme.nix
@@ -12,60 +12,60 @@ in {
       pkgs.liberation_ttf
     ];
 
-    gtk.iconTheme = {
-      name =
-        if config.stylix.polarity == "dark"
-        then "Papirus-Dark"
-        else "Papirus";
-
-      package = pkgs.papirus-icon-theme.override {color = "adwaita";};
-    };
-
     qt = lib.mkIf (!config.ar.home.desktop.kde.enable) {
       enable = true;
       platformTheme.name = "kde";
       style.name = "Breeze";
     };
 
-    stylix.targets.gtk.extraCss = builtins.concatStringsSep "\n" [
-      (lib.optionalString ((cfg.desktop.hyprland.enable || cfg.desktop.sway.enable) && !cfg.desktop.gnome.enable) ''
-        window.background { border-radius: ${toString cfg.theme.borders.radius}; }
-      '')
+    stylix = {
+      iconTheme = {
+        enable = true;
+        dark = "Papirus-Dark";
+        light = "Papirus";
+        package = pkgs.papirus-icon-theme.override {color = "adwaita";};
+      };
 
-      (lib.optionalString (
-          (cfg.desktop.hyprland.enable || cfg.desktop.sway.enable) && (config.stylix.polarity == "light") && !cfg.desktop.gnome.enable
-        ) ''
-          tooltip {
-            &.background { background-color: alpha(${config.lib.stylix.colors.withHashtag.base05}, ${builtins.toString config.stylix.opacity.popups}); }
-            background-color: alpha(${config.lib.stylix.colors.withHashtag.base05}, ${builtins.toString config.stylix.opacity.popups});
+      targets.gtk.extraCss = builtins.concatStringsSep "\n" [
+        (lib.optionalString ((cfg.desktop.hyprland.enable || cfg.desktop.sway.enable) && !cfg.desktop.gnome.enable) ''
+          window.background { border-radius: ${toString cfg.theme.borders.radius}; }
+        '')
+
+        (lib.optionalString (
+            (cfg.desktop.hyprland.enable || cfg.desktop.sway.enable) && (config.stylix.polarity == "light") && !cfg.desktop.gnome.enable
+          ) ''
+            tooltip {
+              &.background { background-color: alpha(${config.lib.stylix.colors.withHashtag.base05}, ${builtins.toString config.stylix.opacity.popups}); }
+              background-color: alpha(${config.lib.stylix.colors.withHashtag.base05}, ${builtins.toString config.stylix.opacity.popups});
+            }'')
+
+        (lib.optionalString (cfg.theme.gtk.hideTitleBar && !cfg.desktop.gnome.enable) ''
+          /* No (default) title bar on wayland */
+          headerbar.default-decoration {
+            /* You may need to tweak these values depending on your GTK theme */
+            border-radius: 0;
+            border: 0;
+            box-shadow: none;
+            font-size: 0;
+            margin-bottom: 50px;
+            margin-top: -100px;
+            min-height: 0;
+            padding: 0;
+          }
+
+          .titlebar,
+          .titlebar .background
+          {
+            border-radius: 0;
+          }
+
+          /* rm -rf window shadows */
+          window.csd,             /* gtk4? */
+          window.csd decoration { /* gtk3 */
+            border-radius: 0;
+            box-shadow: none;
           }'')
-
-      (lib.optionalString (cfg.theme.gtk.hideTitleBar && !cfg.desktop.gnome.enable) ''
-        /* No (default) title bar on wayland */
-        headerbar.default-decoration {
-          /* You may need to tweak these values depending on your GTK theme */
-          border-radius: 0;
-          border: 0;
-          box-shadow: none;
-          font-size: 0;
-          margin-bottom: 50px;
-          margin-top: -100px;
-          min-height: 0;
-          padding: 0;
-        }
-
-        .titlebar,
-        .titlebar .background
-        {
-          border-radius: 0;
-        }
-
-        /* rm -rf window shadows */
-        window.csd,             /* gtk4? */
-        window.csd decoration { /* gtk3 */
-          border-radius: 0;
-          box-shadow: none;
-        }'')
-    ];
+      ];
+    };
   };
 }
