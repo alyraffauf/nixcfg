5f8647cac93000608ff3b6e60f1461b2e522992e
Aly Raffauf <aly@raffauflabs.com>
2024-12-06 17:55:24 -0500

    flake: remove jovian input and cache

--- common/nix.nix
+++ common/nix.nix
@@ -23,14 +23,12 @@
       substituters = [
         "https://alyraffauf.cachix.org"
         "https://cache.nixos.org/"
-        "https://jovian-nixos.cachix.org"
         "https://nix-community.cachix.org"
         "https://nix-gaming.cachix.org"
       ];
 
       trusted-public-keys = [
         "alyraffauf.cachix.org-1:GQVrRGfjTtkPGS8M6y7Ik0z4zLt77O0N25ynv2gWzDM="
-        "jovian-nixos.cachix.org-1:mAWLjAxLNlfxAnozUjOqGj4AxQwCl7MXwOfu7msVlAo="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       ];
