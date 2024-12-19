6bf5eee591dbc553181cedc1b2da2660cef1baa3
Aly Raffauf <aly@raffauflabs.com>
2024-12-19 11:18:47 -0500

    kde: remove package exclusions

--- nixosModules/desktop/kde/default.nix
+++ nixosModules/desktop/kde/default.nix
@@ -1,20 +1,19 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }: {
   config = lib.mkIf config.ar.desktop.kde.enable {
-    environment.plasma6.excludePackages = lib.attrsets.attrValues {
-      inherit
-        (pkgs.kdePackages)
-        elisa
-        gwenview
-        krdp
-        okular
-        oxygen
-        ;
-    };
+    # environment.plasma6.excludePackages = lib.attrsets.attrValues {
+    #   inherit
+    #     (pkgs.kdePackages)
+    #     elisa
+    #     gwenview
+    #     krdp
+    #     okular
+    #     oxygen
+    #     ;
+    # };
 
     services.desktopManager.plasma6.enable = true;
   };
