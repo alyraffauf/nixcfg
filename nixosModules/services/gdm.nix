7540be056c4afd8e2d259d59648efd033befc739
Aly Raffauf <aly@raffauflabs.com>
2025-01-01 15:17:24 -0500

    nixos/gdm: add autologin option

--- nixosModules/services/gdm.nix
+++ nixosModules/services/gdm.nix
@@ -1,24 +1,43 @@
-{...}: {
-  programs.dconf.profiles.gdm.databases = [
-    {
-      settings = {
-        "org/gnome/desktop/peripherals/keyboard" = {
-          numlock-state = true;
-          remember-numlock-state = true;
-        };
-
-        "org/gnome/desktop/peripherals/touchpad" = {
-          tap-to-click = true;
-        };
-      };
-    }
-  ];
-
-  security.pam.services.gdm = {
-    enableGnomeKeyring = true;
-    gnupg.enable = true;
-    kwallet.enable = true;
+{
+  config,
+  lib,
+  ...
+}: {
+  options.gdm.autologin = lib.mkOption {
+    description = "User to autologin.";
+    default = null;
+    type = lib.types.nullOr lib.types.str;
   };
 
-  services.xserver.displayManager.gdm.enable = true;
+  config = {
+    programs.dconf.profiles.gdm.databases = [
+      {
+        settings = {
+          "org/gnome/desktop/peripherals/keyboard" = {
+            numlock-state = true;
+            remember-numlock-state = true;
+          };
+
+          "org/gnome/desktop/peripherals/touchpad" = {
+            tap-to-click = true;
+          };
+        };
+      }
+    ];
+
+    security.pam.services.gdm = {
+      enableGnomeKeyring = true;
+      gnupg.enable = true;
+      kwallet.enable = true;
+    };
+
+    services.xserver.displayManager = {
+      autoLogin = lib.mkIf (config.gdm.autologin != null) {
+        enable = true;
+        user = config.gdm.autologin;
+      };
+
+      gdm.enable = true;
+    };
+  };
 }
