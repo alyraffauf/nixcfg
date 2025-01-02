a6df5a81fb4dfcf9d2fe510c380b926e887d4517
Aly Raffauf <aly@raffauflabs.com>
2025-01-02 00:09:34 -0500

    common/tailscale: add firewall rules

--- common/tailscale.nix
+++ common/tailscale.nix
@@ -1,6 +1,11 @@
 {config, ...}: {
   age.secrets.tailscaleAuthKey.file = ../secrets/tailscale/authKeyFile.age;
 
+  networking.firewall = {
+    allowedUDPPorts = [config.services.tailscale.port];
+    trustedInterfaces = [config.services.tailscale.interfaceName];
+  };
+
   services.tailscale = {
     enable = true;
     authKeyFile = config.age.secrets.tailscaleAuthKey.path;
