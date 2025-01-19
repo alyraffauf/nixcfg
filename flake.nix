5de9239994793110075617b4a5f0ee901c074a78
Aly Raffauf <aly@raffauflabs.com>
2025-01-19 00:27:00 +0000

    disko, locale: migrate to folder structure

--- flake.nix
+++ flake.nix
@@ -139,35 +139,27 @@
     nixosModules = {
       common-mauville-share = import ./modules/common/samba.nix;
       common-wifi-profiles = import ./modules/common/wifi.nix;
-
-      disko-btrfs-subvolumes = import ./modules/disko/btrfs-subvolumes.nix;
-      disko-luks-btrfs-subvolumes = import ./modules/disko/luks-btrfs-subvolumes.nix;
-
-      hw-common = import ./modules/hardware/common;
-
+      disko-btrfs-subvolumes = import ./modules/disko/btrfs-subvolumes;
+      disko-luks-btrfs-subvolumes = import ./modules/disko/luks-btrfs-subvolumes;
       hw-amd-cpu = import ./modules/hardware/amd/cpu;
       hw-amd-gpu = import ./modules/hardware/amd/gpu;
-      hw-intel-cpu = import ./modules/hardware/intel/cpu;
-      hw-intel-gpu = import ./modules/hardware/intel/gpu;
-      hw-nvidia-gpu = import ./modules/hardware/nvidia/gpu;
-
-      hw-profiles-laptop = import ./modules/hardware/profiles/laptop.nix;
-
       hw-beelink-mini-s12pro = import ./modules/hardware/beelink/mini/s12pro;
+      hw-common = import ./modules/hardware/common;
       hw-framework-13-amd-7000 = import ./modules/hardware/framework/13/amd-7000;
       hw-framework-13-intel-11th = import ./modules/hardware/framework/13/intel-11th;
+      hw-intel-cpu = import ./modules/hardware/intel/cpu;
+      hw-intel-gpu = import ./modules/hardware/intel/gpu;
       hw-lenovo-thinkcentre-m700 = import ./modules/hardware/lenovo/thinkcentre/m700;
-      hw-lenovo-thinkpad-T440p = import ./modules/hardware/lenovo/thinkpad/T440p;
       hw-lenovo-thinkpad-5D50X = import ./modules/hardware/lenovo/thinkpad/5D50X;
+      hw-lenovo-thinkpad-T440p = import ./modules/hardware/lenovo/thinkpad/T440p;
       hw-lenovo-yoga-16IMH9 = import ./modules/hardware/lenovo/yoga/16IMH9;
-
-      locale-en-us = import ./modules/locale/en-us.nix;
-
+      hw-nvidia-gpu = import ./modules/hardware/nvidia/gpu;
+      hw-profiles-laptop = import ./modules/hardware/profiles/laptop.nix;
+      locale-en-us = import ./modules/locale/en-us;
       nixos-desktop = import ./modules/nixos/desktop;
       nixos-desktop-gnome = import ./modules/nixos/desktop/gnome;
       nixos-desktop-hyprland = import ./modules/nixos/desktop/hyprland;
       nixos-desktop-kde = import ./modules/nixos/desktop/kde;
-
       nixos-profiles-autoUpgrade = import ./modules/nixos/profiles/autoUpgrade;
       nixos-profiles-base = import ./modules/nixos/profiles/base;
       nixos-profiles-btrfs = import ./modules/nixos/profiles/btrfs;
@@ -175,7 +167,6 @@
       nixos-profiles-gaming = import ./modules/nixos/profiles/gaming;
       nixos-profiles-lanzaboote = import ./modules/nixos/profiles/lanzaboote;
       nixos-profiles-serverOptimizations = import ./modules/nixos/profiles/serverOptimizations;
-
       nixos-programs-firefox = import ./modules/nixos/programs/firefox;
       nixos-programs-nicotine-plus = import ./modules/nixos/programs/nicotine-plus;
       nixos-programs-nix = import ./modules/nixos/programs/nix;
@@ -183,14 +174,12 @@
       nixos-programs-retroarch = import ./modules/nixos/programs/retroarch;
       nixos-programs-steam = import ./modules/nixos/programs/steam;
       nixos-programs-virt-manager = import ./modules/nixos/programs/virt-manager;
-
       nixos-services-flatpak = import ./modules/nixos/services/flatpak;
       nixos-services-gdm = import ./modules/nixos/services/gdm;
       nixos-services-greetd = import ./modules/nixos/services/greetd;
       nixos-services-sddm = import ./modules/nixos/services/sddm;
       nixos-services-sunshine = import ./modules/nixos/services/sunshine;
       nixos-services-tailscale = import ./modules/nixos/services/tailscale;
-
       users = import ./modules/users;
     };
 
