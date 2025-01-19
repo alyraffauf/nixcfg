22bdbffba3dc6ae76b475cc2dcb65f59a3670607
Aly Raffauf <aly@raffauflabs.com>
2025-01-19 16:31:23 -0500

    hardware/16IMH9: break out speaker script and remove torubleshooting cruft

---
+++ modules/hardware/lenovo/yoga/16IMH9/turn-on-speakers.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+# Compliments to the chef: https://github.com/maximmaxim345/yoga_pro_9i_gen9_linux/blob/b7f0fb294c010ba424fb577532091a5daa7fbae4/README.md
+
+export TERM=linux
+
+# Some distros don't have i2c-dev module loaded by default, so we load it manually
+modprobe i2c-dev
+
+# Function to find the correct I2C bus (third DesignWare adapter)
+find_i2c_bus() {
+    adapter_description="Synopsys DesignWare I2C adapter"
+    dw_count=$(i2cdetect -l | grep -c "$adapter_description")
+    if [ "$dw_count" -lt 3 ]; then
+        echo "Error: Less than 3 DesignWare I2C adapters found." >&2
+        return 1
+    fi
+    bus_number=$(i2cdetect -l | grep "$adapter_description" | awk '{print $1}' | sed 's/i2c-//' | sed -n '3p')
+    echo "$bus_number"
+}
+i2c_bus=$(find_i2c_bus)
+if [ -z "$i2c_bus" ]; then
+    echo "Error: Could not find the third DesignWare I2C bus for the audio IC." >&2
+    exit 1
+fi
+echo "Using I2C bus: $i2c_bus"
+i2c_addr=(0x3f 0x38)
+count=0
+for value in "${i2c_addr[@]}"; do
+    val=$((count % 2))
+    i2cset -f -y "$i2c_bus" "$value" 0x00 0x00
+    i2cset -f -y "$i2c_bus" "$value" 0x7f 0x00
+    i2cset -f -y "$i2c_bus" "$value" 0x01 0x01
+    i2cset -f -y "$i2c_bus" "$value" 0x0e 0xc4
+    i2cset -f -y "$i2c_bus" "$value" 0x0f 0x40
+    i2cset -f -y "$i2c_bus" "$value" 0x5c 0xd9
+    i2cset -f -y "$i2c_bus" "$value" 0x60 0x10
+    if [ $val -eq 0 ]; then
+        i2cset -f -y "$i2c_bus" "$value" 0x0a 0x1e
+    else
+        i2cset -f -y "$i2c_bus" "$value" 0x0a 0x2e
+    fi
+    i2cset -f -y "$i2c_bus" "$value" 0x0d 0x01
+    i2cset -f -y "$i2c_bus" "$value" 0x16 0x40
+    i2cset -f -y "$i2c_bus" "$value" 0x00 0x01
+    i2cset -f -y "$i2c_bus" "$value" 0x17 0xc8
+    i2cset -f -y "$i2c_bus" "$value" 0x00 0x04
+    i2cset -f -y "$i2c_bus" "$value" 0x30 0x00
+    i2cset -f -y "$i2c_bus" "$value" 0x31 0x00
+    i2cset -f -y "$i2c_bus" "$value" 0x32 0x00
+    i2cset -f -y "$i2c_bus" "$value" 0x33 0x01
+    i2cset -f -y "$i2c_bus" "$value" 0x00 0x08
+    i2cset -f -y "$i2c_bus" "$value" 0x18 0x00
+    i2cset -f -y "$i2c_bus" "$value" 0x19 0x00
+    i2cset -f -y "$i2c_bus" "$value" 0x1a 0x00
+    i2cset -f -y "$i2c_bus" "$value" 0x1b 0x00
+    i2cset -f -y "$i2c_bus" "$value" 0x28 0x40
+    i2cset -f -y "$i2c_bus" "$value" 0x29 0x00
+    i2cset -f -y "$i2c_bus" "$value" 0x2a 0x00
+    i2cset -f -y "$i2c_bus" "$value" 0x2b 0x00
+    i2cset -f -y "$i2c_bus" "$value" 0x00 0x0a
+    i2cset -f -y "$i2c_bus" "$value" 0x48 0x00
+    i2cset -f -y "$i2c_bus" "$value" 0x49 0x00
+    i2cset -f -y "$i2c_bus" "$value" 0x4a 0x00
+    i2cset -f -y "$i2c_bus" "$value" 0x4b 0x00
+    i2cset -f -y "$i2c_bus" "$value" 0x58 0x40
+    i2cset -f -y "$i2c_bus" "$value" 0x59 0x00
+    i2cset -f -y "$i2c_bus" "$value" 0x5a 0x00
+    i2cset -f -y "$i2c_bus" "$value" 0x5b 0x00
+    i2cset -f -y "$i2c_bus" "$value" 0x00 0x00
+    i2cset -f -y "$i2c_bus" "$value" 0x02 0x00
+    count=$((count + 1))
+done
+
+amixer -c 0 sset Headphone unmute # Unmute Headphones, otherwise they are not unmuted when speakers are turned off.
