ee72564bf4248256102dab0073da126363b2d587
Aly Raffauf <aly@raffauflabs.com>
2025-01-19 19:23:04 -0500

    petalburg: move from kde -> hyprland

--- hosts/petalburg/default.nix
+++ hosts/petalburg/default.nix
@@ -1,6 +1,8 @@
 # Lenovo Yoga Pro 9i with Intel Meteor Lake Core Ultra 9 + NVIDIA 4050 GPU, 32GB RAM, 1TB SSD, and a 16" display.
 {
   config,
+  lib,
+  pkgs,
   self,
   ...
 }: {
@@ -11,7 +13,7 @@
     self.nixosModules.disko-luks-btrfs-subvolumes
     self.nixosModules.hw-lenovo-yoga-16IMH9
     self.nixosModules.locale-en-us
-    self.nixosModules.nixos-desktop-kde
+    self.nixosModules.nixos-desktop-hyprland
     self.nixosModules.nixos-profiles-autoUpgrade
     self.nixosModules.nixos-profiles-base
     self.nixosModules.nixos-profiles-btrfs
@@ -24,7 +26,7 @@
     self.nixosModules.nixos-programs-nix
     self.nixosModules.nixos-programs-podman
     self.nixosModules.nixos-programs-steam
-    self.nixosModules.nixos-services-sddm
+    self.nixosModules.nixos-services-greetd
     self.nixosModules.nixos-services-syncthing
     self.nixosModules.nixos-services-tailscale
   ];
@@ -39,7 +41,35 @@
     };
   };
 
-  services.pipewire.lowLatency.enable = false;
+  services = {
+    pipewire.lowLatency.enable = false;
+
+    udev.extraRules = lib.mkIf config.programs.hyprland.enable (let
+      hyprlandDynamicRes = pkgs.writeShellScript "hyprland-dynamic-resolution" ''
+        MON="eDP-1"
+        RES="3200x2000"
+
+        for dir in /run/user/*; do
+          for hypr_dir in "$dir/hypr/"*/; do
+            socket="''${hypr_dir}.socket.sock"
+            if [[ -S $socket ]]; then
+              monitor_info=$(echo -e "monitors" | ${lib.getExe pkgs.socat} - UNIX-CONNECT:"$socket")
+
+              if echo "$monitor_info" | grep -q "$MON"; then
+                echo -e "keyword monitor $MON, $RES@$1, 0x0, 2, vrr, $2" | ${lib.getExe pkgs.socat} - UNIX-CONNECT:"$socket"
+              fi
+
+            fi
+          done
+        done
+      '';
+    in ''
+      ## Dynamic refresh rate for Hyprland
+      ACTION=="change", SUBSYSTEM=="power_supply", ATTRS{type}=="Mains", ATTRS{online}=="0", RUN+="${hyprlandDynamicRes} 60 0"
+      ACTION=="change", SUBSYSTEM=="power_supply", ATTRS{type}=="Mains", ATTRS{online}=="1", RUN+="${hyprlandDynamicRes} 165 1"
+    '');
+  };
+
   system.stateVersion = "25.05";
   time.timeZone = "America/New_York";
 
--- hosts/petalburg/home.nix
+++ hosts/petalburg/home.nix
@@ -2,6 +2,10 @@
   home-manager = {
     sharedModules = [
       {
+        myHome = {
+          desktop.hyprland.laptopMonitors = ["desc:Lenovo Group Limited 0x8BA1 0x00006003,3200x2000@165, 0x0, 2, vrr, 1"];
+          laptopMode = true;
+        };
       }
     ];
 
