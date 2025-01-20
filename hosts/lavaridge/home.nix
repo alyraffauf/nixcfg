c1833b6cbd1c63ce36abfc4ec6fd8bfa275a4682
Aly Raffauf <aly@raffauflabs.com>
2025-01-20 01:16:23 -0500

    lavaridge: remove easyeffects

--- hosts/lavaridge/home.nix
+++ hosts/lavaridge/home.nix
@@ -5,13 +5,6 @@
         myHome = {
           desktop.hyprland.laptopMonitors = ["desc:BOE NE135A1M-NY1,2880x1920@60, 0x0, 2, vrr, 0"];
           laptopMode = true;
-
-          services = {
-            easyeffects = {
-              enable = true;
-              preset = "fw13-autogain";
-            };
-          };
         };
       }
     ];
