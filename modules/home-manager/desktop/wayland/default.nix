49c7d6613232520b04a31ceb3d9cf6b9e30b185c
Aly Raffauf <aly@raffauflabs.com>
2025-01-22 17:01:54 -0500

    home/wayland: don't enable pipewire-inhibit-idle

--- modules/home-manager/desktop/wayland/default.nix
+++ modules/home-manager/desktop/wayland/default.nix
@@ -15,7 +15,6 @@
       services = {
         hypridle.enable = lib.mkDefault config.myHome.desktop.hyprland.enable;
         mako.enable = lib.mkDefault true;
-        pipewire-inhibit.enable = lib.mkDefault true;
         swayosd.enable = lib.mkDefault true;
         waybar.enable = lib.mkDefault true;
       };
