318ed905823bed4920f9ff3670e664f64efb70e2
Aly Raffauf <aly@raffauflabs.com>
2025-01-25 18:02:37 -0500

    home/hypridle: fixup and improve timeouts

--- modules/home-manager/services/hypridle/default.nix
+++ modules/home-manager/services/hypridle/default.nix
@@ -7,69 +7,71 @@
   cfg = config.myHome;
 in {
   config = lib.mkIf cfg.services.hypridle.enable {
-    programs.hyprlock.settings = {
+    programs.hyprlock = {
       enable = true;
 
-      auth.fingerprint = {
-        enabled = true;
-        present_message = "Scanning fingerprint...";
-        ready_message = "Scan fingerprint to unlock.";
-      };
+      settings = {
+        auth.fingerprint = {
+          enabled = true;
+          present_message = "Scanning fingerprint...";
+          ready_message = "Scan fingerprint to unlock.";
+        };
 
-      input-field = {
-        font_family = config.stylix.fonts.sansSerif.name;
-        rounding = config.myHome.theme.borders.radius;
-      };
-
-      shape = [
-        {
-          border_color = "rgb(${config.lib.stylix.colors.base0D})";
-          border_size = 8;
-          color = "rgb(${config.lib.stylix.colors.base00})";
-          halign = "center";
-          position = "-33%, 0";
-          rotate = 0;
+        input-field = {
+          font_family = config.stylix.fonts.sansSerif.name;
           rounding = config.myHome.theme.borders.radius;
-          shadow_color = "rgb(${config.lib.stylix.colors.base01})";
-          shadow_passes = 3;
-          size = "600, 600";
-          valign = "center";
-          xray = false;
-        }
-      ];
+        };
 
-      label = [
-        {
-          color = "rgb(${config.lib.stylix.colors.base05})";
-          font_family = config.stylix.fonts.sansSerif.name;
-          font_size = 40;
-          halign = "center";
-          position = "-33%, 200";
-          shadow_passes = 0;
-          text = "Hi $DESC 👋";
-          valign = "center";
-        }
-        {
-          color = "rgb(${config.lib.stylix.colors.base05})";
-          font_family = config.stylix.fonts.sansSerif.name;
-          font_size = 40;
-          halign = "center";
-          position = "-33%, -200";
-          shadow_passe = 0;
-          text = "It is $TIME12.";
-          valign = "center";
-        }
-        {
-          color = "rgb(${config.lib.stylix.colors.base05})";
-          font_family = config.stylix.fonts.sansSerif.name;
-          font_size = 24;
-          halign = "center";
-          position = "-33%, 0";
-          shadow_passes = 0;
-          text = "$FPRINTMESSAGE";
-          valign = "center";
-        }
-      ];
+        shape = [
+          {
+            border_color = "rgb(${config.lib.stylix.colors.base0D})";
+            border_size = 8;
+            color = "rgb(${config.lib.stylix.colors.base00})";
+            halign = "center";
+            position = "-33%, 0";
+            rotate = 0;
+            rounding = config.myHome.theme.borders.radius;
+            shadow_color = "rgb(${config.lib.stylix.colors.base01})";
+            shadow_passes = 3;
+            size = "600, 600";
+            valign = "center";
+            xray = false;
+          }
+        ];
+
+        label = [
+          {
+            color = "rgb(${config.lib.stylix.colors.base05})";
+            font_family = config.stylix.fonts.sansSerif.name;
+            font_size = 40;
+            halign = "center";
+            position = "-33%, 200";
+            shadow_passes = 0;
+            text = "Hi $DESC 👋";
+            valign = "center";
+          }
+          {
+            color = "rgb(${config.lib.stylix.colors.base05})";
+            font_family = config.stylix.fonts.sansSerif.name;
+            font_size = 40;
+            halign = "center";
+            position = "-33%, -200";
+            shadow_passe = 0;
+            text = "It is $TIME12.";
+            valign = "center";
+          }
+          {
+            color = "rgb(${config.lib.stylix.colors.base05})";
+            font_family = config.stylix.fonts.sansSerif.name;
+            font_size = 24;
+            halign = "center";
+            position = "-33%, 0";
+            shadow_passes = 0;
+            text = "$FPRINTMESSAGE";
+            valign = "center";
+          }
+        ];
+      };
     };
 
     services.hypridle = {
@@ -78,46 +80,40 @@ in {
       settings = {
         general = {
           after_sleep_cmd = "hyprctl dispatch dpms on";
-          lock_cmd = "hyprlock";
-          before_sleep_cmd = "hyprlock --immediate --no-fade-in";
+          lock_cmd = "pidof hyprlock || hyprlock";
+          before_sleep_cmd = "loginctl lock-session";
         };
 
         listener =
           [
             {
               timeout = 30;
-              on-timeout = "brightnessctl -sd chromeos::kbd_backlight set 0";
-              on-resume = "brightnessctl -rd chromeos::kbd_backlight";
+              on-timeout = "${pkgs.brightnessctl}/bin/brightnessctl -sd chromeos::kbd_backlight set 0";
+              on-resume = "${pkgs.brightnessctl}/bin/brightnessctl -rd chromeos::kbd_backlight";
             }
             {
               timeout = 30;
-              on-timeout = "brightnessctl -sd platform::kbd_backlight set 0";
-              on-resume = "brightnessctl -rd platform::kbd_backlight";
+              on-timeout = "${pkgs.brightnessctl}/bin/brightnessctl -sd platform::kbd_backlight set 0";
+              on-resume = "${pkgs.brightnessctl}/bin/brightnessctl -rd platform::kbd_backlight";
             }
             {
-              timeout = 120;
-              on-timeout = "brightnessctl -s set 10";
-              on-resume = "brightnessctl -r";
+              timeout = 60;
+              on-timeout = "${pkgs.brightnessctl}/bin/brightnessctl -s set 10";
+              on-resume = "${pkgs.brightnessctl}/bin/brightnessctl -r";
+            }
+            {
+              timeout = 300;
+              on-timeout = "loginctl lock-session";
+            }
+            {
+              timeout = 360;
+              on-timeout = "hyprctl dispatch dpms off";
+              on-resume = "hyprctl dispatch dpms on";
             }
           ]
-          ++ lib.optional (!cfg.desktop.autoSuspend) {
-            timeout = 600;
-            on-timeout = "hyprlock";
-          }
           ++ lib.optional cfg.desktop.autoSuspend {
             timeout = 600;
             on-timeout = "systemctl suspend";
-          }
-          ++ lib.optional
-          (!cfg.desktop.autoSuspend) {
-            timeout = 630;
-            on-timeout = "hyprctl dispatch dpms off";
-          }
-          ++ lib.optional (!cfg.desktop.autoSuspend && cfg.desktop.hyprland.enable)
-          {
-            timeout = 630;
-            on-timeout = "hyprctl dispatch dpms off";
-            on-resume = "hyprctl dispatch dpms on";
           };
       };
     };
@@ -131,7 +127,7 @@ in {
             lib.makeBinPath (
               (with pkgs; [
                 bash
-                brightnessctl
+                brightnessctl # Not working as of 2025-01-25, needs nixpkgs interpolation to work.
                 coreutils
                 hyprlock
                 systemd
@@ -140,6 +136,7 @@ in {
             )
           }"
         ];
+
         Restart = lib.mkForce "no";
       };
 
