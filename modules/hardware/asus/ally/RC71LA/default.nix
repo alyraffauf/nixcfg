2fc8269d814a10b907dfc48052846f225d8900b2
Aly Raffauf <aly@raffauflabs.com>
2025-01-30 09:30:25 -0500

    hardware/RC72LA: add wireplumber rule to prefer HDMI output

--- modules/hardware/asus/ally/RC71LA/default.nix
+++ modules/hardware/asus/ally/RC71LA/default.nix
@@ -27,6 +27,28 @@
         ui.enable = lib.mkDefault true;
       };
 
+      pipewire.wireplumber.configPackages = [
+        pkgs.writeTextDir
+        "share/wireplumber/wireplumber.conf.d/51-preferHDMI.conf"
+        ''
+          monitor.alsa.rules = [
+            {
+              matches = [
+                {
+                  node.name = "alsa_output.pci-0000_64_00.1.hdmi-stereo"
+                }
+              ]
+              actions = {
+                update-props = {
+                  priority.driver = 1100
+                  priority.session = 1100
+                }
+              }
+            }
+          ]
+        ''
+      ];
+
       power-profiles-daemon.enable = lib.mkDefault true;
       upower.enable = lib.mkDefault true;
     };
