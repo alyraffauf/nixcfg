6d966ef0ef8fd823ab02654c5c6d8ac6ba03895d
Aly Raffauf <aly@raffauflabs.com>
2025-01-31 23:02:26 -0500

    homes/dustin: migrate and disable unused services

--- homes/dustin/default.nix
+++ homes/dustin/default.nix
@@ -52,9 +52,9 @@
     apps = {
       chromium.enable = true;
       firefox.enable = true;
-      kitty.enable = true;
       shell.enable = true;
       vsCodium.enable = true;
+      wezterm.enable = true;
     };
 
     defaultApps.enable = true;
@@ -65,11 +65,6 @@
         "desc:HP Inc. HP 24mh 3CM037248S,preferred,-1920x0,auto"
         "desc:LG Electronics LG IPS QHD 109NTWG4Y865,preferred,-2560x0,auto"
       ];
-    };
-
-    services = {
-      gammastep.enable = true;
-      randomWallpaper.enable = false;
     };
 
     theme.enable = true;
