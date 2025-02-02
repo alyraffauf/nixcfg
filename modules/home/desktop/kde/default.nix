5d028f4247e4419c4e21393e6f2c028e6c5782fa
Aly Raffauf <aly@raffauflabs.com>
2025-02-01 23:52:34 -0500

    home/kde: remove duplicate dconf declarations

--- modules/home/desktop/kde/default.nix
+++ modules/home/desktop/kde/default.nix
@@ -7,9 +7,6 @@
 
       settings = {
         "org/gnome/desktop/wm/preferences".button-layout = "appmenu:minimize,maximize,close";
-        "org/gnome/nm-applet".disable-connected-notifications = true;
-        "org/gtk/gtk4/settings/file-chooser".sort-directories-first = true;
-        "org/gtk/settings/file-chooser".sort-directories-first = true;
       };
     };
   };
