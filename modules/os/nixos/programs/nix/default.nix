a80f9f42ed6eed2e88165caccd6497d7683ac261
Aly Raffauf <aly@raffauflabs.com>
2025-02-03 15:55:51 -0500

    nixos/nix: garbage collect older than 7 days

--- modules/os/nixos/programs/nix/default.nix
+++ modules/os/nixos/programs/nix/default.nix
@@ -10,7 +10,7 @@
       gc = {
         automatic = true;
         dates = "daily";
-        options = "--delete-older-than 14d";
+        options = "--delete-older-than 7d";
         persistent = true;
         randomizedDelaySec = "60min";
       };
