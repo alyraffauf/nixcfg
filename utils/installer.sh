446aa50913a50ebedfd2aa7701176d1ccdd7df44
Aly Raffauf <aly@raffauflabs.com>
2025-02-04 16:25:41 -0500

    installer: build host with substituters before installing

--- utils/installer.sh
+++ utils/installer.sh
@@ -4,8 +4,10 @@ set -euo pipefail
 
 read -rp "Which host are you installing? " HOST
 
-FLAKE=github:alyraffauf/nixcfg#$HOST
-echo "Installing from $FLAKE"
+FLAKE=github:alyraffauf/nixcfg
+FLAKE_NIXOS="$FLAKE"#"$HOST"
+
+echo "Installing from $FLAKE_NIXOS"
 
 echo "Warning: Running this script will wipe the currently installed system."
 read -rp "Do you want to continue? (y/n): " answer
@@ -15,8 +17,16 @@ if [ "$answer" != "y" ]; then
   exit 0
 fi
 
-sudo nix --experimental-features "nix-command flakes" run \
-  github:nix-community/disko -- --mode disko --flake "$FLAKE"
+# Build the NixOS configuration first to use substituters and check evaluation.
+sudo nix --accept-flake-config --experimental-features "nix-command flakes" \
+  build "$FLAKE"#nixosConfigurations."$HOST".config.system.build.toplevel
 
-# Install NixOS with the updated flake input and root settings
-sudo nixos-install --no-root-password --root /mnt --flake "$FLAKE"
+# Partition the disk with disk.
+sudo nix --experimental-features "nix-command flakes" run \
+  github:nix-community/disko -- --mode disko --flake "$FLAKE_NIXOS"
+
+# Install NixOS to the partitioned disk.
+sudo nixos-install --no-root-password --root /mnt --flake "$FLAKE_NIXOS"
+
+echo "Installation complete."
+echo "Warning: If you're using lanzaboote, you need to nixos-enter the installed system, generate your keys, and rebuild the system before rebooting."
