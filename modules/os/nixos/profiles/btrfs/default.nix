e3db8bf712d3cb909ab0391b477355f4c1330996
Aly Raffauf <aly@raffauflabs.com>
2025-02-06 14:49:59 -0500

    nixos/profiles/btrfs: check for btrfs filesysterms before enabling autoscrub + snapper

--- modules/os/nixos/profiles/btrfs/default.nix
+++ modules/os/nixos/profiles/btrfs/default.nix
@@ -3,14 +3,29 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  # Compute a list of Btrfs file systems (with mountPoint and device)
+  btrfsFS = let
+    isDeviceInList = list: device: builtins.filter (e: e.device == device) list != [];
+    uniqueDeviceList = lib.foldl' (acc: e:
+      if isDeviceInList acc e.device
+      then acc
+      else acc ++ [e]) [];
+  in
+    uniqueDeviceList (
+      lib.mapAttrsToList (name: fs: {
+        mountPoint = fs.mountPoint;
+        device = fs.device;
+      }) (lib.filterAttrs (name: fs: fs.fsType == "btrfs") config.fileSystems)
+    );
+in {
   options.myNixOS.profiles.btrfs.enable = lib.mkEnableOption "btrfs filesystem configuration";
 
   config = lib.mkIf config.myNixOS.profiles.btrfs.enable {
     boot.supportedFilesystems = ["btrfs"];
     environment.systemPackages = lib.optionals (config.services.xserver.enable) [pkgs.snapper-gui];
 
-    services = {
+    services = lib.mkIf (builtins.length btrfsFS > 0) {
       btrfs.autoScrub.enable = true;
 
       snapper = {
