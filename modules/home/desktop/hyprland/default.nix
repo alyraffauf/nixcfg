51a8e157368883b93bdd72c7bad64d8c44a7a55d
Aly Raffauf <aly@raffauflabs.com>
2025-02-07 13:50:31 -0500

    Revert "hyprland: let stylix set qt themes"
    This reverts commit b29818f0e7de14de6fc4973d7f8c2a48281e54a4.

--- modules/home/desktop/hyprland/default.nix
+++ modules/home/desktop/hyprland/default.nix
@@ -39,6 +39,12 @@
       networkmanagerapplet
     ];
 
+    qt = {
+      enable = true;
+      platformTheme.name = "kde";
+      style.name = "Breeze";
+    };
+
     dconf.settings = {
       "org/gnome/desktop/wm/preferences".button-layout = "";
 
