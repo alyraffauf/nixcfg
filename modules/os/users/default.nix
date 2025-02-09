0cc0b9c18a3b18d8cb633bd547f40c1e94f82773
Aly Raffauf <aly@raffauflabs.com>
2025-02-08 22:02:51 -0500

    github: add deployment workflow

--- modules/os/users/default.nix
+++ modules/os/users/default.nix
@@ -23,6 +23,7 @@
 
       keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGcJBb7+ZxkDdk06A0csNsbgT9kARUN185M8k3Lq7E/d u0_a336@localhost" # termux on winona
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGzc0kupJmNgdHK1FtPxJIe3dKtsuIScLXgHZU3ggVUN actions@github" # github actions
       ];
     };
   };
