f50f6f5d8e00c4e8a237c251b016ca7143a0eb16
Aly Raffauf <aly@raffauflabs.com>
2025-02-09 21:46:24 -0500

    users: only enable if aly or dustin.enable = true

--- modules/os/users/default.nix
+++ modules/os/users/default.nix
@@ -1,4 +1,5 @@
 {
+  config,
   lib,
   pkgs,
   ...
@@ -9,21 +10,23 @@
     ./options.nix
   ];
 
-  programs.zsh.enable = true;
+  config = lib.mkIf (config.myUsers.aly.enable or config.myUsers.dustin.enable) {
+    programs.zsh.enable = true;
 
-  users = {
-    defaultUserShell = pkgs.zsh;
-    mutableUsers = false;
+    users = {
+      defaultUserShell = pkgs.zsh;
+      mutableUsers = false;
 
-    users.root.openssh.authorizedKeys = {
-      keyFiles =
-        lib.map (file: ../../../secrets/publicKeys + "/${file}")
-        (lib.filter (file: lib.hasPrefix "aly_" file)
-          (builtins.attrNames (builtins.readDir ../../../secrets/publicKeys)));
+      users.root.openssh.authorizedKeys = {
+        keyFiles =
+          lib.map (file: ../../../secrets/publicKeys + "/${file}")
+          (lib.filter (file: lib.hasPrefix "aly_" file)
+            (builtins.attrNames (builtins.readDir ../../../secrets/publicKeys)));
 
-      keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGcJBb7+ZxkDdk06A0csNsbgT9kARUN185M8k3Lq7E/d u0_a336@localhost" # termux on winona
-      ];
+        keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGcJBb7+ZxkDdk06A0csNsbgT9kARUN185M8k3Lq7E/d u0_a336@localhost" # termux on winona
+        ];
+      };
     };
   };
 }
