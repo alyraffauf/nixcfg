1db0e10e2eccac404f05289d705829adf83a37aa
Aly Raffauf <aly@raffauflabs.com>
2025-02-09 21:49:11 -0500

    rusboro: harden config

--- hosts/rustboro/secrets.nix
+++ hosts/rustboro/secrets.nix
@@ -1,7 +1,2 @@
 {
-  age.secrets = {
-    tailscaleAuthKey.file = ../../secrets/tailscale/authKeyFile.age;
-    syncthingCert.file = ../../secrets/aly/syncthing/rustboro/cert.age;
-    syncthingKey.file = ../../secrets/aly/syncthing/rustboro/key.age;
-  };
 }
