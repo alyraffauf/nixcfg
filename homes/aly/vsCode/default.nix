c0f4bc2bbf6d7ec54a42e91420b75672ce6050c3
Aly Raffauf <aly@raffauflabs.com>
2025-02-26 12:17:23 -0500

    aly/vscode: migrate

--- homes/aly/vsCode/default.nix
+++ homes/aly/vsCode/default.nix
@@ -7,67 +7,70 @@
     programs.vscode = {
       enable = true;
       package = pkgs.vscodium;
-      enableUpdateCheck = false;
-      enableExtensionUpdateCheck = false;
 
-      keybindings = [
-        {
-          "key" = "alt+e";
-          "command" = "workbench.action.quickOpen";
-        }
-        {
-          "key" = "ctrl+p";
-          "command" = "-workbench.action.quickOpen";
-        }
-        {
-          "key" = "ctrl+p";
-          "command" = "workbench.action.showCommands";
-        }
-        {
-          "key" = "ctrl+shift+t";
-          "command" = "-workbench.action.reopenClosedEditor";
-        }
-        {
-          "key" = "ctrl+shift+t";
-          "command" = "workbench.action.tasks.runTask";
-        }
-        {
-          "key" = "ctrl+shift+p";
-          "command" = "-workbench.action.showCommands";
-        }
-      ];
+      profiles.default = {
+        enableUpdateCheck = false;
+        enableExtensionUpdateCheck = false;
 
-      userSettings = {
-        "diffEditor.ignoreTrimWhitespace" = false;
-        "direnv.restart.automatic" = true;
-        "editor.formatOnPaste" = true;
-        "editor.formatOnSave" = true;
-        "editor.formatOnType" = true;
-        "editor.rulers" = [80];
-        "editor.wordWrap" = "wordWrapColumn";
-        "editor.wordWrapColumn" = 80;
-        "explorer.confirmDelete" = false;
-        "files.autoSave" = "afterDelay";
-        "git.autoStash" = true;
-        "git.autofetch" = true;
-        "git.confirmSync" = false;
-        "github.gitProtocol" = "ssh";
-        "window.menuBarVisibility" = "hidden";
+        extensions = with pkgs.vscode-extensions; [
+          github.vscode-github-actions
+          github.vscode-pull-request-github
+          ms-python.python
+          oderwat.indent-rainbow
+          rubymaniac.vscode-paste-and-indent
+          supermaven.supermaven
+        ];
 
-        "window.titleBarStyle" =
-          if config.myHome.desktop.gnome.enable
-          then "custom"
-          else "native";
+        keybindings = [
+          {
+            "key" = "alt+e";
+            "command" = "workbench.action.quickOpen";
+          }
+          {
+            "key" = "ctrl+p";
+            "command" = "-workbench.action.quickOpen";
+          }
+          {
+            "key" = "ctrl+p";
+            "command" = "workbench.action.showCommands";
+          }
+          {
+            "key" = "ctrl+shift+t";
+            "command" = "-workbench.action.reopenClosedEditor";
+          }
+          {
+            "key" = "ctrl+shift+t";
+            "command" = "workbench.action.tasks.runTask";
+          }
+          {
+            "key" = "ctrl+shift+p";
+            "command" = "-workbench.action.showCommands";
+          }
+        ];
+
+        userSettings = {
+          "diffEditor.ignoreTrimWhitespace" = false;
+          "direnv.restart.automatic" = true;
+          "editor.formatOnPaste" = true;
+          "editor.formatOnSave" = true;
+          "editor.formatOnType" = true;
+          "editor.rulers" = [80];
+          "editor.wordWrap" = "wordWrapColumn";
+          "editor.wordWrapColumn" = 80;
+          "explorer.confirmDelete" = false;
+          "files.autoSave" = "afterDelay";
+          "git.autoStash" = true;
+          "git.autofetch" = true;
+          "git.confirmSync" = false;
+          "github.gitProtocol" = "ssh";
+          "window.menuBarVisibility" = "hidden";
+
+          "window.titleBarStyle" =
+            if config.myHome.desktop.gnome.enable
+            then "custom"
+            else "native";
+        };
       };
-
-      extensions = with pkgs.vscode-extensions; [
-        github.vscode-github-actions
-        github.vscode-pull-request-github
-        ms-python.python
-        oderwat.indent-rainbow
-        rubymaniac.vscode-paste-and-indent
-        supermaven.supermaven
-      ];
     };
   };
 }
