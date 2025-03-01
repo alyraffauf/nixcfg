137a484d768879c9d46adde54e981a196c3db385
Aly Raffauf <aly@raffauflabs.com>
2025-03-01 09:40:19 -0500

    lilycove: migrate secrets

--- hosts/lilycove/secrets.nix
+++ hosts/lilycove/secrets.nix
@@ -1,8 +1,7 @@
-{
+{self, ...}: {
   age.secrets = {
-    tailscaleAuthKey.file = ../../secrets/tailscale/authKeyFile.age;
-    k3s.file = ../../secrets/k3s.age;
-    syncthingCert.file = ../../secrets/aly/syncthing/lilycove/cert.age;
-    syncthingKey.file = ../../secrets/aly/syncthing/lilycove/key.age;
+    tailscaleAuthKey.file = "${self.inputs.secrets}/tailscale/authKeyFile.age";
+    syncthingCert.file = "${self.inputs.secrets}/aly/syncthing/lilycove/cert.age";
+    syncthingKey.file = "${self.inputs.secrets}/aly/syncthing/lilycove/key.age";
   };
 }
