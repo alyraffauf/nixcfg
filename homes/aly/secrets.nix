61fb2952438b62840dc06f7b883a21a444077db0
Aly Raffauf <aly@raffauflabs.com>
2025-03-01 09:47:44 -0500

    aly: migrate secrets

--- homes/aly/secrets.nix
+++ homes/aly/secrets.nix
@@ -1,12 +1,16 @@
-{config, ...}: {
+{
+  config,
+  self,
+  ...
+}: {
   age.secrets = {
-    achacegaGmail.file = ../../secrets/aly/mail/achacega_gmail.age;
-    alyraffaufFastmail.file = ../../secrets/aly/mail/alyraffauf_fastmail.age;
-    rclone-b2.file = ../../secrets/aly/rclone/b2.age;
-    rclone-icloud.file = ../../secrets/aly/rclone/icloud.age;
+    achacegaGmail.file = "${self.inputs.secrets}/aly/mail/achacega_gmail.age";
+    alyraffaufFastmail.file = "${self.inputs.secrets}/aly/mail/alyraffauf_fastmail.age";
+    rclone-b2.file = "${self.inputs.secrets}/aly/rclone/b2.age";
+    rclone-icloud.file = "${self.inputs.secrets}/aly/rclone/icloud.age";
 
     transmissionRemote = {
-      file = ../../secrets/aly/transmissionRemote.age;
+      file = "${self.inputs.secrets}/aly/transmissionRemote.age";
       path = "${config.home.homeDirectory}/.config/transmission-remote-gtk/config.json";
     };
   };
