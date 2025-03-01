4f7eca6ac8731091aa1b52cb29a38663f37193c0
Aly Raffauf <aly@raffauflabs.com>
2025-03-01 09:43:02 -0500

    mauville: migrate secrets

--- hosts/mauville/secrets.nix
+++ hosts/mauville/secrets.nix
@@ -1,32 +1,32 @@
-{
+{self, ...}: {
   age.secrets = {
-    rclone-b2.file = ../../secrets/rclone/b2.age;
-    restic-passwd.file = ../../secrets/restic.age;
-    tailscaleAuthKey.file = ../../secrets/tailscale/authKeyFile.age;
-    k3s.file = ../../secrets/k3s.age;
+    rclone-b2.file = "${self.inputs.secrets}/rclone/b2.age";
+    restic-passwd.file = "${self.inputs.secrets}/restic.age";
+    tailscaleAuthKey.file = "${self.inputs.secrets}/tailscale/authKeyFile.age";
+    k3s.file = "${self.inputs.secrets}/k3s.age";
 
     lastfmId = {
       owner = "navidrome";
-      file = ../../secrets/lastFM/apiKey.age;
+      file = "${self.inputs.secrets}/lastFM/apiKey.age";
     };
 
     lastfmSecret = {
       owner = "navidrome";
-      file = ../../secrets/lastFM/secret.age;
+      file = "${self.inputs.secrets}/lastFM/secret.age";
     };
 
     spotifyId = {
       owner = "navidrome";
-      file = ../../secrets/spotify/clientId.age;
+      file = "${self.inputs.secrets}/spotify/clientId.age";
     };
 
     spotifySecret = {
       owner = "navidrome";
-      file = ../../secrets/spotify/clientSecret.age;
+      file = "${self.inputs.secrets}/spotify/clientSecret.age";
     };
 
-    syncthingCert.file = ../../secrets/aly/syncthing/mauville/cert.age;
-    syncthingKey.file = ../../secrets/aly/syncthing/mauville/key.age;
-    transmission.file = ../../secrets/transmission.age;
+    syncthingCert.file = "${self.inputs.secrets}/aly/syncthing/mauville/cert.age";
+    syncthingKey.file = "${self.inputs.secrets}/aly/syncthing/mauville/key.age";
+    transmission.file = "${self.inputs.secrets}/transmission.age";
   };
 }
