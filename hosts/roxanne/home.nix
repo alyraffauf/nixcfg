f3b8c2b0a6ad96af85d6c12e1eb92af9e55b5fa7
Aly Raffauf <aly@raffauflabs.com>
2025-03-02 16:33:53 -0500

    roxanne: add home-manager config

--- hosts/roxanne/home.nix
+++ hosts/roxanne/home.nix
@@ -1,1 +1,31 @@
-{}
+{
+  lib,
+  self,
+  ...
+}: {
+  home-manager.users.aly = lib.mkForce (
+    {...}: {
+      imports = [self.homeManagerModules.default];
+
+      home = {
+        homeDirectory = "/home/aly";
+        stateVersion = "25.05";
+        username = "aly";
+      };
+
+      programs = {
+        helix = {
+          enable = true;
+          defaultEditor = true;
+        };
+
+        home-manager.enable = true;
+      };
+
+      myHome = {
+        profiles.shell.enable = true;
+        programs.fastfetch.enable = true;
+      };
+    }
+  );
+}
