dbe281c2bcce0da51187291a55eef9254af6ff89
Aly Raffauf <aly@raffauflabs.com>
2025-03-04 11:09:19 -0500

    roxanne: enable jellyfin with cloud mounts

--- hosts/roxanne/default.nix
+++ hosts/roxanne/default.nix
@@ -11,9 +11,76 @@
     self.inputs.nixos-hardware.nixosModules.raspberry-pi-4
   ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
-    fsType = "ext4";
+  environment.systemPackages = [pkgs.rclone];
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+      fsType = "ext4";
+    };
+
+    "/Audiobooks" = {
+      device = "b2:aly-audiobooks";
+      fsType = "rclone";
+
+      options = [
+        "allow_other"
+        "args2env"
+        "cache-dir=/.rclone-cache"
+        "config=${config.age.secrets.rclone-b2.path}"
+        "nodev"
+        "nofail"
+        "vfs-cache-max-age=2160h" # Cache files for up to 3 months (2160 hours)
+        "vfs-cache-max-size=1G" # Cache up to 100GB
+        "vfs-cache-mode=full" # Enables full read/write caching
+        "vfs-read-ahead=512M" # Preload 512MB of data for smoother playback
+        "vfs-write-back=10s" # Delay write operations by 10 seconds
+        "x-systemd.after=network.target"
+        "x-systemd.automount"
+      ];
+    };
+
+    "/Movies" = {
+      device = "b2:aly-movies";
+      fsType = "rclone";
+
+      options = [
+        "allow_other"
+        "args2env"
+        "cache-dir=/.rclone-cache"
+        "config=${config.age.secrets.rclone-b2.path}"
+        "nodev"
+        "nofail"
+        "vfs-cache-max-age=2160h" # Cache files for up to 3 months (2160 hours)
+        "vfs-cache-max-size=10G" # Cache up to 100GB
+        "vfs-cache-mode=full" # Enables full read/write caching
+        "vfs-read-ahead=512M" # Preload 512MB of data for smoother playback
+        "vfs-write-back=10s" # Delay write operations by 10 seconds
+        "x-systemd.after=network.target"
+        "x-systemd.automount"
+      ];
+    };
+
+    "/Shows" = {
+      device = "b2:aly-shows";
+      fsType = "rclone";
+
+      options = [
+        "allow_other"
+        "args2env"
+        "cache-dir=/.rclone-cache"
+        "config=${config.age.secrets.rclone-b2.path}"
+        "nodev"
+        "nofail"
+        "vfs-cache-max-age=2160h" # Cache files for up to 3 months (2160 hours)
+        "vfs-cache-max-size=10G" # Cache up to 100GB
+        "vfs-cache-mode=full" # Enables full read/write caching
+        "vfs-read-ahead=512M" # Preload 512MB of data for smoother playback
+        "vfs-write-back=10s" # Delay write operations by 10 seconds
+        "x-systemd.after=network.target"
+        "x-systemd.automount"
+      ];
+    };
   };
 
   hardware.enableRedistributableFirmware = true;
@@ -32,6 +99,11 @@
   };
 
   services = {
+    jellyfin = {
+      enable = true;
+      openFirewall = true;
+    };
+
     journald.extraConfig = ''
       # Store logs in RAM
       Compress=yes
