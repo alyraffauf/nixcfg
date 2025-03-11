11d212851e3fca85a4dc4c2e2e0678d0ce74e88a
Aly Raffauf <aly@raffauflabs.com>
2025-03-11 01:10:25 -0400

    fortree: enable aerospace tiling wm

--- hosts/fortree/default.nix
+++ hosts/fortree/default.nix
@@ -50,12 +50,79 @@
 
   nixpkgs.hostPlatform = "aarch64-darwin";
 
+  services.aerospace = {
+    enable = true;
+
+    settings = {
+      enable-normalization-flatten-containers = false;
+      enable-normalization-opposite-orientation-for-nested-containers =
+        false;
+
+      gaps = {
+        outer.left = 8;
+        outer.bottom = 8;
+        outer.top = 8;
+        outer.right = 8;
+        inner.horizontal = 8;
+        inner.vertical = 8;
+      };
+
+      on-focused-monitor-changed = ["move-mouse monitor-lazy-center"];
+
+      mode.main.binding = {
+        alt-0 = "workspace 10";
+        alt-1 = "workspace 1";
+        alt-2 = "workspace 2";
+        alt-3 = "workspace 3";
+        alt-4 = "workspace 4";
+        alt-5 = "workspace 5";
+        alt-6 = "workspace 6";
+        alt-7 = "workspace 7";
+        alt-8 = "workspace 8";
+        alt-9 = "workspace 9";
+        alt-h = "focus --boundaries-action wrap-around-the-workspace left";
+        alt-j = "focus --boundaries-action wrap-around-the-workspace down";
+        alt-k = "focus --boundaries-action wrap-around-the-workspace up";
+        alt-l = "focus --boundaries-action wrap-around-the-workspace right";
+        alt-leftSquareBracket = "split horizontal";
+        alt-r = "mode resize";
+        alt-rightSquareBracket = "split vertical";
+        alt-shift-0 = ["move-node-to-workspace 10" "workspace 10"];
+        alt-shift-1 = ["move-node-to-workspace 1" "workspace 1"];
+        alt-shift-2 = ["move-node-to-workspace 2" "workspace 2"];
+        alt-shift-3 = ["move-node-to-workspace 3" "workspace 3"];
+        alt-shift-4 = ["move-node-to-workspace 4" "workspace 4"];
+        alt-shift-5 = ["move-node-to-workspace 5" "workspace 5"];
+        alt-shift-6 = ["move-node-to-workspace 6" "workspace 6"];
+        alt-shift-7 = ["move-node-to-workspace 7" "workspace 7"];
+        alt-shift-8 = ["move-node-to-workspace 8" "workspace 8"];
+        alt-shift-9 = ["move-node-to-workspace 9" "workspace 9"];
+        alt-shift-h = "move left";
+        alt-shift-j = "move down";
+        alt-shift-k = "move up";
+        alt-shift-l = "move right";
+        alt-shift-v = "layout floating tiling";
+        alt-shift-w = "fullscreen";
+        alt-v = "layout tiles";
+      };
+
+      mode.resize.binding = {
+        alt-h = "resize width -50";
+        alt-j = "resize height +50";
+        alt-k = "resize height -50";
+        alt-l = "resize width +50";
+        enter = "mode main";
+        esc = "mode main";
+      };
+    };
+  };
+
   system = {
     configurationRevision = self.rev or self.dirtyRev or null; # Set Git commit hash for darwin-version.
 
     defaults = {
       dock = {
-        autohide = false; # autohide dock
+        autohide = true; # autohide dock
         mru-spaces = false; # do not rearrange spaces based on most recent use
         show-recents = false; # do not show recently closed apps
 
