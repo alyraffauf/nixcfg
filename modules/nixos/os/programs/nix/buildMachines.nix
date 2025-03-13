0f6a944c13435e609a5d2098b16e01aeadeb33c2
Aly Raffauf <aly@raffauflabs.com>
2025-03-12 23:39:15 -0400

    buildMachines: adjust for darwin with preference for lilycove

--- modules/nixos/os/programs/nix/buildMachines.nix
+++ modules/nixos/os/programs/nix/buildMachines.nix
@@ -3,27 +3,15 @@
     hostName = "lilycove";
     maxJobs = 6;
     speedFactor = 4;
-    sshKey = "/home/aly/.ssh/id_ed25519";
-    sshUser = "root";
+    sshUser = "aly";
     supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
     system = "x86_64-linux";
   }
-  {
-    hostName = "mauville";
-    maxJobs = 4;
-    speedFactor = 1;
-    sshKey = "/home/aly/.ssh/id_ed25519";
-    sshUser = "root";
-    supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
-    system = "x86_64-linux";
-  }
-
   {
     hostName = "roxanne";
     maxJobs = 4;
     speedFactor = 1;
-    sshKey = "/home/aly/.ssh/id_ed25519";
-    sshUser = "root";
+    sshUser = "aly";
     supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
     system = "aarch64-linux";
   }
