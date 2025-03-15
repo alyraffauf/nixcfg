b88cd5a2e633325cc8e039716edc77b0122fbc65
Aly Raffauf <aly@raffauflabs.com>
2025-03-14 20:48:31 -0400

    nixos/base: use sudo-rs

--- modules/nixos/os/profiles/base/default.nix
+++ modules/nixos/os/profiles/base/default.nix
@@ -44,6 +44,11 @@
     security = {
       polkit.enable = true;
       rtkit.enable = true;
+
+      sudo-rs = {
+        enable = true;
+        wheelNeedsPassword = false;
+      };
     };
 
     services = {
