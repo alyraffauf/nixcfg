1e15e271b5c88833f099813cdda5ebc1af3b6d3c
Aly Raffauf <aly@aly.codes>
2025-04-09 17:44:07 -0400

    lavaridge: define custom hm config

--- hosts/lavaridge/home.nix
+++ hosts/lavaridge/home.nix
@@ -1,3 +1,32 @@
 {self, ...}: {
-  home-manager.users.aly = self.homeManagerModules.aly-linux;
+  home-manager.users.aly = (
+    {pkgs, ...}: {
+      imports = [self.homeManagerModules.default];
+
+      home = {
+        homeDirectory = "/home/aly";
+
+        packages = with pkgs; [
+          curl
+        ];
+
+        stateVersion = "25.05";
+        username = "aly";
+      };
+
+      programs = {
+        helix = {
+          enable = true;
+          defaultEditor = true;
+        };
+
+        home-manager.enable = true;
+      };
+
+      myHome = {
+        profiles.shell.enable = true;
+        programs.fastfetch.enable = true;
+      };
+    }
+  );
 }
