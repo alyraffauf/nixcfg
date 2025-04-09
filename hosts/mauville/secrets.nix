a3dc12c7ff7ca304481fbb125b5f57feb72baa10
Aly Raffauf <aly@aly.codes>
2025-04-09 18:26:34 -0400

    mauville: fixup disable navidrome secrets

--- hosts/mauville/secrets.nix
+++ hosts/mauville/secrets.nix
@@ -5,27 +5,27 @@
     tailscaleAuthKey.file = "${self.inputs.secrets}/tailscale/auth.age";
     k3s.file = "${self.inputs.secrets}/k3s.age";
 
-    lastfmId = {
-      owner = "navidrome";
-      file = "${self.inputs.secrets}/lastfm/api.age";
-    };
+    # lastfmId = {
+    #   owner = "navidrome";
+    #   file = "${self.inputs.secrets}/lastfm/api.age";
+    # };
 
-    lastfmSecret = {
-      owner = "navidrome";
-      file = "${self.inputs.secrets}/lastfm/secret.age";
-    };
+    # lastfmSecret = {
+    #   owner = "navidrome";
+    #   file = "${self.inputs.secrets}/lastfm/secret.age";
+    # };
 
     slskd.file = "${self.inputs.secrets}/slskd.age";
 
-    spotifyId = {
-      owner = "navidrome";
-      file = "${self.inputs.secrets}/spotify/client-id.age";
-    };
+    # spotifyId = {
+    #   owner = "navidrome";
+    #   file = "${self.inputs.secrets}/spotify/client-id.age";
+    # };
 
-    spotifySecret = {
-      owner = "navidrome";
-      file = "${self.inputs.secrets}/spotify/client-secret.age";
-    };
+    # spotifySecret = {
+    #   owner = "navidrome";
+    #   file = "${self.inputs.secrets}/spotify/client-secret.age";
+    # };
 
     syncthingCert.file = "${self.inputs.secrets}/aly/syncthing/mauville/cert.age";
     syncthingKey.file = "${self.inputs.secrets}/aly/syncthing/mauville/key.age";
