2fe567d5ab6690a4cc8fd0c6183c53face690791
Aly Raffauf <aly@aly.codes>
2025-04-09 18:21:47 -0400

    lavaridge: add restic and rclone

--- hosts/lavaridge/home.nix
+++ hosts/lavaridge/home.nix
@@ -1,19 +1,25 @@
 {self, ...}: {
   home-manager.users.aly = (
     {pkgs, ...}: {
-      imports = [self.homeManagerModules.default];
+      imports = [
+        self.homeManagerModules.default
+        self.inputs.agenix.homeManagerModules.default
+      ];
+
+      age.secrets.rclone-b2.file = "${self.inputs.secrets}/rclone/b2.age";
 
       home = {
         homeDirectory = "/home/aly";
 
         packages = with pkgs; [
           curl
+          rclone
+          restic
         ];
 
         stateVersion = "25.05";
         username = "aly";
       };
-
       programs = {
         helix = {
           enable = true;
