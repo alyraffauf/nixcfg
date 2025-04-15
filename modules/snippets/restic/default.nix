4ab1dea314320f47bc32166697fe3c575aaae2d4
Aly Raffauf <aly@aly.codes>
2025-04-15 16:44:28 -0400

    snippets/restic: reduce monthly backups to 3

--- modules/snippets/restic/default.nix
+++ modules/snippets/restic/default.nix
@@ -22,7 +22,7 @@
         pruneOpts = [
           "--keep-daily 7"
           "--keep-weekly 4"
-          "--keep-monthly 6"
+          "--keep-monthly 3"
         ];
 
         rcloneConfigFile = config.age.secrets.rclone-b2.path;
