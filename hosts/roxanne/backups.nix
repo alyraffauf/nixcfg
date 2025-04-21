f3f1740e23d096cb97b9091fcc3b73cb7a6027a1
Aly Raffauf <aly@aly.codes>
2025-04-21 08:34:49 -0400

    roxanne: backup tautulli

--- hosts/roxanne/backups.nix
+++ hosts/roxanne/backups.nix
@@ -4,6 +4,15 @@
   ...
 }: {
   services.restic.backups = {
+    tautulli =
+      config.mySnippets.restic
+      // {
+        backupCleanupCommand = "${pkgs.systemd}/bin/systemctl start tautulli";
+        backupPrepareCommand = ''${pkgs.systemd}/bin/systemctl stop tautulli'';
+        paths = ["/var/lib/plexpy"];
+        repository = "rclone:b2:aly-backups/${config.networking.hostName}/tautulli";
+      };
+
     uptime-kuma =
       config.mySnippets.restic
       // {
