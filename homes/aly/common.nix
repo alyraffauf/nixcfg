e25da41c66ee77dcdc104754575749b862a2a733
Aly Raffauf <aly@aly.codes>
2025-05-04 17:43:54 -0400

    aly: add vesktop settings

--- homes/aly/common.nix
+++ homes/aly/common.nix
@@ -39,7 +39,39 @@
     };
 
     home-manager.enable = true;
-    vesktop.enable = true;
+
+    vesktop = {
+      enable = true;
+
+      settings = {
+        appBadge = false;
+        arRPC = true;
+        disableMinSize = true;
+        minimizeToTray = false;
+        tray = false;
+        hardwareAcceleration = true;
+        discordBranch = "stable";
+      };
+
+      vencord.settings = {
+        autoUpdate = true;
+        autoUpdateNotification = false;
+        disableMinSize = true;
+        notifyAboutUpdates = false;
+
+        plugins = {
+          AlwaysTrust.enabled = true;
+          BlurNSFW.enabled = true;
+          ClearURLs.enabled = true;
+          FakeNitro.enabled = true;
+          PinDMs.enabled = true;
+          WebKeybinds.enabled = true;
+          WebScreenShareFixes.enabled = true;
+        };
+
+        useQuickCss = true;
+      };
+    };
   };
 
   myHome = {
