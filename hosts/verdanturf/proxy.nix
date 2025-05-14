1431ddebce39d33957c0c6b7bccb52d60d61c877
Aly Raffauf <aly@raffauflabs.com>
2025-05-14 17:33:24 -0400

    verdanturf/proxy: proxy websockets for vaultwarden

--- hosts/verdanturf/proxy.nix
+++ hosts/verdanturf/proxy.nix
@@ -58,6 +58,7 @@ in {
 
           locations."/" = {
             proxyPass = "http://127.0.0.1:${toString config.services.vaultwarden.config.ROCKET_PORT}";
+            proxyWebsockets = true;
           };
 
           serverAliases = ["v.${newDomain}" "passwords.${oldDomain}"];
