3533271755523de614031a9d0faae23be7216ff3
Aly Raffauf <aly@raffauflabs.com>
2025-05-17 19:29:33 -0400

    slateport/anubis: passthrough opengraph

--- hosts/slateport/anubis.nix
+++ hosts/slateport/anubis.nix
@@ -2,6 +2,8 @@
   services.anubis = {
     defaultOptions.settings = {
       DIFFICULTY = 4;
+      OG_CACHE_CONSIDER_HOST = true;
+      OG_PASSTHROUGH = true;
       SERVE_ROBOTS_TXT = true;
       WEBMASTER_EMAIL = "hello@aly.codes";
     };
