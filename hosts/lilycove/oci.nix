b51cc109725f0653e0f294a335f0efc4116d8456
Aly Raffauf <aly@aly.codes>
2025-05-17 16:18:21 -0400

    lilycove: disable tubesync

--- hosts/lilycove/oci.nix
+++ hosts/lilycove/oci.nix
@@ -41,17 +41,17 @@
       ];
     };
 
-    tubesync = {
-      environment.TUBESYNC_WORKERS = "8";
-      extraOptions = ["--pull=always"];
-      image = "ghcr.io/meeb/tubesync:latest";
-      ports = ["0.0.0.0:14848:4848"];
+    # tubesync = {
+    #   environment.TUBESYNC_WORKERS = "8";
+    #   extraOptions = ["--pull=always"];
+    #   image = "ghcr.io/meeb/tubesync:latest";
+    #   ports = ["0.0.0.0:14848:4848"];
 
-      volumes = [
-        "/mnt/Data/tubesync/config/:/config"
-        "/mnt/Media/YouTube/:/downloads"
-        "/etc/localtime:/etc/localtime:ro"
-      ];
-    };
+    #   volumes = [
+    #     "/mnt/Data/tubesync/config/:/config"
+    #     "/mnt/Media/YouTube/:/downloads"
+    #     "/etc/localtime:/etc/localtime:ro"
+    #   ];
+    # };
   };
 }
