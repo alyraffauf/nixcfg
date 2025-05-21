894554b705fd03c6336386ad722674c4b10d1b0b
Aly Raffauf <aly@raffauflabs.com>
2025-05-21 07:45:41 -0400

    profiles/server: use stable kernel

--- modules/nixos/os/profiles/server/default.nix
+++ modules/nixos/os/profiles/server/default.nix
@@ -1,20 +1,15 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }: {
   options.myNixOS.profiles.server.enable = lib.mkEnableOption "server optimizations";
   config = lib.mkIf config.myNixOS.profiles.server.enable {
-    boot = {
-      kernel.sysctl = {
-        # Improved file monitoring
-        "fs.file-max" = lib.mkDefault 2097152;
-        "fs.inotify.max_user_instances" = lib.mkOverride 100 8192;
-        "fs.inotify.max_user_watches" = lib.mkOverride 100 524288;
-      };
-
-      kernelPackages = pkgs.linuxPackages_latest;
+    boot.kernel.sysctl = {
+      # Improved file monitoring
+      "fs.file-max" = lib.mkDefault 2097152;
+      "fs.inotify.max_user_instances" = lib.mkOverride 100 8192;
+      "fs.inotify.max_user_watches" = lib.mkOverride 100 524288;
     };
 
     services = {
