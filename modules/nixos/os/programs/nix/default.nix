a37fa8c73bfc07a8afd41ad77a16123845552a95
Aly Raffauf <aly@raffauflabs.com>
2025-05-23 22:01:12 -0400

    programs/nix: only make nixbuild user/group on buildMachines

--- modules/nixos/os/programs/nix/default.nix
+++ modules/nixos/os/programs/nix/default.nix
@@ -4,13 +4,16 @@
   pkgs,
   self,
   ...
-}: {
+}: let
+  isBuildMachine = let buildHosts = lib.map (m: m.hostName) config.mySnippets.nix.buildMachines; in lib.elem config.networking.hostName buildHosts;
+in {
   options.myNixOS.programs.nix.enable = lib.mkEnableOption "sane nix configuration";
 
   config = lib.mkIf config.myNixOS.programs.nix.enable {
     nix = {
       buildMachines = lib.mkIf config.services.tailscale.enable (
-        lib.filter (m: m.hostName != "${config.networking.hostName}") config.mySnippets.nix.buildMachines
+        lib.filter (m: m.hostName != config.networking.hostName)
+        config.mySnippets.nix.buildMachines
       );
 
       distributedBuilds = true;
@@ -22,7 +25,6 @@
         randomizedDelaySec = "60min";
       };
 
-      # Run GC when there is less than 1GiB left.
       extraOptions = ''
         min-free = ${toString (1 * 1024 * 1024 * 1024)}   # 1 GiB
         max-free = ${toString (5 * 1024 * 1024 * 1024)}   # 5 GiB
@@ -40,21 +42,21 @@
 
     programs.nix-ld.enable = true;
 
-    users.users.nixbuild = {
+    # Only create the nixbuild user (and its group) on build machines
+    users.users.nixbuild = lib.mkIf isBuildMachine {
       uid = 1999;
       isNormalUser = true;
       createHome = false;
       group = "nixbuild";
 
-      openssh.authorizedKeys.keyFiles =
-        lib.map (file: "${self.inputs.secrets}/publicKeys/${file}")
-        (lib.filter (file: lib.hasPrefix "aly_" file)
-          (builtins.attrNames (builtins.readDir "${self.inputs.secrets}/publicKeys")))
-        ++ lib.map (file: "${self.inputs.secrets}/publicKeys/${file}")
-        (lib.filter (file: lib.hasPrefix "root_" file)
-          (builtins.attrNames (builtins.readDir "${self.inputs.secrets}/publicKeys")));
+      openssh.authorizedKeys.keyFiles = let
+        pubDir = "${self.inputs.secrets}/publicKeys";
+        aly = lib.filter (file: lib.hasPrefix "aly_" file) (builtins.attrNames (builtins.readDir pubDir));
+        root = lib.filter (file: lib.hasPrefix "root_" file) (builtins.attrNames (builtins.readDir pubDir));
+      in
+        lib.map (file: "${pubDir}/${file}") (aly ++ root);
     };
 
-    users.groups.nixbuild = {};
+    users.groups.nixbuild = lib.mkIf isBuildMachine {};
   };
 }
