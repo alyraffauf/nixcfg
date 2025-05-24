7e18413e9b0c302164347eac51999af2f11577ac
Aly Raffauf <aly@raffauflabs.com>
2025-05-24 17:23:22 -0400

    rustboro: drop hardening

--- hosts/rustboro/default.nix
+++ hosts/rustboro/default.nix
@@ -1,78 +1,18 @@
-# Lenovo Thinkpad T440p with a Core i5 4210M, 16GB RAM, 512GB SSD.
-{
-  pkgs,
-  self,
-  ...
-}: {
+{self, ...}: {
   imports = [
     ./home.nix
     ./secrets.nix
     ./stylix.nix
-    self.nixosModules.disko-luks-btrfs-subvolumes
+    self.nixosModules.disko-btrfs-subvolumes
     self.nixosModules.hardware-lenovo-thinkpad-T440p
     self.nixosModules.locale-en-us
   ];
 
-  environment = {
-    gnome.excludePackages = with pkgs; [
-      epiphany
-      geary
-      gnome-calendar
-      gnome-clocks
-      gnome-contacts
-      gnome-maps
-      gnome-tour
-      gnome-weather
-      snapshot
-    ];
-
-    variables.GDK_SCALE = "1.25";
-  };
-
+  environment.variables.GDK_SCALE = "1.25";
   networking.hostName = "rustboro";
-
-  programs = {
-    firefox.enable = true;
-    zsh.enable = true;
-  };
-
-  services.xserver = {
-    desktopManager.gnome.favoriteAppsOverride = ''
-      [org.gnome.shell]
-      favorite-apps=[ 'firefox.desktop', 'torbrowser.desktop', 'nautilus.desktop' ]
-    '';
-
-    xkb.options = "ctrl:nocaps";
-  };
-
+  services.xserver.xkb.options = "ctrl:nocaps";
   system.stateVersion = "25.05";
   time.timeZone = "America/New_York";
-
-  users = {
-    defaultUserShell = pkgs.zsh;
-    mutableUsers = false;
-
-    users.aly = {
-      description = "Aly Raffauf";
-
-      extraGroups = [
-        "dialout"
-        "lp"
-        "networkmanager"
-        "plugdev"
-        "scanner"
-        "video"
-        "wheel"
-      ];
-
-      hashedPassword = "$y$j9T$VMCXwk0X5m6xW6FGLc39F/$r9gmyeB70RCq3k4oLPHFZyy7wng6WyX2xYMKLO/A.rB";
-      isNormalUser = true;
-
-      shell = pkgs.zsh;
-      uid = 1000;
-    };
-  };
-
   myDisko.installDrive = "/dev/sda";
 
   myNixOS = {
@@ -82,14 +22,26 @@
       autoUpgrade.enable = true;
       base.enable = true;
       btrfs.enable = true;
-      hardened.enable = true;
+      swap.enable = true;
+      wifi.enable = true;
+      workstation.enable = true;
     };
 
     programs = {
-      lanzaboote.enable = true;
+      firefox.enable = true;
       nix.enable = true;
+      podman.enable = true;
+      systemd-boot.enable = true;
     };
 
-    services.gdm.enable = true;
+    services = {
+      gdm.enable = true;
+      tailscale.enable = true;
+    };
+  };
+
+  myUsers.aly = {
+    enable = true;
+    password = "$y$j9T$VMCXwk0X5m6xW6FGLc39F/$r9gmyeB70RCq3k4oLPHFZyy7wng6WyX2xYMKLO/A.rB";
   };
 }
--- hosts/rustboro/secrets.nix
+++ hosts/rustboro/secrets.nix
@@ -1,2 +1,7 @@
-{
+{self, ...}: {
+  age.secrets = {
+    tailscaleAuthKey.file = "${self.inputs.secrets}/tailscale/auth.age";
+    syncthingCert.file = "${self.inputs.secrets}/aly/syncthing/rustboro/cert.age";
+    syncthingKey.file = "${self.inputs.secrets}/aly/syncthing/rustboro/key.age";
+  };
 }
