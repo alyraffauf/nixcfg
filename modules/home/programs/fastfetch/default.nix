7b592ab082dc9b2fdc7e742bf88a306cd859c367
Aly Raffauf <aly@raffauflabs.com>
2025-05-27 16:39:03 -0400

    home/programs/fastfetch: use hm module

--- modules/home/programs/fastfetch/default.nix
+++ modules/home/programs/fastfetch/default.nix
@@ -1,13 +1,90 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }: {
   options.myHome.programs.fastfetch.enable = lib.mkEnableOption "fastfetch system information";
 
   config = lib.mkIf config.myHome.programs.fastfetch.enable {
-    home.packages = [pkgs.fastfetch];
-    xdg.configFile."fastfetch/config.jsonc".text = lib.generators.toJSON {} (import ./config.nix);
+    programs.fastfetch = {
+      enable = true;
+
+      settings = {
+        display = {separator = "  ";};
+
+        logo = {
+          padding = {
+            bottom = 3;
+            top = 3;
+          };
+
+          type = "small";
+        };
+
+        modules = [
+          "break"
+          "title"
+          {
+            format = "{5} {1}";
+            key = "host  ";
+            keyColor = "33";
+            type = "host";
+          }
+          {
+            format = "{2}";
+            key = "os    ";
+            keyColor = "33";
+            type = "os";
+          }
+          {
+            format = "{1}d {2}h {3}m";
+            key = "uptime";
+            keyColor = "33";
+            type = "uptime";
+          }
+          {
+            key = "kernel";
+            keyColor = "33";
+            type = "kernel";
+          }
+          {
+            format = "{1}";
+            key = "wm    ";
+            keyColor = "33";
+            type = "wm";
+          }
+          {
+            format = "{1}";
+            key = "cpu   ";
+            keyColor = "33";
+            type = "cpu";
+          }
+          {
+            format = "{2}";
+            key = "gpu   ";
+            keyColor = "33";
+            type = "gpu";
+          }
+          {
+            key = "memory";
+            keyColor = "33";
+            type = "memory";
+          }
+          {
+            format = "{1} / {2} ({3})";
+            key = "disk  ";
+            keyColor = "33";
+            type = "disk";
+          }
+          {
+            format = "{5} ({4})";
+            key = "bat   ";
+            keyColor = "33";
+            type = "battery";
+          }
+          "break"
+        ];
+      };
+    };
   };
 }
