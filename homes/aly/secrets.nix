79d05890220ee3955946e276decee4600fa26d78
Aly Raffauf <aly@aly.codes>
2025-05-26 21:48:36 -0400

    aly/secrets: drop transmissionRemote

--- homes/aly/secrets.nix
+++ homes/aly/secrets.nix
@@ -1,18 +1,9 @@
-{
-  config,
-  self,
-  ...
-}: {
+{self, ...}: {
   age.secrets = {
     achacegaGmail.file = "${self.inputs.secrets}/aly/mail/gmail.age";
     alyraffaufFastmail.file = "${self.inputs.secrets}/aly/mail/fastmail.age";
     aws.file = "${self.inputs.secrets}/aly/aws.age";
     rclone-b2.file = "${self.inputs.secrets}/rclone/b2.age";
     rclone-icloud.file = "${self.inputs.secrets}/aly/rclone/icloud.age";
-
-    transmissionRemote = {
-      file = "${self.inputs.secrets}/aly/transmission-remote.age";
-      path = "${config.home.homeDirectory}/.config/transmission-remote-gtk/config.json";
-    };
   };
 }
