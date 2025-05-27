de9ca758ed2aaf0e2cffafe53120e0869d9c21b0
Aly Raffauf <aly@aly.codes>
2025-05-27 00:05:50 -0400

    verdanturf/nginx: interpolate couchdb url

--- hosts/verdanturf/proxy.nix
+++ hosts/verdanturf/proxy.nix
@@ -50,7 +50,7 @@ in {
           forceSSL = true;
 
           locations."/" = {
-            proxyPass = "http://127.0.0.1:5984";
+            proxyPass = "http://127.0.0.1:${toString config.services.couchdb.port}";
           };
 
           serverAliases = ["couch.${oldDomain}"];
