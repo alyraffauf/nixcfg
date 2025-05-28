86eae6b82e17970001eb6dc7e29e9590b92159e6
Aly Raffauf <aly@aly.codes>
2025-05-27 22:13:33 -0400

    profiles/shell: swap yazi for joshuto

--- modules/home/profiles/shell/default.nix
+++ modules/home/profiles/shell/default.nix
@@ -61,27 +61,7 @@
       };
 
       ripgrep-all.enable = true;
-
-      yazi = {
-        enable = true;
-        enableBashIntegration = true;
-
-        settings = {
-          log.enabled = false;
-
-          manager = {
-            show_hidden = false;
-            sort_by = "mtime";
-            sort_dir_first = true;
-            sort_reverse = true;
-            sort_sensitive = true;
-            linemode = "size";
-            show_symlink = true;
-          };
-
-          preview.tab_size = 4;
-        };
-      };
+      joshuto.enable = true;
 
       zellij = {
         enable = true;
