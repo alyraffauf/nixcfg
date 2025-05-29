ff1d5a5e79d8890f716c265344f7776ef8dc4518
Aly Raffauf <aly@aly.codes>
2025-05-29 09:42:19 -0400

    hyprland :disable ANR dialog

--- modules/home/desktop/hyprland/settings.nix
+++ modules/home/desktop/hyprland/settings.nix
@@ -200,6 +200,7 @@ in {
   misc = {
     disable_hyprland_logo = true;
     disable_splash_rendering = true;
+    enable_anr_dialog = false;
     enable_swallow = true;
     focus_on_activate = true;
     font_family = config.stylix.fonts.sansSerif.name;
