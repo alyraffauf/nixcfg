dc1513902435cae7f68e05b75fe5fd2fa15f6743
Aly Raffauf <aly@aly.codes>
2025-06-08 10:57:11 -0400

    services/caddy: fixup reloading

--- modules/nixos/os/services/caddy/default.nix
+++ modules/nixos/os/services/caddy/default.nix
@@ -12,8 +12,9 @@
     networking.firewall.allowedTCPPorts = [80 443];
 
     services = {
-      caddy = lib.mkIf config.myNixOS.services.tailscale.enableCaddy {
+      caddy = {
         enable = true;
+        enableReload = false;
         environmentFile = config.age.secrets.tailscaleCaddyAuth.path;
 
         globalConfig = ''
@@ -25,30 +26,6 @@
         package = pkgs.caddy.withPlugins {
           plugins = ["github.com/tailscale/caddy-tailscale@v0.0.0-20250508175905-642f61fea3cc"];
           hash = "sha256-Kbqr7spiL8/UvT0HtCm0Ufh5Nm1VYDjyNWPCd1Yxyxc=";
-        };
-
-        virtualHosts = {
-          "${config.networking.hostName}.${config.mySnippets.tailnet}".extraConfig = let
-            qbittorrent = ''
-              redir /qbittorrent /qbittorrent/
-              handle_path /qbittorrent/* {
-                reverse_proxy localhost:${toString config.myNixOS.services.qbittorrent.port}
-              }
-            '';
-
-            syncthing = ''
-              redir /syncthing /syncthing/
-              handle_path /syncthing/* {
-                reverse_proxy localhost:8384 {
-                  header_up Host localhost
-                }
-              }
-            '';
-          in
-            lib.concatLines (
-              lib.optional (config.myNixOS.services.qbittorrent.enable) qbittorrent
-              ++ lib.optional (config.services.syncthing.enable) syncthing
-            );
         };
       };
 
