85d16cd81fd5181ec5786aa4bd188aa34bddaafd
Aly Raffauf <aly@raffauflabs.com>
2025-06-08 00:19:06 -0400

    snippets: add tailnet

--- modules/snippets/default.nix
+++ modules/snippets/default.nix
@@ -1,8 +1,14 @@
-{...}: {
+{lib, ...}: {
   imports = [
     ./nix
     ./restic
     ./ssh
     ./syncthing
   ];
+
+  options.mySnippets.tailnet = lib.mkOption {
+    default = "narwhal-snapper.ts.net";
+    description = "Tailnet name.";
+    type = lib.types.str;
+  };
 }
--- modules/snippets/ssh/knownHosts.nix
+++ modules/snippets/ssh/knownHosts.nix
@@ -1,4 +1,5 @@
 {
+  config,
   lib,
   self,
   ...
@@ -9,62 +10,62 @@
 
     default = {
       fallarbor = {
-        hostNames = ["fallarbor" "fallarbor.local" "fallarbor.narwhal-snapper.ts.net"];
+        hostNames = ["fallarbor" "fallarbor.local" "fallarbor.${config.mySnippets.tailnet}"];
         publicKeyFile = "${self.inputs.secrets}/publicKeys/root_fallarbor.pub";
       };
 
       fortree = {
-        hostNames = ["fortree" "fortree.local" "fortree.narwhal-snapper.ts.net"];
+        hostNames = ["fortree" "fortree.local" "fortree.${config.mySnippets.tailnet}"];
         publicKeyFile = "${self.inputs.secrets}/publicKeys/root_fortree.pub";
       };
 
       lavaridge = {
-        hostNames = ["lavaridge" "lavaridge.local" "lavaridge.narwhal-snapper.ts.net"];
+        hostNames = ["lavaridge" "lavaridge.local" "lavaridge.${config.mySnippets.tailnet}"];
         publicKeyFile = "${self.inputs.secrets}/publicKeys/root_lavaridge.pub";
       };
 
       lilycove = {
-        hostNames = ["lilycove" "lilycove.local" "lilycove.narwhal-snapper.ts.net"];
+        hostNames = ["lilycove" "lilycove.local" "lilycove.${config.mySnippets.tailnet}"];
         publicKeyFile = "${self.inputs.secrets}/publicKeys/root_lilycove.pub";
       };
 
       mauville = {
-        hostNames = ["mauville" "mauville.local" "mauville.narwhal-snapper.ts.net"];
+        hostNames = ["mauville" "mauville.local" "mauville.${config.mySnippets.tailnet}"];
         publicKeyFile = "${self.inputs.secrets}/publicKeys/root_mauville.pub";
       };
 
       mossdeep = {
-        hostNames = ["mossdeep" "mossdeep.local" "mossdeep.narwhal-snapper.ts.net"];
+        hostNames = ["mossdeep" "mossdeep.local" "mossdeep.${config.mySnippets.tailnet}"];
         publicKeyFile = "${self.inputs.secrets}/publicKeys/root_mossdeep.pub";
       };
 
       petalburg = {
-        hostNames = ["petalburg" "petalburg.local" "petalburg.narwhal-snapper.ts.net"];
+        hostNames = ["petalburg" "petalburg.local" "petalburg.${config.mySnippets.tailnet}"];
         publicKeyFile = "${self.inputs.secrets}/publicKeys/root_petalburg.pub";
       };
 
       roxanne = {
-        hostNames = ["roxanne" "roxanne.local" "roxanne.narwhal-snapper.ts.net"];
+        hostNames = ["roxanne" "roxanne.local" "roxanne.${config.mySnippets.tailnet}"];
         publicKeyFile = "${self.inputs.secrets}/publicKeys/root_roxanne.pub";
       };
 
       rustboro = {
-        hostNames = ["rustboro" "rustboro.local" "rustboro.narwhal-snapper.ts.net"];
+        hostNames = ["rustboro" "rustboro.local" "rustboro.${config.mySnippets.tailnet}"];
         publicKeyFile = "${self.inputs.secrets}/publicKeys/root_rustboro.pub";
       };
 
       slateport = {
-        hostNames = ["slateport" "slateport.local" "slateport.narwhal-snapper.ts.net"];
+        hostNames = ["slateport" "slateport.local" "slateport.${config.mySnippets.tailnet}"];
         publicKeyFile = "${self.inputs.secrets}/publicKeys/root_slateport.pub";
       };
 
       sootopolis = {
-        hostNames = ["sootopolis" "sootopolis.local" "sootopolis.narwhal-snapper.ts.net"];
+        hostNames = ["sootopolis" "sootopolis.local" "sootopolis.${config.mySnippets.tailnet}"];
         publicKeyFile = "${self.inputs.secrets}/publicKeys/root_sootopolis.pub";
       };
 
       verdanturf = {
-        hostNames = ["verdanturf" "verdanturf.local" "verdanturf.narwhal-snapper.ts.net"];
+        hostNames = ["verdanturf" "verdanturf.local" "verdanturf.${config.mySnippets.tailnet}"];
         publicKeyFile = "${self.inputs.secrets}/publicKeys/root_verdanturf.pub";
       };
     };
