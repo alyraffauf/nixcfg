213d0421cc20bd8aa256badc45f72b60fa147de0
Aly Raffauf <aly@raffauflabs.com>
2025-06-09 16:01:24 -0400

    users: null passwd by default

--- modules/nixos/users/options.nix
+++ modules/nixos/users/options.nix
@@ -4,6 +4,7 @@
       enable = lib.mkEnableOption "${user}.";
 
       password = lib.mkOption {
+        default = null;
         description = "Hashed password for ${user}.";
         type = lib.types.nullOr lib.types.str;
       };
