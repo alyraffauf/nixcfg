8bdfe6d0c58b2b50bac546bf0b897fe88c324aa4
Aly Raffauf <aly@aly.codes>
2025-06-10 20:54:56 -0400

    sootopolis/home: enable select stylix modules

--- hosts/sootopolis/home.nix
+++ hosts/sootopolis/home.nix
@@ -1,3 +1,18 @@
 {self, ...}: {
   home-manager.users.aly = self.homeManagerModules.aly;
+
+  home-manager.sharedModules = [
+    {
+      stylix.targets = {
+        bat.enable = true;
+        font-packages.enable = true;
+        fontconfig.enable = true;
+        ghostty.enable = true;
+        helix.enable = true;
+        vesktop.enable = true;
+        vscode.enable = true;
+        zellij.enable = true;
+      };
+    }
+  ];
 }
