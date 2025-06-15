6ce2918723a5baf709851a832aff240d9cd51a94
Aly Raffauf <aly@raffauflabs.com>
2025-06-15 14:04:41 -0400

    home/aly/desktop/macos: auto hide dock

--- modules/home/aly/desktop/macos/default.nix
+++ modules/home/aly/desktop/macos/default.nix
@@ -14,7 +14,7 @@
         };
 
         "com.apple.dock" = {
-          autohide = false; # don't autohide dock
+          autohide = true; # hide dock
           expose-group-apps = true;
           minimize-to-application = true;
           mru-spaces = false; # do not rearrange spaces based on most recent use
