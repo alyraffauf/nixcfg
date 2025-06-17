994c186483c03e27f017be6772a8d5d372d3b0c5
Aly Raffauf <aly@raffauflabs.com>
2025-06-17 17:07:32 -0400

    home/aly/programs/git: use gitFull package

--- modules/home/aly/programs/git/default.nix
+++ modules/home/aly/programs/git/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   options.myHome.aly.programs.git.enable = lib.mkEnableOption "git version control";
@@ -11,6 +12,7 @@
         enable = true;
         delta.enable = true;
         lfs.enable = true;
+        package = pkgs.gitFull;
         userName = "Aly Raffauf";
         userEmail = "aly@aly.codes";
 
