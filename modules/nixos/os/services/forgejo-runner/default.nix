4c1d00fb5c1599ab8a33c672da92ae2b01f6aa7c
Aly Raffauf <aly@aly.codes>
2025-06-26 11:25:48 -0400

    nixos/services/forgejo-runner: use forgejo-runner package, don't launch extra instances for igt.aly.codes

--- modules/nixos/os/services/forgejo-runner/default.nix
+++ modules/nixos/os/services/forgejo-runner/default.nix
@@ -25,14 +25,12 @@
 
     age.secrets.act-runner.file = "${self.inputs.secrets}/act-runner.age";
 
-    services.gitea-actions-runner.instances = let
-      runnerCount = config.myNixOS.services.forgejo-runner.number;
-      runnerIndices = lib.map toString (lib.range 1 runnerCount);
-
-      # eg x86_64-linux → x86_64_linux
+    services.gitea-actions-runner = let
       arch = lib.replaceStrings ["-"] ["_"] pkgs.system;
-    in
-      lib.genAttrs runnerIndices (idx: {
+    in {
+      package = pkgs.forgejo-runner;
+
+      instances.alycodes = {
         enable = true;
 
         hostPackages = with pkgs;
@@ -56,10 +54,58 @@
           ]
           ++ lib.optional (arch == "aarch64_linux") "ubuntu-24.04-arm:docker://gitea/runner-images:ubuntu-latest" ++ lib.optional (arch == "x86_64_linux") "ubuntu-latest:docker://gitea/runner-images:ubuntu-latest";
 
-        name = "${arch}-${config.networking.hostName}-${idx}";
-        settings.container.network = "host";
+        name = "${arch}-${config.networking.hostName}-alycodes";
+
+        settings = {
+          container.network = "host";
+          runner.capacity = config.myNixOS.services.forgejo-runner.number;
+        };
+
         tokenFile = config.age.secrets.act-runner.path;
         url = "http://mossdeep:3001";
-      });
+      };
+
+      # instances = let
+      #   runnerCount = config.myNixOS.services.forgejo-runner.number;
+      #   runnerIndices = lib.map toString (lib.range 1 runnerCount);
+
+      #   # eg x86_64-linux → x86_64_linux
+      #   arch = lib.replaceStrings ["-"] ["_"] pkgs.system;
+      # in
+      #   lib.genAttrs runnerIndices (idx: {
+      #     enable = true;
+
+      #     hostPackages = with pkgs;
+      #       [
+      #         bash
+      #         cachix
+      #         coreutils
+      #         curl
+      #         gawk
+      #         gitMinimal
+      #         gnused
+      #         jq
+      #         nodejs
+      #         wget
+      #       ]
+      #       ++ [config.nix.package];
+
+      #     labels =
+      #       [
+      #         "nixos-${arch}:host"
+      #       ]
+      #       ++ lib.optional (arch == "aarch64_linux") "ubuntu-24.04-arm:docker://gitea/runner-images:ubuntu-latest" ++ lib.optional (arch == "x86_64_linux") "ubuntu-latest:docker://gitea/runner-images:ubuntu-latest";
+
+      #     name = "${arch}-${config.networking.hostName}-${idx}";
+
+      #     settings = {
+      #       container.network = "host";
+      #       runner.capacity = 2;
+      #     };
+
+      #     tokenFile = config.age.secrets.act-runner.path;
+      #     url = "http://mossdeep:3001";
+      #   });
+    };
   };
 }
