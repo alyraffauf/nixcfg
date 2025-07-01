d2b0fad29a9b9322b9613f9e3a7e37522adbba05
Aly Raffauf <aly@raffauflabs.com>
2025-06-30 22:43:20 -0400

    lavaridge/home: enable stylix for select apps

--- hosts/lavaridge/home.nix
+++ hosts/lavaridge/home.nix
@@ -1,36 +1,17 @@
 {self, ...}: {
-  home-manager.users.aly = {pkgs, ...}: {
-    imports = [
-      self.homeManagerModules.default
-      self.inputs.agenix.homeManagerModules.default
-    ];
+  home-manager.users.aly = self.homeManagerModules.aly;
 
-    age.secrets.rclone-b2.file = "${self.inputs.secrets}/rclone/b2.age";
-
-    home = {
-      homeDirectory = "/home/aly";
-
-      packages = with pkgs; [
-        curl
-        rclone
-        restic
-      ];
-
-      stateVersion = "25.05";
-      username = "aly";
-    };
-    programs = {
-      helix = {
-        enable = true;
-        defaultEditor = true;
+  home-manager.sharedModules = [
+    {
+      stylix.targets = {
+        bat.enable = true;
+        font-packages.enable = true;
+        fontconfig.enable = true;
+        ghostty.enable = true;
+        helix.enable = true;
+        vscode.enable = true;
+        zellij.enable = true;
       };
-
-      home-manager.enable = true;
-    };
-
-    myHome = {
-      profiles.shell.enable = true;
-      programs.fastfetch.enable = true;
-    };
-  };
+    }
+  ];
 }
