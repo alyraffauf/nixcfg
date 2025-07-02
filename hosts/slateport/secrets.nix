b6a8491438804429dd7ae7a25eee07a63e95f8c6
Aly Raffauf <aly@raffauflabs.com>
2025-07-02 19:24:06 -0400

    slateport: remove cloudflare and k3s secrets

--- hosts/slateport/secrets.nix
+++ hosts/slateport/secrets.nix
@@ -1,8 +1,6 @@
 {self, ...}: {
   age.secrets = {
     tailscaleAuthKey.file = "${self.inputs.secrets}/tailscale/auth.age";
-    cloudflare.file = "${self.inputs.secrets}/cloudflare.age";
-    k3s.file = "${self.inputs.secrets}/k3s.age";
     rclone-b2.file = "${self.inputs.secrets}/rclone/b2.age";
     restic-passwd.file = "${self.inputs.secrets}/restic-password.age";
     syncthingCert.file = "${self.inputs.secrets}/aly/syncthing/slateport/cert.age";
