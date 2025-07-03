5de8bc0942bc966a528b4b4f70574ed3a9c46c48
Aly Raffauf <aly@raffauflabs.com>
2025-07-03 13:56:56 -0400

    lilycove/prometheus: export to tailnet vHosts

--- hosts/lilycove/prometheus.nix
+++ hosts/lilycove/prometheus.nix
@@ -4,35 +4,35 @@
       enable = true;
       apiKeyFile = config.age.secrets.bazarrApiKey.path;
       port = 9708;
-      url = "https://bazarr.${config.mySnippets.tailnet.name}";
+      url = "https://${config.mySnippets.tailnet.networkMap.bazarr.vHost}";
     };
 
     exportarr-lidarr = {
       enable = true;
       apiKeyFile = config.age.secrets.lidarrApiKey.path;
       port = 9709;
-      url = "https://lidarr.${config.mySnippets.tailnet.name}";
+      url = "https://${config.mySnippets.tailnet.networkMap.lidarr.vHost}";
     };
 
     exportarr-prowlarr = {
       enable = true;
       apiKeyFile = config.age.secrets.prowlarrApiKey.path;
       port = 9710;
-      url = "https://prowlarr.${config.mySnippets.tailnet.name}";
+      url = "https://${config.mySnippets.tailnet.networkMap.prowlarr.vHost}";
     };
 
     exportarr-radarr = {
       enable = true;
       apiKeyFile = config.age.secrets.radarrApiKey.path;
       port = 9711;
-      url = "https://radarr.${config.mySnippets.tailnet.name}";
+      url = "https://${config.mySnippets.tailnet.networkMap.radarr.vHost}";
     };
 
     exportarr-sonarr = {
       enable = true;
       apiKeyFile = config.age.secrets.sonarrApiKey.path;
       port = 9712;
-      url = "https://sonarr.${config.mySnippets.tailnet.name}";
+      url = "https://${config.mySnippets.tailnet.networkMap.sonarr.vHost}";
     };
 
     smartctl.enable = true;
