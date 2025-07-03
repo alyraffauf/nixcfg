115cc993eefcbb23676d625b32f294eed7f71fab
Aly Raffauf <aly@raffauflabs.com>
2025-07-02 20:29:38 -0400

    mossdeep: migrate to mySnippets.cute-haus.networkMap

--- hosts/mossdeep/services.nix
+++ hosts/mossdeep/services.nix
@@ -4,7 +4,7 @@
       enable = true;
       environmentFiles = [config.age.secrets.pds.path];
       pdsadmin.enable = true;
-      settings.PDS_HOSTNAME = "aly.social";
+      settings.PDS_HOSTNAME = config.mySnippets.cute-haus.networkMap.pds.vHost;
     };
   };
 
