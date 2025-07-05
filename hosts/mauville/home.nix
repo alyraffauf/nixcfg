29c347859941e982a3897a24bdbc43ce0206f04b
Aly Raffauf <aly@raffauflabs.com>
2025-07-04 23:58:41 -0400

    mauville/home: remove backblaze-sync

--- hosts/mauville/home.nix
+++ hosts/mauville/home.nix
@@ -4,11 +4,7 @@
   ...
 }: {
   home-manager.users.aly = lib.mkForce (
-    {
-      config,
-      pkgs,
-      ...
-    }: {
+    {pkgs, ...}: {
       imports = [
         self.homeManagerModules.default
         self.inputs.agenix.homeManagerModules.default
@@ -35,46 +31,6 @@
         };
 
         home-manager.enable = true;
-      };
-
-      systemd.user = {
-        services.backblaze-sync = {
-          Unit.Description = "Backup to Backblaze.";
-
-          Service = {
-            Environment = [
-              "PATH=${
-                lib.makeBinPath (with pkgs; [
-                  rclone
-                  uutils-coreutils-noprefix
-                ])
-              }"
-            ];
-
-            ExecStart = "${pkgs.writeShellScript "backblaze-sync" ''
-              declare -A backups
-              backups=(
-                ['/home/aly/roms']="b2://aly-roms"
-              )
-
-              # Recursively backup folders to B2 with sanity checks.
-              for folder in "''${!backups[@]}"; do
-                if [ -d "$folder" ] && [ "$(ls -A "$folder")" ]; then
-                  rclone --config=${config.age.secrets.rclone-b2.path} sync --metadata --track-renames --transfers 10 --progress $folder b2:''${backups[$folder]}
-                else
-                  echo "$folder does not exist or is empty."
-                  exit 1
-                fi
-              done
-            ''}";
-          };
-        };
-
-        timers.backblaze-sync = {
-          Install.WantedBy = ["timers.target"];
-          Timer.OnCalendar = "*-*-* 03:00:00";
-          Unit.Description = "Daily backups to Backblaze.";
-        };
       };
 
       myHome = {
