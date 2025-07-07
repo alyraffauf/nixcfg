f0644d257e1f73f4609a78a43506a595c9543a31
Aly Raffauf <aly@raffauflabs.com>
2025-07-07 14:32:10 -0400

    overlays: pin qbittorrent to 5.1.2

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,13 +9,13 @@ _: _final: prev: {
   });
 
   qbittorrent-nox = prev.qbittorrent-nox.overrideAttrs (_old: rec {
-    version = "5.1.0";
+    version = "5.1.2";
 
     src = prev.fetchFromGitHub {
       owner = "qbittorrent";
       repo = "qBittorrent";
       rev = "release-${version}";
-      hash = "sha256-ZLmKEdvtOxCzEOnJ4JPQQhR427YA288vTRxpk6O0tUc=";
+      hash = "sha256-2hcG2rMwo5wxVQjCEXXqPLGpdT6ihqtt3HsNlK1D9CA=";
     };
   });
 }
