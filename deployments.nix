436fa7df7b302964a5297a2c99c793344065e9d0
Aly Raffauf <aly@aly.codes>
2025-07-07 16:52:58 -0400

    deployments: specify types

--- deployments.nix
+++ deployments.nix
@@ -2,42 +2,49 @@
   evergrande = {
     output = "evergrande";
     hostname = "evergrande";
+    type = "nixos";
     user = "root";
   };
 
   lavaridge = {
     output = "lavaridge";
     hostname = "lavaridge";
+    type = "nixos";
     user = "root";
   };
 
   lilycove = {
     output = "lilycove";
     hostname = "lilycove";
+    type = "nixos";
     user = "root";
   };
 
   mauville = {
     output = "mauville";
     hostname = "mauville";
+    type = "nixos";
     user = "root";
   };
 
   mossdeep = {
     output = "mossdeep";
     hostname = "mossdeep";
+    type = "nixos";
     user = "root";
   };
 
   # slateport = {
   #   output = "slateport";
   #   hostname = "slateport";
+  #   type = "nixos";
   #   user = "root";
   # };
 
   verdanturf = {
     output = "verdanturf";
     hostname = "verdanturf";
+    type = "nixos";
     user = "root";
   };
 }
