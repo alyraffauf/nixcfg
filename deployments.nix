cc44438c92038fc486b62844b834f233cd458a19
Aly Raffauf <aly@aly.codes>
2025-07-10 13:36:06 -0400

    deployments: update to latest nynx spec

--- deployments.nix
+++ deployments.nix
@@ -1,57 +1,41 @@
 {
   evergrande = {
-    output = "evergrande";
-    hostname = "evergrande";
     type = "nixos";
     user = "root";
   };
 
   # Darwin example
   # fortree = {
-  #   output = "fortree";
-  #   hostname = "fortree";
   #   user = "aly";
   #   type = "darwin";
   # };
 
   lavaridge = {
-    output = "lavaridge";
-    hostname = "lavaridge";
     type = "nixos";
     user = "root";
   };
 
   lilycove = {
-    output = "lilycove";
-    hostname = "lilycove";
     type = "nixos";
     user = "root";
   };
 
   mauville = {
-    output = "mauville";
-    hostname = "mauville";
     type = "nixos";
     user = "root";
   };
 
   mossdeep = {
-    output = "mossdeep";
-    hostname = "mossdeep";
     type = "nixos";
     user = "root";
   };
 
   # slateport = {
-  #   output = "slateport";
-  #   hostname = "slateport";
   #   type = "nixos";
   #   user = "root";
   # };
 
   verdanturf = {
-    output = "verdanturf";
-    hostname = "verdanturf";
     type = "nixos";
     user = "root";
   };
