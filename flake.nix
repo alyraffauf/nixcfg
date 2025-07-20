74d21ad12562b0df4fb4a33b0e4c4a380029eb28
Aly Raffauf <aly@aly.codes>
2025-07-19 22:57:11 -0400

    github/tilscale: move ACLs to flake

--- flake.nix
+++ flake.nix
@@ -357,5 +357,28 @@
         text = builtins.readFile ./utils/formatter.sh;
       };
     });
+
+    tailscaleACLs = {
+      acls = [
+        {
+          action = "accept";
+          dst = ["*:*"];
+          src = ["*"];
+        }
+      ];
+
+      ssh = [
+        {
+          action = "accept";
+          dst = ["autogroup:self"];
+          src = ["autogroup:member"];
+
+          users = [
+            "autogroup:nonroot"
+            "root"
+          ];
+        }
+      ];
+    };
   };
 }
