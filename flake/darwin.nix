f3720d9f26b75c88faa70370cee75a68a24813d0
Aly Raffauf <aly@aly.codes>
2025-07-22 17:40:09 -0400

    flake/darwin: nest in toplevel flake = {}

--- flake/darwin.nix
+++ flake/darwin.nix
@@ -3,36 +3,39 @@
   inputs,
   ...
 }: {
-  flake.darwinModules.default = ../modules/darwin;
+  flake = {
+    darwinModules.default = ../modules/darwin;
 
-  flake.darwinConfigurations.fortree = inputs.nix-darwin.lib.darwinSystem {
-    specialArgs = {inherit self;};
-    modules = [
-      ../hosts/fortree
-      self.darwinModules.default
-      inputs.agenix.darwinModules.default
-      inputs.home-manager.darwinModules.home-manager
-      inputs.nix-homebrew.darwinModules.nix-homebrew
-      inputs.stylix.darwinModules.stylix
-      self.nixosModules.snippets
+    darwinConfigurations.fortree = inputs.nix-darwin.lib.darwinSystem {
+      modules = [
+        ../hosts/fortree
+        self.darwinModules.default
+        inputs.agenix.darwinModules.default
+        inputs.home-manager.darwinModules.home-manager
+        inputs.nix-homebrew.darwinModules.nix-homebrew
+        inputs.stylix.darwinModules.stylix
+        self.nixosModules.snippets
 
-      {
-        home-manager = {
-          useGlobalPkgs = true;
-          useUserPackages = true;
-          extraSpecialArgs = {inherit self;};
-          backupFileExtension = "backup";
-        };
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = {inherit self;};
+            backupFileExtension = "backup";
+          };
 
-        nixpkgs = {
-          overlays = [
-            self.inputs.nur.overlays.default
-            self.overlays.default
-          ];
+          nixpkgs = {
+            overlays = [
+              self.inputs.nur.overlays.default
+              self.overlays.default
+            ];
 
-          config.allowUnfree = true;
-        };
-      }
-    ];
+            config.allowUnfree = true;
+          };
+        }
+      ];
+
+      specialArgs = {inherit self;};
+    };
   };
 }
