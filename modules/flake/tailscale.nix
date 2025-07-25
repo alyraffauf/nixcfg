111e43f23776fc975c686e5e2da36fb501ebd221
Aly Raffauf <aly@raffauflabs.com>
2025-07-25 10:38:02 -0400

    modules/flake/tailscale: setup mullvad

--- modules/flake/tailscale.nix
+++ modules/flake/tailscale.nix
@@ -13,10 +13,26 @@ _: {
         action = "accept";
         dst = ["autogroup:self"];
         src = ["autogroup:member"];
-        users = [
-          "autogroup:nonroot"
-          "root"
-        ];
+        users = ["autogroup:nonroot" "root"];
+      }
+    ];
+
+    nodeAttrs = [
+      {
+        target = ["100.106.251.41"];
+        attr = ["mullvad"];
+      }
+      {
+        target = ["100.76.68.70"];
+        attr = ["mullvad"];
+      }
+      {
+        target = ["100.103.102.18"];
+        attr = ["mullvad"];
+      }
+      {
+        target = ["100.124.238.118"];
+        attr = ["mullvad"];
       }
     ];
   };
