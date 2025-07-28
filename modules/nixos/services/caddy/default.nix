18430c63b6df100b58c4a0f718c78558738810d5
Aly Raffauf <aly@raffauflabs.com>
2025-07-28 09:12:39 -0400

    nixos/services/caddy: fixup hash for caddy-tailscale

--- modules/nixos/services/caddy/default.nix
+++ modules/nixos/services/caddy/default.nix
@@ -25,7 +25,7 @@
 
         package = pkgs.caddy.withPlugins {
           plugins = ["github.com/tailscale/caddy-tailscale@v0.0.0-20250508175905-642f61fea3cc"];
-          hash = "sha256-K4K3qxN1TQ1Ia3yVLNfIOESXzC/d6HhzgWpC1qkT22k=";
+          hash = "sha256-0GsjeeJnfLsJywWzWwJcCDk5wjTSBwzqMBY7iHjPQa8=";
         };
       };
 
