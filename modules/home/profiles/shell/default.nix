6b9db4fb2005abc464038fc737d5604d729eadef
Aly Raffauf <aly@raffauflabs.com>
2025-07-29 10:05:26 -0400

    home/profiles/shell: store direnv cache in XDG_CACHE_HOME

--- modules/home/profiles/shell/default.nix
+++ modules/home/profiles/shell/default.nix
@@ -51,6 +51,18 @@
         enable = true;
         nix-direnv.enable = true;
         silent = true;
+
+        stdlib = ''
+          : ''${XDG_CACHE_HOME:=$HOME/.cache}
+          declare -A direnv_layout_dirs
+
+          direnv_layout_dir() {
+            echo "''${direnv_layout_dirs[$PWD]:=$(
+              echo -n "$XDG_CACHE_HOME"/direnv/layouts/
+              echo -n "$PWD" | sha1sum | cut -d ' ' -f 1
+            )}"
+          }
+        '';
       };
 
       eza = {
