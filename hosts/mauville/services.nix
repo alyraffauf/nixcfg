b7b11b40f7155e32c0d30d1ab7e0a7158b380b08
Aly Raffauf <aly@aly.codes>
2025-07-29 12:11:41 -0400

    hosts/{dewford,mauville}: move loki/grafana/prometheus proxies to mauville

--- hosts/mauville/services.nix
+++ hosts/mauville/services.nix
@@ -12,6 +12,35 @@
       port = 13378;
     };
 
+    caddy = {
+      email = "alyraffauf@fastmail.com";
+      virtualHosts = {
+        "${config.mySnippets.tailnet.networkMap.grafana.vHost}" = {
+          extraConfig = ''
+            bind tailscale/grafana
+            encode zstd gzip
+            reverse_proxy ${config.mySnippets.tailnet.networkMap.grafana.hostName}:${toString config.mySnippets.tailnet.networkMap.grafana.port}
+          '';
+        };
+
+        "${config.mySnippets.tailnet.networkMap.loki.vHost}" = {
+          extraConfig = ''
+            bind tailscale/loki
+            encode zstd gzip
+            reverse_proxy ${config.mySnippets.tailnet.networkMap.loki.hostName}:${toString config.mySnippets.tailnet.networkMap.loki.port}
+          '';
+        };
+
+        "${config.mySnippets.tailnet.networkMap.prometheus.vHost}" = {
+          extraConfig = ''
+            bind tailscale/prometheus
+            encode zstd gzip
+            reverse_proxy ${config.mySnippets.tailnet.networkMap.prometheus.hostName}:${toString config.mySnippets.tailnet.networkMap.prometheus.port}
+          '';
+        };
+      };
+    };
+
     glance = {
       enable = true;
       openFirewall = true;
