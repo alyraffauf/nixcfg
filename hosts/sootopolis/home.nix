4cf45002127fc3408ce87aee8be7614c34aacef9
Aly Raffauf <aly@aly.codes>
2025-08-03 14:35:12 -0400

    sootopolis: enable fontix

--- hosts/sootopolis/home.nix
+++ hosts/sootopolis/home.nix
@@ -1,17 +1,42 @@
-{self, ...}: {
+{
+  pkgs,
+  self,
+  ...
+}: {
   home-manager.users.aly = self.homeConfigurations.aly;
 
   home-manager.sharedModules = [
     {
-      stylix.targets = {
-        bat.enable = true;
+      fontix = {
+        fonts = {
+          monospace = {
+            name = "CaskaydiaCove Nerd Font";
+            package = pkgs.nerd-fonts.caskaydia-cove;
+          };
+
+          sansSerif = {
+            name = "Adwaita";
+            package = pkgs.adwaita-fonts;
+          };
+
+          serif = {
+            name = "Source Serif Pro";
+            package = pkgs.source-serif-pro;
+          };
+        };
+
+        sizes = {
+          applications = 11;
+          desktop = 10;
+        };
+
         font-packages.enable = true;
         fontconfig.enable = true;
         ghostty.enable = true;
-        helix.enable = true;
-        vscode.enable = true;
-        # zed.enable = true;
-        zellij.enable = true;
+        gnome.enable = true;
+        gtk.enable = true;
+        halloy.enable = true;
+        zed-editor.enable = true;
       };
     }
   ];
