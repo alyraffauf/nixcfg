3d4ccf25a47e58a2a32b1a9215a0f7d4ac4ab5cd
Aly Raffauf <aly@aly.codes>
2025-08-03 20:23:15 -0400

    home/services/swayosd: drop custom css

--- modules/home/services/swayosd/default.nix
+++ modules/home/services/swayosd/default.nix
@@ -11,65 +11,12 @@
       swayosd
     ];
 
-    services.swayosd = {
-      enable = lib.mkDefault true;
-      stylePath = "${config.xdg.configHome}/swayosd/style.css";
-    };
+    services.swayosd.enable = lib.mkDefault true;
 
     systemd.user.services.swayosd = {
       Install.WantedBy = lib.mkForce (lib.optional config.wayland.windowManager.hyprland.enable "hyprland-session.target");
       Service.Restart = lib.mkForce "no";
       Unit.BindsTo = lib.optional config.wayland.windowManager.hyprland.enable "hyprland-session.target";
-    };
-
-    xdg.configFile."swayosd/style.css" = {
-      text = ''
-        window#osd {
-          padding: 12px 20px;
-          border-radius: ${toString 10}px;
-          border: 4px solid alpha(${config.lib.stylix.colors.withHashtag."base0D"}, ${toString config.stylix.opacity.popups});
-          background: alpha(${config.lib.stylix.colors.withHashtag."base01"}, ${toString config.stylix.opacity.popups});
-        }
-
-        window#osd #container {
-          margin: 16px;
-        }
-
-        window#osd image,
-        window#osd label {
-          color: ${config.lib.stylix.colors.withHashtag."base06"};
-        }
-
-        window#osd progressbar:disabled,
-        window#osd image:disabled {
-          opacity: 0.5;
-        }
-
-        window#osd progressbar {
-          min-height: 6px;
-          border-radius: 999px;
-          background: transparent;
-          border: none;
-        }
-
-        window#osd trough {
-          min-height: inherit;
-          border-radius: inherit;
-          border: none;
-          background: alpha(${config.lib.stylix.colors.withHashtag."base06"}, 0.5);
-        }
-
-        window#osd progress {
-          min-height: inherit;
-          border-radius: inherit;
-          border: none;
-          background: ${config.lib.stylix.colors.withHashtag."base06"};
-        }
-      '';
-
-      onChange = ''
-        ${lib.getExe' pkgs.systemd "systemctl"} restart --user swayosd
-      '';
     };
   };
 }
