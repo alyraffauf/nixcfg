bc3bfa1be53a6be4e5489b63e8d261a27d080b04
Aly Raffauf <aly@aly.codes>
2025-08-08 21:38:26 -0400

    flake/overlays: tidy up overlays, pull packages from nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     actions-nix = {
       url = "github:alyraffauf/actions.nix";
--- modules/flake/overlays.nix
+++ modules/flake/overlays.nix
@@ -1,13 +1,12 @@
-_: {
+{self, ...}: {
   flake.overlays = {
-    default = _final: prev: {
-      ghostty = prev.ghostty.overrideAttrs (_: {
-        preBuild = ''
-          shopt -s globstar
-          sed -i 's/^const xev = @import("xev");$/const xev = @import("xev").Epoll;/' **/*.zig
-          shopt -u globstar
-        '';
-      });
+    default = _final: prev: let
+      nixpkgs-unstable = import self.inputs.nixpkgs-unstable {
+        config.allowUnfree = true;
+        inherit (prev) system;
+      };
+    in {
+      inherit (nixpkgs-unstable) helix ghostty vscode zed-editor;
 
       qbittorrent-nox = prev.qbittorrent-nox.overrideAttrs (_old: rec {
         version = "5.1.2";
