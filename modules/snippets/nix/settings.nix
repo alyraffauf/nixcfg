6350192bdb507c4a652935f7bdf820f0cc1d22b7
Aly Raffauf <aly@aly.codes>
2025-08-09 22:17:32 -0400

    snippets/nix: add catppuccin cachy

--- modules/snippets/nix/settings.nix
+++ modules/snippets/nix/settings.nix
@@ -18,12 +18,14 @@
         substituters = [
           "https://alyraffauf.cachix.org"
           "https://cache.nixos.org/"
+          "https://catppuccin.cachix.org"
           "https://chaotic-nyx.cachix.org/"
           "https://nix-community.cachix.org"
         ];
 
         trusted-public-keys = [
           "alyraffauf.cachix.org-1:GQVrRGfjTtkPGS8M6y7Ik0z4zLt77O0N25ynv2gWzDM="
+          "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
           "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8"
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         ];
