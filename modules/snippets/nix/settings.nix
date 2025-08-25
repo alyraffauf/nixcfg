781c2db822d87e12b962c21bf16d0dc27b613f31
Aly Raffauf <aly@aly.codes>
2025-08-24 21:41:18 -0400

    snippets/nix/settings: add aly user

--- modules/snippets/nix/settings.nix
+++ modules/snippets/nix/settings.nix
@@ -30,7 +30,7 @@
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         ];
 
-        trusted-users = ["@admin" "@wheel" "nixbuild"];
+        trusted-users = ["aly" "@admin" "@wheel" "nixbuild"];
       };
     };
   };
