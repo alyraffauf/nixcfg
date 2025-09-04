2cd578dfd567394e636c31ea04249b49b14a9e67
Aly Raffauf <aly@aly.codes>
2025-09-03 23:40:57 -0400

    lavaridge/home: add gui-less home

--- hosts/lavaridge/home.nix
+++ hosts/lavaridge/home.nix
@@ -1,3 +1,43 @@
 {self, ...}: {
-  home-manager.users.aly = self.homeConfigurations.aly;
+  home-manager.users.aly = {pkgs, ...}: {
+    imports = [
+      self.homeModules.default
+      self.inputs.agenix.homeManagerModules.default
+    ];
+
+    age.secrets.rclone-b2.file = "${self.inputs.secrets}/rclone/b2.age";
+
+    home = {
+      homeDirectory = "/home/aly";
+
+      packages = with pkgs; [
+        curl
+        rclone
+        restic
+      ];
+
+      stateVersion = "25.05";
+      username = "aly";
+    };
+    programs = {
+      helix = {
+        enable = true;
+        defaultEditor = true;
+      };
+
+      home-manager.enable = true;
+    };
+
+    myHome = {
+      aly.programs = {
+        git.enable = true;
+        awscli.enable = true;
+        rbw.enable = true;
+        ssh.enable = true;
+      };
+
+      profiles.shell.enable = true;
+      programs.fastfetch.enable = true;
+    };
+  };
 }
