75d670663dc85569036aa1c22fc86c6590c769b8
Aly Raffauf <aly@aly.codes>
2025-09-28 22:33:35 -0400

    pacifidlog: add disk by id

--- hosts/pacifidlog/default.nix
+++ hosts/pacifidlog/default.nix
@@ -21,7 +21,7 @@
   services.xserver.xkb.options = "ctrl:nocaps";
   system.stateVersion = "25.11";
   time.timeZone = "America/New_York";
-  # myDisko.installDrive = "/dev/disk/by-id/nvme-SHPP41-1000GM_ANCAN50211160B42I";
+  myDisko.installDrive = "/dev/disk/by-id/nvme-SK_hynix_PC801_HFS002TEJ9X101N_BDE4N54161OA05A2VI";
   myHardware.hp.omnibook.fh0xxx.enable = true;
 
   myNixOS = {
