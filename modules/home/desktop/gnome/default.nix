bc1e3171aeaa54659fe121fe34589341c3267d94
Aly Raffauf <aly@aly.codes>
2025-09-30 13:27:28 -0400

    home/desktop/gnome: remove search providers

--- modules/home/desktop/gnome/default.nix
+++ modules/home/desktop/gnome/default.nix
@@ -31,7 +31,6 @@
         };
 
         # "org/gnome/desktop/peripherals/touchpad".tap-to-click = true;
-        "org/gnome/desktop/search-providers".enabled = "['org.gnome.Calendar.desktop', 'org.gnome.Weather.desktop', 'org.gnome.Contacts.desktop', 'org.gnome.Calculator.desktop', 'org.gnome.Characters.desktop', 'org.gnome.clocks.desktop']";
 
         "org/gnome/desktop/wm/preferences" = {
           auto-raise = true;
