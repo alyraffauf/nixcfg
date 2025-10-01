459e9182b42f0576504ca4c45d4a0c55f59ad37c
Aly Raffauf <aly@aly.codes>
2025-10-01 11:28:17 -0400

    flake/overlays: peg linuxPackages_latest at 6.16.8

--- modules/flake/overlays.nix
+++ modules/flake/overlays.nix
@@ -1,6 +1,18 @@
 _: {
   flake.overlays = {
     default = _final: prev: {
+      linuxPackages_latest = prev.linuxPackagesFor (prev.linux_6_16.override {
+        argsOverride = rec {
+          src = prev.fetchurl {
+            url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
+            sha256 = "IxMRvXCE3DEplE0mu0O+b/g32oL7IQSmdwSuvKi/pp8=";
+          };
+
+          version = "6.16.8";
+          modDirVersion = "6.16.8";
+        };
+      });
+
       qbittorrent-nox = prev.qbittorrent-nox.overrideAttrs (_old: rec {
         version = "5.1.2";
 
