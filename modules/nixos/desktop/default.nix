1ef095035ab8db64adf16e52c94fe7e7dc1ec978
Aly Raffauf <aly@aly.codes>
2025-10-28 19:07:36 -0400

    nixos/desktop: silence boot text and bootloader options for flickerless boot.

--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -21,6 +21,16 @@
     boot = {
       consoleLogLevel = 0;
       initrd.verbose = false;
+
+      kernelParams = [
+        "quiet"
+        "splash"
+        "boot.shell_on_fail"
+        "udev.log_priority=3"
+        "rd.systemd.show_status=auto"
+      ];
+
+      loader.timeout = 0;
       plymouth.enable = true;
     };
 
