d6d2fc86fd547de9e9409fd9b135722f3e3149d4
Aly Raffauf <aly@aly.codes>
2025-10-30 23:47:35 -0400

    Revert "flake/overlays: remove nixos-unstable-small overlays"
    This reverts commit 72a9bd0c2456c88ddd486aefb57cb633da99a481.

--- modules/flake/overlays.nix
+++ modules/flake/overlays.nix
@@ -1,6 +1,13 @@
-_: {
+{self, ...}: {
   flake.overlays = {
-    default = _final: prev: {
+    default = _final: prev: let
+      nixos-unstable-small = import self.inputs.nixpkgs-unstable-small {
+        config.allowUnfree = true;
+        inherit (prev) system;
+      };
+    in {
+      inherit (nixos-unstable-small) firefox plex plexRaw thunderbird zed-editor;
+
       qbittorrent-nox = prev.qbittorrent-nox.overrideAttrs (_old: rec {
         version = "5.1.2";
 
