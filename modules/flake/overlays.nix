d649db1ccaf4d1ec1580666162a6215be5b95f31
Aly Raffauf <aly@aly.codes>
2025-10-30 23:49:01 -0400

    flake/overlays: pull cosmic from unstable-small

--- modules/flake/overlays.nix
+++ modules/flake/overlays.nix
@@ -6,7 +6,39 @@
         inherit (prev) system;
       };
     in {
-      inherit (nixos-unstable-small) firefox plex plexRaw thunderbird zed-editor;
+      inherit
+        (nixos-unstable-small)
+        plex
+        firefox
+        plexRaw
+        cosmic-bg
+        cosmic-osd
+        zed-editor
+        cosmic-comp
+        cosmic-edit
+        cosmic-idle
+        cosmic-term
+        thunderbird
+        cosmic-files
+        cosmic-icons
+        cosmic-panel
+        cosmic-randr
+        cosmic-store
+        cosmic-player
+        cosmic-applets
+        cosmic-greeter
+        cosmic-session
+        cosmic-launcher
+        cosmic-settings
+        cosmic-applibrary
+        cosmic-screenshot
+        cosmic-wallpapers
+        cosmic-initial-setup
+        cosmic-notifications
+        cosmic-settings-daemon
+        cosmic-workspaces-epoch
+        xdg-desktop-portal-cosmic
+        ;
 
       qbittorrent-nox = prev.qbittorrent-nox.overrideAttrs (_old: rec {
         version = "5.1.2";
