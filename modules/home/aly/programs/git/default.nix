663a0258ccac32267024ad8a519b53cb0b4f851d
Aly Raffauf <aly@aly.codes>
2025-11-05 07:34:44 -0500

    home/aly/programs/git: add wl-clipboard for lazygit

--- modules/home/aly/programs/git/default.nix
+++ modules/home/aly/programs/git/default.nix
@@ -1,11 +1,14 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   options.myHome.aly.programs.git.enable = lib.mkEnableOption "git version control";
 
   config = lib.mkIf config.myHome.aly.programs.git.enable {
+    home.packages = [pkgs.wl-clipboard];
+
     programs = {
       delta = {
         enable = true;
