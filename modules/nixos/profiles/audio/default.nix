fd15263134346288d95e7152631e60a6db5e9846
Aly Raffauf <aly@aly.codes>
2025-11-19 21:22:22 -0500

    nixos/profiles/audio; disable bugged airplay support

--- modules/nixos/profiles/audio/default.nix
+++ modules/nixos/profiles/audio/default.nix
@@ -18,17 +18,22 @@
         };
 
         pulse.enable = true;
-        raopOpenFirewall = true;
+        # raopOpenFirewall = true;
 
-        extraConfig.pipewire = {
-          "10-airplay" = {
-            "context.modules" = [
-              {
-                name = "libpipewire-module-raop-discover";
-              }
-            ];
-          };
-        };
+        # extraConfig.pipewire = {
+        #   "10-airplay" = {
+        #     "context.modules" = [
+        #       {
+        #         name = "libpipewire-module-raop-discover";
+
+        #         # increase the buffer size if you get dropouts/glitches
+        #         args = {
+        #           "raop.latency.ms" = 500;
+        #         };
+        #       }
+        #     ];
+        #   };
+        # };
       };
 
       pulseaudio.support32Bit = true;
