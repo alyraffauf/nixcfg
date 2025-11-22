ac5b4dff8fbffaa073853a2b6db05dac01471069
Aly Raffauf <aly@aly.codes>
2025-11-21 21:29:04 -0500

    hardware/profiles/laptop: cleanup

--- modules/hardware/profiles/laptop/default.nix
+++ modules/hardware/profiles/laptop/default.nix
@@ -8,19 +8,11 @@
   config = lib.mkMerge [
     (lib.mkIf config.myHardware.profiles.laptop.enable
       {
-        boot.kernel.sysctl."kernel.nmi_watchdog" = lib.mkDefault 0;
-
         services = {
           tuned = {
             enable = lib.mkDefault true;
             settings.dynamic_tuning = true;
           };
-
-          # udev.extraRules = lib.mkIf config.services.power-profiles-daemon.enable ''
-          #   ## Automatically switch power profiles based on AC power status.
-          #   ACTION=="change", SUBSYSTEM=="power_supply", ATTRS{type}=="Mains", ATTRS{online}=="0", RUN+="${lib.getExe pkgs.power-profiles-daemon} set power-saver"
-          #   ACTION=="change", SUBSYSTEM=="power_supply", ATTRS{type}=="Mains", ATTRS{online}=="1", RUN+="${lib.getExe pkgs.power-profiles-daemon} set balanced"
-          # '';
 
           upower.enable = true;
         };
