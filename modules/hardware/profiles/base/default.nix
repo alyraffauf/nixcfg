295f72c4f5d7b8aa8eb54093b8b1426ca87a44b1
Aly Raffauf <aly@aly.codes>
2025-11-21 22:26:04 -0500

    hardware/profiles/base: remove easyeffects profiles

--- modules/hardware/profiles/base/default.nix
+++ modules/hardware/profiles/base/default.nix
@@ -7,18 +7,6 @@
 
   config = lib.mkIf config.myHardware.profiles.base.enable {
     hardware.enableAllFirmware = true;
-
-    home-manager.sharedModules = [
-      (
-        {config, ...}: {
-          xdg.configFile = lib.mkIf config.services.easyeffects.enable {
-            "easyeffects/output/LoudnessEqualizer.json".source = ./LoudnessEqualizer.json;
-            "easyeffects/output/AdvancedAutoGain.json".source = ./AdvancedAutoGain.json;
-          };
-        }
-      )
-    ];
-
     services.fstrim.enable = true;
     myHardware.lenovo.thinkpad.kb5D50X.enable = true;
   };
