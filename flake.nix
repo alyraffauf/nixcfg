da91271e56018dfb120ebcd430b248645018f16f
Aly Raffauf <aly@raffauflabs.com>
2025-12-01 00:00:06 -0500

    flake: lock nix-darwin to nix-darwin-25.11
    Flake lock file updates:

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
     };
 
     nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/master";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
