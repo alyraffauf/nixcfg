9d20e0848c721e47b544a93fcc2c12a72afe0bd5
Aly Raffauf <aly@aly.codes>
2025-12-16 21:25:21 -0500

    Revert "homes: drop aly@pacifidlog"
    This reverts commit d5d2e59cb5c37f5ce10b6bf258a9cc4835f9216d.

--- modules/flake/home-manager.nix
+++ modules/flake/home-manager.nix
@@ -1,5 +1,26 @@
-_: {
+{self, ...}: {
   flake = {
+    homeConfigurations = {
+      "aly@pacifidlog" = self.inputs.home-manager.lib.homeManagerConfiguration {
+        extraSpecialArgs = {inherit self;};
+
+        modules = [
+          ../../homes/aly/pacifidlog.nix
+        ];
+
+        pkgs = import self.inputs.nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+
+          overlays = [
+            self.inputs.nixgl.overlay
+            self.inputs.nur.overlays.default
+            self.overlays.default
+          ];
+        };
+      };
+    };
+
     homeModules = {
       default = ../home;
       aly = ../../homes/aly;
