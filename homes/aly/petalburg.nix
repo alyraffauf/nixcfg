7dd879f4d6d3f97f6b5b942b1ccd8fd957e0119b
Aly Raffauf <aly@aly.codes>
2025-12-27 17:23:29 -0500

    add aly@petalburg

---
+++ homes/aly/petalburg.nix
@@ -0,0 +1,84 @@
+{
+  config,
+  lib,
+  pkgs,
+  self,
+  ...
+}: {
+  imports = [
+    self.homeModules.default
+    self.inputs.safari.homeModules.default
+  ];
+
+  age.secrets = {
+    syncthingCert.file = "${self.inputs.secrets}/aly/syncthing/petalburg/cert.age";
+    syncthingKey.file = "${self.inputs.secrets}/aly/syncthing/petalburg/key.age";
+  };
+
+  home = {
+    homeDirectory = "/home/aly";
+    sessionVariables.NIXOS_OZONE_WL = "1";
+    stateVersion = "25.11";
+    username = "aly";
+  };
+
+  nix = {
+    inherit (config.mySnippets.nix) settings;
+    package = pkgs.nix;
+
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 3d";
+      persistent = true;
+      randomizedDelaySec = "60min";
+    };
+  };
+
+  programs = {
+    home-manager.enable = true;
+    # zed-editor.package = config.lib.nixGL.wrap pkgs.zed-editor;
+  };
+
+  services.syncthing = let
+    inherit (config.mySnippets.syncthing) devices;
+
+    folders = lib.mkMerge [
+      config.mySnippets.syncthing.folders
+      {
+        "music" = {
+          enable = lib.mkForce false;
+          path = "/home/aly/music";
+        };
+
+        "roms".path = "/home/aly/ROMs";
+        "screenshots".path = lib.mkForce "/home/aly/Pictures/Screenshots";
+        "sync".path = lib.mkForce "/home/aly/Sync";
+      }
+    ];
+  in {
+    enable = true;
+    cert = config.age.secrets.syncthingCert.path;
+    key = config.age.secrets.syncthingKey.path;
+
+    settings = {
+      options = {
+        localAnnounceEnabled = true;
+        relaysEnabled = true;
+        urAccepted = -1;
+      };
+
+      inherit devices folders;
+    };
+  };
+
+  targets.genericLinux = {
+    enable = true;
+
+    nixGL = {
+      inherit (self.inputs.nixgl) packages;
+      vulkan.enable = true;
+    };
+  };
+
+  xdg.enable = true;
+}
--- modules/flake/home-manager.nix
+++ modules/flake/home-manager.nix
@@ -20,6 +20,25 @@
         };
       };
 
+      "aly@petalburg" = self.inputs.home-manager.lib.homeManagerConfiguration {
+        extraSpecialArgs = {inherit self;};
+
+        modules = [
+          ../../homes/aly/petalburg.nix
+        ];
+
+        pkgs = import self.inputs.nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+
+          overlays = [
+            self.inputs.nixgl.overlay
+            self.inputs.nur.overlays.default
+            self.overlays.default
+          ];
+        };
+      };
+
       "aly@rustboro" = self.inputs.home-manager.lib.homeManagerConfiguration {
         extraSpecialArgs = {inherit self;};
 
