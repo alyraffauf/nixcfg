d00fe2206a83dc173d49d48ea1e95522a6a230bf
Aly Raffauf <aly@aly.codes>
2026-05-30 10:09:02 -0400

    cosic: drop hm enablement

--- modules/nixos/desktop/cosmic/default.nix
+++ modules/nixos/desktop/cosmic/default.nix
@@ -6,12 +6,6 @@
   options.myNixOS.desktop.cosmic.enable = lib.mkEnableOption "COSMIC desktop environment";
 
   config = lib.mkIf config.myNixOS.desktop.cosmic.enable {
-    home-manager.sharedModules = [
-      {
-        config.myHome.desktop.cosmic.enable = true;
-      }
-    ];
-
     programs.firefox.policies.Preferences."browser.tabs.inTitlebar" = lib.mkIf config.programs.firefox.enable 1;
     services.desktopManager.cosmic.enable = true;
     system.nixos.tags = ["cosmic"];
