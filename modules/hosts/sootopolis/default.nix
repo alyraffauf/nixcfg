3aaa3b3510eb3ffc5505573c28655e30847826ea
Aly Raffauf <aly@aly.codes>
2026-08-07 18:35:33 -0400

    hosts/sootopolis: preserve Fedora session path

--- modules/hosts/sootopolis/default.nix
+++ modules/hosts/sootopolis/default.nix
@@ -1,8 +1,17 @@
-{inputs, ...}: {
+{
+  inputs,
+  lib,
+  ...
+}: {
   flake.systemConfigs.sootopolis = inputs.system-manager.lib.makeSystemConfig {
     modules = [
       inputs.nix-system-graphics.systemModules.default
       ({pkgs, ...}: {
+        environment.etc."environment.d/10-system-manager.conf".text = lib.mkForce ''
+          PATH=/run/system-manager/sw/bin:/usr/local/bin:/usr/bin
+          XDG_DATA_DIRS=/run/system-manager/sw/share:/usr/local/share:/usr/share
+        '';
+
         environment.systemPackages = with pkgs; [
           ghostty
           system-manager
