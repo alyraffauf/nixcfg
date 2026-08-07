d809c2cc1027bce0a4d4dcd9ec797251ee842d56
Aly Raffauf <aly@aly.codes>
2026-08-07 14:47:05 -0400

    nixos: configure flatpak portals

--- modules/nixos/services/flatpak.nix
+++ modules/nixos/services/flatpak.nix
@@ -1,6 +1,12 @@
 _: {
-  flake.nixosModules.default = {
+  flake.nixosModules.default = {pkgs, ...}: {
     services.flatpak.enable = true;
-    xdg.portal.enable = true;
+
+    xdg.portal = {
+      enable = true;
+      config.common.default = "*";
+      extraPortals = [pkgs.xdg-desktop-portal-gtk];
+      xdgOpenUsePortal = true;
+    };
   };
 }
