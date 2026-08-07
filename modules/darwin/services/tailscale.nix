125539ac0eed26823776a41ad06e3f352b755008
Aly Raffauf <aly@aly.codes>
2026-08-07 12:55:35 -0400

    document tailscale

--- modules/darwin/services/tailscale.nix
+++ modules/darwin/services/tailscale.nix
@@ -1,17 +1,20 @@
 _: {
   flake.darwinModules.default = {
     networking = {
+      # Explicitly manage these
       knownNetworkServices = [
         "Wi-Fi"
         "Ethernet Adaptor"
         "Thunderbolt Ethernet"
       ];
 
+      # Otherwise tailscale ssh only works with FQDNs
       search = ["narwhal-snapper.ts.net"];
     };
 
     services.tailscale = {
       enable = true;
+      # Needs a declared DNS provider in Tailscale's admin panel
       overrideLocalDns = true;
     };
   };
