6b9f8a1da149825e71abc879ca0af705826de219
Aly Raffauf <aly@aly.codes>
2026-08-07 17:09:56 -0400

    nixos: restore standard appimage handling

--- modules/nixos/programs/appimage.nix
+++ modules/nixos/programs/appimage.nix
@@ -1,23 +1,8 @@
 _: {
-  flake.nixosModules.default = {pkgs, ...}: {
+  flake.nixosModules.default = {
     programs.appimage = {
       enable = true;
       binfmt = true;
-      package = pkgs.writeShellScriptBin "appimage-run" ''
-        if [ "$2" = "--appimage-extract" ]; then
-          if ${pkgs.dwarfs}/bin/dwarfsck --quiet --input "$1" >/dev/null 2>&1; then
-            ${pkgs.coreutils}/bin/mkdir -p "$PWD/squashfs-root"
-            exec ${pkgs.dwarfs}/bin/dwarfsextract \
-              --input "$1" \
-              --output "$PWD/squashfs-root" \
-              --log-level=error
-          fi
-
-          exec ${pkgs.appimage-run}/bin/appimage-run -x "$PWD/squashfs-root" "$1"
-        fi
-
-        exec ${pkgs.appimage-run}/bin/appimage-run "$@"
-      '';
     };
   };
 }
