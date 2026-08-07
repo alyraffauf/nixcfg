c9c995484de50d48813376d51dd746962944d140
Aly Raffauf <aly@aly.codes>
2026-08-07 15:40:53 -0400

    nixos: authenticate tailscale with sops

--- modules/nixos/services/tailscale.nix
+++ modules/nixos/services/tailscale.nix
@@ -1,11 +1,18 @@
 _: {
-  flake.nixosModules.default = {config, ...}: {
+  flake.nixosModules.default = {
+    config,
+    self,
+    ...
+  }: {
     networking.firewall = {
       allowedUDPPorts = [config.services.tailscale.port];
       trustedInterfaces = [config.services.tailscale.interfaceName];
     };
 
+    sops.secrets.tailscale-auth-key.sopsFile = self + "/secrets/tailscale.yaml";
+
     services.tailscale = {
+      authKeyFile = config.sops.secrets.tailscale-auth-key.path;
       enable = true;
       extraUpFlags = ["--ssh" "--accept-routes"];
       openFirewall = true;
