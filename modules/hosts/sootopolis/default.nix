6b85c8efd96eaacc3216376d0298d465f0b27ecf
Aly Raffauf <aly@aly.codes>
2026-08-07 18:26:31 -0400

    hosts/sootopolis: allow unfree packages

--- modules/hosts/sootopolis/default.nix
+++ modules/hosts/sootopolis/default.nix
@@ -6,7 +6,11 @@
         environment.systemPackages = [
           inputs.system-manager.packages.x86_64-linux.default
         ];
-        nixpkgs.hostPlatform = "x86_64-linux";
+
+        nixpkgs = {
+          config.allowUnfree = true;
+          hostPlatform = "x86_64-linux";
+        };
         system-manager.allowAnyDistro = true;
         system-graphics.enable = true;
       }
