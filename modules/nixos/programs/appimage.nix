1bea5860a2b7bf57923b916ee4f2f2577e624b3a
Aly Raffauf <aly@aly.codes>
2026-08-07 17:07:08 -0400

    nixos: extract dwarfs appimages

--- modules/nixos/programs/appimage.nix
+++ modules/nixos/programs/appimage.nix
@@ -5,6 +5,14 @@ _: {
       binfmt = true;
       package = pkgs.writeShellScriptBin "appimage-run" ''
         if [ "$2" = "--appimage-extract" ]; then
+          if ${pkgs.dwarfs}/bin/dwarfsck --quiet --input "$1" >/dev/null 2>&1; then
+            ${pkgs.coreutils}/bin/mkdir -p "$PWD/squashfs-root"
+            exec ${pkgs.dwarfs}/bin/dwarfsextract \
+              --input "$1" \
+              --output "$PWD/squashfs-root" \
+              --log-level=error
+          fi
+
           exec ${pkgs.appimage-run}/bin/appimage-run -x "$PWD/squashfs-root" "$1"
         fi
 
