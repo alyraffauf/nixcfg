b46d863176dab947b5fc878ca35f0da927b1d26b
Aly Raffauf <aly@aly.codes>
2026-08-07 16:56:36 -0400

    nixos: support appimage cask extraction

--- modules/nixos/programs/appimage.nix
+++ modules/nixos/programs/appimage.nix
@@ -1,8 +1,15 @@
 _: {
-  flake.nixosModules.default = {
+  flake.nixosModules.default = {pkgs, ...}: {
     programs.appimage = {
       enable = true;
       binfmt = true;
+      package = pkgs.writeShellScriptBin "appimage-run" ''
+        if [ "$2" = "--appimage-extract" ]; then
+          exec ${pkgs.appimage-run}/bin/appimage-run -x "$PWD/squashfs-root" "$1"
+        fi
+
+        exec ${pkgs.appimage-run}/bin/appimage-run "$@"
+      '';
     };
   };
 }
