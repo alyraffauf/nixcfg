7fce15ec8daf0998ebf081c3d3c853dabaa697c5
Aly Raffauf <aly@aly.codes>
2026-08-07 14:03:02 -0400

    pipes

--- modules/darwin/authorized-keys.nix
+++ modules/darwin/authorized-keys.nix
@@ -5,10 +5,11 @@ _: {
     ...
   }: let
     keysDirectory = self + "/keys";
-    alyKeyFiles = lib.map (file: "${keysDirectory}/${file}") (
-      lib.filter (file: lib.hasPrefix "aly_" file)
-      (builtins.attrNames (builtins.readDir keysDirectory))
-    );
+    alyKeyFiles = lib.pipe (builtins.readDir keysDirectory) [
+      builtins.attrNames
+      (lib.filter (file: lib.hasPrefix "aly_" file))
+      (lib.map (file: "${keysDirectory}/${file}"))
+    ];
   in {
     users.users = {
       aly.openssh.authorizedKeys.keyFiles = alyKeyFiles;
