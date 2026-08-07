b20c20e8d5d83556ee86e1a5659b83b21cf64e26
Aly Raffauf <aly@aly.codes>
2026-08-07 15:43:35 -0400

    nixos: add graphical boot

---
+++ modules/nixos/boot/graphical.nix
@@ -0,0 +1,15 @@
+_: {
+  flake.nixosModules.default = {
+    boot = {
+      consoleLogLevel = 0;
+      initrd.verbose = false;
+      kernelParams = [
+        "quiet"
+        "rd.systemd.show_status=auto"
+        "udev.log_level=3"
+      ];
+      loader.timeout = 1;
+      plymouth.enable = true;
+    };
+  };
+}
