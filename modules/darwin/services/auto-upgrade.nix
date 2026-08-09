0bd8db6e6d7bf65088ed098d84df41f5c9c8d6ad
Aly Raffauf <aly@aly.codes>
2026-08-09 16:59:25 -0400

    darwin: add automatic upgrades

---
+++ modules/darwin/services/auto-upgrade.nix
@@ -0,0 +1,43 @@
+_: {
+  flake.darwinModules.default = {
+    config,
+    lib,
+    ...
+  }: let
+    maxAttempts = 3;
+    randomizedDelaySeconds = 45 * 60;
+    retryDelaySeconds = 15 * 60;
+  in {
+    launchd.daemons.nix-darwin-upgrade = {
+      script = ''
+        set -eu
+
+        random_delay="$(( $(/usr/bin/printf '%s' ${lib.escapeShellArg config.networking.hostName} | /usr/bin/cksum | /usr/bin/awk '{ print $1 }') % ${toString randomizedDelaySeconds} ))"
+        /bin/sleep "$random_delay"
+
+        for attempt in $(/usr/bin/jot 1 ${toString maxAttempts}); do
+          if ${config.system.build.darwin-rebuild}/bin/darwin-rebuild switch --flake ${lib.escapeShellArg "github:alyraffauf/hoenn#${config.networking.hostName}"} --refresh; then
+            exit 0
+          fi
+
+          if [ "$attempt" -lt ${toString maxAttempts} ]; then
+            /bin/sleep ${toString retryDelaySeconds}
+          fi
+        done
+
+        exit 1
+      '';
+
+      serviceConfig = {
+        ProcessType = "Background";
+        RunAtLoad = false;
+        StandardErrorPath = "/var/log/nix-darwin-upgrade.log";
+        StandardOutPath = "/var/log/nix-darwin-upgrade.log";
+        StartCalendarInterval = {
+          Hour = 2;
+          Minute = 0;
+        };
+      };
+    };
+  };
+}
