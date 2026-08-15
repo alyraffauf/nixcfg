cf8d13fcf7cb3758339648a0c18d0b275debe47c
Aly Raffauf <aly@aly.codes>
2026-08-15 19:28:18 -0400

    nixos: ensure LilycoveDeptStore Wi-Fi profile

--- modules/nixos/services/networkmanager.nix
+++ modules/nixos/services/networkmanager.nix
@@ -1,5 +1,41 @@
-_: {
+{
+  config,
+  self,
+  ...
+}: {
   flake.nixosModules.default = {
-    networking.networkmanager.enable = true;
+    sops.secrets.lilycove-dept-store-wifi = {
+      key = "environment";
+      sopsFile = self + "/secrets/wifi.yaml";
+    };
+
+    networking.networkmanager = {
+      enable = true;
+
+      ensureProfiles = {
+        environmentFiles = [config.sops.secrets.lilycove-dept-store-wifi.path];
+
+        profiles.LilycoveDeptStore = {
+          connection = {
+            id = "LilycoveDeptStore";
+            type = "wifi";
+          };
+          ipv4.method = "auto";
+          ipv6 = {
+            addr-gen-mode = "default";
+            method = "auto";
+          };
+          wifi = {
+            mode = "infrastructure";
+            ssid = "LilycoveDeptStore";
+          };
+          wifi-security = {
+            auth-alg = "open";
+            key-mgmt = "wpa-psk";
+            psk = "$LilycoveDeptStorePSK";
+          };
+        };
+      };
+    };
   };
 }
