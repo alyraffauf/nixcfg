d97ffa57cd2e39e4b15790ecdcb219a3883a4e89
Aly Raffauf <aly@aly.codes>
2026-08-16 22:27:06 -0400

    feat(nixos): add Bitwarden and AppIndicator support

--- modules/nixos/features/gnome.nix
+++ modules/nixos/features/gnome.nix
@@ -1,5 +1,7 @@
 _: {
-  flake.nixosModules.gnome = {
+  flake.nixosModules.gnome = {pkgs, ...}: {
+    environment.systemPackages = [pkgs.gnomeExtensions.appindicator];
+
     services = {
       desktopManager.gnome.enable = true;
       displayManager.gdm.enable = true;
--- modules/nixos/users/aly/zen-browser.nix
+++ modules/nixos/users/aly/zen-browser.nix
@@ -1,7 +1,12 @@
 {inputs, ...}: {
   flake.nixosModules.aly = {pkgs, ...}: {
     users.users.aly.packages = [
-      inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
+      (pkgs.wrapFirefox inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.zen-browser-unwrapped {
+        extraPolicies.ExtensionSettings."{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+          installation_mode = "normal_installed";
+        };
+      })
     ];
   };
 }
